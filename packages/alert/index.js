2a01030420ad052fde9e38f04906f39663e5fa61
Caroline Gard <caroline.gard@mpyadigital.com>
2023-05-12 09:23:28 +0200

    feat(alert): warpify alert component (#17)
    * feat(alert): warpify alert component
    
    * docs(alert): add todo comment

--- packages/alert/index.js
+++ packages/alert/index.js
@@ -1,35 +1,15 @@
-import { css, html, LitElement } from 'lit';
-import { infoSvg, negativeSvg, positiveSvg, warningSvg } from './svgs';
+// TODO: replace text-14 with a token
 
-const variants = {
-  negative: {
-    color: 'red',
-    icon: negativeSvg(),
-    bgColor: 'bg-red-50',
-    borderColor: 'border-red-300',
-    textColor: 'text-red-600',
-  },
-  positive: {
-    color: 'green',
-    icon: positiveSvg(),
-    bgColor: 'bg-green-50',
-    borderColor: 'border-green-300',
-    textColor: 'text-green-600',
-  },
-  warning: {
-    color: 'yellow',
-    icon: warningSvg(),
-    bgColor: 'bg-yellow-50',
-    borderColor: 'border-yellow-300',
-    textColor: 'text-yellow-600',
-  },
-  info: {
-    color: 'aqua',
-    icon: infoSvg(),
-    bgColor: 'bg-aqua-50',
-    borderColor: 'border-aqua-300',
-    textColor: 'text-aqua-600',
-  },
+import { css, html, LitElement } from 'lit';
+import { alert as ccAlert } from '@warp-ds/component-classes';
+import { infoSvg, negativeSvg, positiveSvg, warningSvg } from './svgs';
+import { classNames } from '@chbphone55/classnames';
+
+const icons = {
+  negative: negativeSvg(),
+  positive: positiveSvg(),
+  warning: warningSvg(),
+  info: infoSvg(),
 };
 
 class WarpAlert extends LitElement {
@@ -47,44 +27,56 @@ class WarpAlert extends LitElement {
 
   connectedCallback() {
     super.connectedCallback();
-    if (!this.variant || !Object.keys(variants).includes(this.variant)) {
+    if (!this.variant || !Object.keys(icons).includes(this.variant)) {
       throw new Error(
-        'Invalid "variant" attribute. Set its value to one of the following:\nnegative, positive, warning, info.',
+        'Invalid "variant" attribute. Set its value to one of the following:\nnegative, positive, warning, info.'
       );
     }
+  }
+
+  get _wrapperClasses() {
+    return classNames({
+      [ccAlert.alert]: true,
+      [ccAlert[this.variant]]: true,
+    });
+  }
+
+  get _iconClasses() {
+    const activeIconClassNames = ccAlert[`${this.variant}Icon`];
+
+    return classNames({
+      [ccAlert.icon]: true,
+      [activeIconClassNames]: true,
+    });
   }
 
   // Slotted elements remain in lightDOM which allows for control of their style outside of shadowDOM.
   // ::slotted([Simple Selector]) confirms to Specificity rules, but (being simple) does not add weight to lightDOM skin selectors,
   // so never gets higher Specificity. Thus in order to overwrite style linked within shadowDOM, we need to use !important.
   // https://stackoverflow.com/a/61631668
-  static styles = 
-    css`
-      /* @unocss-placeholder */
-      :host {
-        display: block;
-      }
-      ::slotted(:last-child) {
-        margin-bottom: 0px !important;
-      }
-    `;
+  static styles = css`
+    /* @unocss-placeholder */
+    :host {
+      display: block;
+    }
+    ::slotted(:first-child) {
+      margin-top: 0px;
+    }
+    ::slotted(:last-child) {
+      margin-bottom: 0px !important;
+    }
+  `;
 
-  get _style() {
-    return this.variant ? variants[this.variant] : {};
+  get _icon() {
+    return this.variant ? icons[this.variant] : {};
   }
 
   render() {
-    const { color, icon, bgColor, borderColor, textColor } = this._style;
-
     return html`
       <w-expand-transition ?show=${this.show}>
-        <div
-          role=${this.role}
-          class="${`flex p-16 border rounded-4 border-l-4 ${bgColor} ${borderColor}`}"
-          style="border-left-color:var(--w-${color}-600)"
-        >
-          <div class="mr-8 ${textColor}">${icon}</div>
-          <div class="text-14">
+        <div role=${this.role} class=${this._wrapperClasses}>
+          <div class=${this._iconClasses}>${this._icon}</div>
+          <div class="last:mb-0 text-14">
             <slot></slot>
           </div>
         </div>
