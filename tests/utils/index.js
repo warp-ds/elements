1fcc72ac6f34c2e15347cf1fb1335b06a88c9fb2
Silje Christensen <silje.christensen@cognite.com>
2023-08-11 12:29:30 +0200

    chore: update eik urls in test/utils/index.js and vite.config.js

--- tests/utils/index.js
+++ tests/utils/index.js
@@ -2,8 +2,9 @@ export async function addContentToPage({ page, content }) {
   let updatedPage = page;
   await page.setContent(content);
   await page.addScriptTag({ path: './dist/index.js', type: 'module' });
-  await page.addStyleTag({ url: 'https://assets.finn.no/pkg/@warp-ds/css/1.0.0-alpha.33/tokens/finn-no.css' });
-  await page.addStyleTag({ url: 'https://assets.finn.no/pkg/@warp-ds/css/1.0.0-alpha.33/resets.min.css' });
+  await page.addStyleTag({ url: 'https://assets.finn.no/pkg/@warp-ds/fonts/v1/finn-no.css' });
+  await page.addStyleTag({ url: 'https://assets.finn.no/pkg/@warp-ds/css/1.0.0-alpha.37/tokens/finn-no.css' });
+  await page.addStyleTag({ url: 'https://assets.finn.no/pkg/@warp-ds/css/1.0.0-alpha.37/resets.css' });
 
   return updatedPage;
 }
