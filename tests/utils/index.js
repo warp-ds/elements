c38022ec8b485ddff85597cd1566325b26d82ecb
BalbinaK <balbuhhha@gmail.com>
2023-08-29 14:40:00 +0200

    fix: replace exact version number with v1 alias in eik urls

--- tests/utils/index.js
+++ tests/utils/index.js
@@ -3,8 +3,8 @@ export async function addContentToPage({ page, content }) {
   await page.setContent(content);
   await page.addScriptTag({ path: './dist/index.js', type: 'module' });
   await page.addStyleTag({ url: 'https://assets.finn.no/pkg/@warp-ds/fonts/v1/finn-no.css' });
-  await page.addStyleTag({ url: 'https://assets.finn.no/pkg/@warp-ds/css/1.0.0-alpha.37/tokens/finn-no.css' });
-  await page.addStyleTag({ url: 'https://assets.finn.no/pkg/@warp-ds/css/1.0.0-alpha.37/resets.css' });
+  await page.addStyleTag({ url: 'https://assets.finn.no/pkg/@warp-ds/css/v1/tokens/finn-no.css' });
+  await page.addStyleTag({ url: 'https://assets.finn.no/pkg/@warp-ds/css/v1/resets.css' });
 
   return updatedPage;
 }
