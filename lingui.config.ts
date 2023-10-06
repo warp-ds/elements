1735094e665f34f157e1339d3fe61a07d1ba3ba7
Silje Christensen <siljec@users.noreply.github.com>
2023-10-06 13:23:45 +0200

    feat(i18n): add i18n support (#61)
    * feat(i18n): add i18n support
    
    * fix: install @types/node to fix tsc issues on build
    
    * ci: validate extracted messages when opening/editing PRs
    
    ---------
    
    Co-authored-by: Snorre Nygaard <snorre.nygaard@finn.no>
    Co-authored-by: BalbinaK <balbuhhha@gmail.com>

---
+++ lingui.config.ts
@@ -0,0 +1,24 @@
+import type { LinguiConfig } from '@lingui/conf';
+import { supportedLocales } from './packages/i18n';
+
+const config: LinguiConfig = {
+  locales: supportedLocales as unknown as string[],
+  catalogs: [
+    {
+      include: ['packages/breadcrumbs/index.js'],
+      path: 'packages/breadcrumbs/locales/{locale}/messages',
+    },
+    {
+      include: ['packages/button/index.js'],
+      path: 'packages/button/locales/{locale}/messages',
+    },
+    {
+      include: ['packages/select/index.js'],
+      path: 'packages/select/locales/{locale}/messages',
+    },
+  ],
+  compileNamespace: 'es',
+  sourceLocale: 'en',
+};
+
+export default config;
