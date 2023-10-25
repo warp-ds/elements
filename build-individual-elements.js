0021d6c586c38079926280ea48b68eccba45eebe
imprashast <imprashast@gmail.com>
2023-10-25 12:59:59 +0200

    some cleanup

--- build-individual-elements.js
+++ build-individual-elements.js
@@ -1,8 +1,4 @@
-import { build } from "vite";
 import esbuild from "esbuild";
-import { classes } from '@warp-ds/css/component-classes/classes';
-import { presetWarp } from '@warp-ds/uno';
-import uno from 'unocss/vite';
 import pkg from "glob";
 const { glob } = pkg;
 
@@ -14,7 +10,6 @@ imports.forEach(async (item) => {
 
   if (item.includes("utils")) return;
 
-  // could use esbuild instead of vite but since we are using unocss plugin, vite is a better choice
   try {
     await esbuild.build({
       entryPoints: [item],
@@ -29,29 +24,4 @@ imports.forEach(async (item) => {
   } catch (err) {
     console.error(err);
   }
-
-  // await build({
-  //   configFile: false,
-  //   plugins: [
-  //     uno({
-  //       presets: [presetWarp()],
-  //       mode: 'shadow-dom',
-  //       safelist: classes,
-  //     })
-  //   ],
-  //   build: {
-  //     emptyOutDir: false,
-  //     lib: {
-  //       formats: ["es"],
-  //       entry: item,
-  //       fileName: `packages/${match[1]}/index`,
-  //     },
-  //     rollupOptions: {
-  //       external: ["lit", /^lit\/.*/],
-  //       output: {
-  //         globals: { lit: "lit" },
-  //       },
-  //     },
-  //   },
-  // });
 });
