93ae4309d3d0782960913d539a25a09b2e6e5279
imprashast <imprashast@users.noreply.github.com>
2023-10-26 14:36:24 +0000

    Deploy preview for PR 93 🛫

---
+++ pr-preview/pr-93/assets/card-c025bee9.js
@@ -0,0 +1,1 @@
+import"./sidebar-element-cc971a5f.js";window.addEventListener("load",()=>{document.querySelector("#property-example w-card").selected=!0});
---
+++ pr-preview/pr-93/assets/index-4962bdb2.js
@@ -0,0 +1,1 @@
+import"./sidebar-element-cc971a5f.js";import"./main-a2e0840b.js";
