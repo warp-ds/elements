ff5e48ede1217bd1594367a1f3b8845e0b2b7c86
magnuh <magnuh@users.noreply.github.com>
2023-10-31 08:24:29 +0000

    Deploy preview for PR 86 🛫

---
+++ pr-preview/pr-86/assets/card-a27108f9.js
@@ -0,0 +1,1 @@
+import"./sidebar-element-829dd623.js";window.addEventListener("load",()=>{document.querySelector("#property-example w-card").selected=!0});
---
+++ pr-preview/pr-86/assets/expandable-899c11b2.js
@@ -0,0 +1,1 @@
+import"./sidebar-element-829dd623.js";
