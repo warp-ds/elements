d24c8ef36205be67ac67839902ba26f594b0e612
felicia-haggqvist <felicia-haggqvist@users.noreply.github.com>
2023-11-02 12:22:32 +0000

    Deploy preview for PR 103 🛫

--- pr-preview/pr-103/assets/card-98c402ec.js
+++
@@ -1,1 +0,0 @@
-import"./sidebar-element-2efe4b2e.js";window.addEventListener("load",()=>{document.querySelector("#property-example w-card").selected=!0});
--- pr-preview/pr-103/assets/index-0189daa8.js
+++
@@ -1,1 +0,0 @@
-import"./sidebar-element-2efe4b2e.js";import"./main-7b1c629e.js";
