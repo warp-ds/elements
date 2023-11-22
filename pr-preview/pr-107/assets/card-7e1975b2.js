a03a9c2eb09b83c4f31839ae9e4f76a9626f4b7f
imprashast <imprashast@gmail.com>
2023-11-22 13:26:43 +0000

    Deploying to gh-pages from @ warp-ds/elements@af3ff4f7ba55e7007d39ca8179f5dcbac82c3698 🚀

--- pr-preview/pr-107/assets/card-7e1975b2.js
+++
@@ -1,1 +0,0 @@
-import"./sidebar-element-563db57c.js";window.addEventListener("load",()=>{document.querySelector("#property-example w-card").selected=!0});
--- pr-preview/pr-79/assets/card-aea2dc6f.js
+++
@@ -1,1 +0,0 @@
-import"./sidebar-element-3dcd30cc.js";window.addEventListener("load",()=>{document.querySelector("#property-example w-card").selected=!0});
