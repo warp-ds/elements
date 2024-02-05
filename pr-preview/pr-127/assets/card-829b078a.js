c9f8332efbe4ccf57defc695164a2a98ff70bba0
pearofducks <pearofducks@users.noreply.github.com>
2024-02-05 10:12:02 +0000

    Deploy preview for PR 127 🛫

---
+++ pr-preview/pr-127/assets/card-829b078a.js
@@ -0,0 +1,1 @@
+import"./sidebar-element-7b4aae4d.js";window.addEventListener("load",()=>{document.querySelector("#property-example w-card").selected=!0});
---
+++ pr-preview/pr-127/assets/pages-components-eb2502bf.js
@@ -0,0 +1,1 @@
+import"./sidebar-element-7b4aae4d.js";import"./expandable-41bc074c.js";
