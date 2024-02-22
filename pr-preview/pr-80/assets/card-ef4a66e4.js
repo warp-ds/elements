c33aa8b09837ecaaca3e2c46a87f30adaea81c71
BalbinaK <BalbinaK@users.noreply.github.com>
2024-02-22 12:04:52 +0000

    Remove preview for PR 80 🛬

--- pr-preview/pr-80/assets/card-ef4a66e4.js
+++
@@ -1,1 +0,0 @@
-import"./sidebar-element-f18c7bab.js";window.addEventListener("load",()=>{document.querySelector("#property-example w-card").selected=!0});
--- pr-preview/pr-80/assets/index-d362c23b.js
+++
@@ -1,1 +0,0 @@
-import"./sidebar-element-f18c7bab.js";import"./main-b20772de.js";
