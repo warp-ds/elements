b70de10c66688958c729176bdc295e05e85e67f7
BalbinaK <BalbinaK@users.noreply.github.com>
2024-02-22 11:59:00 +0000

    Deploy preview for PR 80 🛫

--- pr-preview/pr-80/assets/card-8f67f421.js
+++
@@ -1,1 +0,0 @@
-import"./sidebar-element-e35f82c8.js";window.addEventListener("load",()=>{document.querySelector("#property-example w-card").selected=!0});
--- pr-preview/pr-80/assets/index-96e9c764.js
+++
@@ -1,1 +0,0 @@
-import"./sidebar-element-e35f82c8.js";import"./main-9e9be605.js";
