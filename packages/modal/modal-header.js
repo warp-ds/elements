5d71819ddc12bb6bd104972665c1f9b7d5625289
Dave Honneffer <pearofducks@gmail.com>
2024-03-14 10:48:57 +0100

    Cleanup and add no-close feature

--- packages/modal/modal-header.js
+++ packages/modal/modal-header.js
@@ -5,12 +5,15 @@ import { createRef, ref } from 'lit/directives/ref.js'
 import { modalV2 as cc } from './component-classes.js'
 import { Move } from '@itsy/animate/move'
 
+const NO_CLOSE_BUTTON = 'no-close'
+
 export class ModalHeader extends CanCloseMixin(WarpElement) {
   titleEl = createRef()
 
   static properties = {
     title: { type: String },
     back: { type: Boolean },
+    [NO_CLOSE_BUTTON]: { type: Boolean },
     _hasTopContent: { state: true },
   }
   constructor() {
@@ -24,7 +27,7 @@ export class ModalHeader extends CanCloseMixin(WarpElement) {
         <div class="${!this._hasTopContent && cc.titleBarSlot}">
           ${this.backButton}
           <h1 ${ref(this.titleEl)} class="${this.titleClasses}">${this.title}</h1>
-          ${this._hasTopContent ? this.topCloseButton : this.normalCloseButton}
+          ${this.closeButton}
         </div>
       </div>
     `
@@ -46,24 +49,25 @@ export class ModalHeader extends CanCloseMixin(WarpElement) {
   }
   get backButton() {
     return this.back ? html`
-      <button class="${cc.titleButton} ${cc.titleBarButton} ${cc.titleButtonLeft}" @click="${this.close}">
+      <button class="${cc.titleButton} ${cc.titleBarButton} ${cc.titleButtonLeft}" @click="${this.emitBack}">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 16 16" class="s-icon"><title>Leftward-pointing arrow</title><path stroke="currentColor" stroke-linecap="round" stroke-width="1.5" d="M5.222 8h6.667"></path><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M7.444 11.75 4.111 8l3.333-3.75"></path></svg>
       </button>`
     : nothing
   }
-  // TODO: wire up a11y and i18n
-  get normalCloseButton() {
+  get closeButton() {
+    if (this[NO_CLOSE_BUTTON]) return nothing
+    const classes = [
+      cc.titleButton,
+      cc.titleButtonRight,
+      this._hasTopContent ? cc.titleCloseButton : cc.titleBarButton
+    ].join(' ')
     return html`
-      <button class="${cc.titleButton} ${cc.titleBarButton} ${cc.titleButtonRight}" @click="${this.close}">
+      <button class="${classes}" @click="${this.close}">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 16 16"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m12.5 3.5-9 9M3.5 3.5l9 9"></path></svg>
       </button>`
   }
-  // TODO: wire up a11y and i18n
-  get topCloseButton() {
-    return html`
-      <button class="${cc.titleButton} ${cc.titleCloseButton} ${cc.titleButtonRight} new-pill" @click="${this.close}">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 16 16"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m12.5 3.5-9 9M3.5 3.5l9 9"></path></svg>
-      </button>`
+  emitBack() {
+    this.dispatchEvent(new CustomEvent('backClicked', { bubbles: true, composed: true }))
   }
   handleTopSlotChange(slotEvent) {
     const topContent = slotEvent.target.assignedElements({ flatten: true })
