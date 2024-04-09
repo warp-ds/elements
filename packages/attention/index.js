49f63a3a1c86fed8eaa8d8b9b245775230e9aa75
felicia-haggqvist <felicia.haggqvist@schibsted.com>
2024-04-09 10:38:16 +0200

    fix: lint issues

--- packages/attention/index.js
+++ packages/attention/index.js
@@ -76,33 +76,31 @@ class WarpAttention extends kebabCaseAttributes(WarpElement) {
     this._initialPlacement = this.placement;
     this._actualDirection = this.placement;
   }
-  
+
   connectedCallback() {
     super.connectedCallback();
     if (this.placement && !Object.keys(opposites).includes(this.placement)) {
-      throw new Error(`Invalid "placement" attribute. Set its value to one of the following:\n${JSON.stringify(Object.keys(opposites))}`
-      );
-    };
-    
-    if (this.fallbackPlacements && !this.fallbackPlacements.every(fallbackPlacement => directions.includes(fallbackPlacement))) {
-      throw new Error(`Invalid "fallbackPlacements" attribute. Set its value to an array with one or more of the following:\n${JSON.stringify(directions)}`
-      );
-    };
+      throw new Error(`Invalid "placement" attribute. Set its value to one of the following:\n${JSON.stringify(Object.keys(opposites))}`);
+    }
+
+    if (this.fallbackPlacements && !this.fallbackPlacements.every((fallbackPlacement) => directions.includes(fallbackPlacement))) {
+      throw new Error(`Invalid "fallbackPlacements" attribute. Set its value to an array with one or more of the following:\n${JSON.stringify(directions)}`);
+    }
 
     // Fix FOUC effect issues
     setTimeout(() => this.requestUpdate(), 0);
-      if (!this.callout) {
-        window.addEventListener('click', this.handleDone);
-        window.addEventListener('scroll', this.handleDone);
-        window.addEventListener('resize', this.handleDone);
-        window.addEventListener('touch', this.handleDone);
-      }
-      
-      if (this.tooltip) {
-        window.addEventListener('mouseover', this.handleDone)
-        window.addEventListener('mouseout', this.handleDone)
-      }
-  };
+    if (!this.callout) {
+      window.addEventListener('click', this.handleDone);
+      window.addEventListener('scroll', this.handleDone);
+      window.addEventListener('resize', this.handleDone);
+      window.addEventListener('touch', this.handleDone);
+    }
+
+    if (this.tooltip) {
+      window.addEventListener('mouseover', this.handleDone);
+      window.addEventListener('mouseout', this.handleDone);
+    }
+  }
 
   disconnectedCallback() {
     window.removeEventListener('click', this.handleDone);
@@ -113,19 +111,18 @@ class WarpAttention extends kebabCaseAttributes(WarpElement) {
     window.removeEventListener('mouseout', this.handleDone);
 
     super.disconnectedCallback();
-  };
+  }
 
-   handleDone() {
-     window.requestAnimationFrame(() => {
-    if(this.show && this._targetEl && this._attentionEl) {
+  handleDone() {
+    window.requestAnimationFrame(() => {
+      if (this.show && this._targetEl && this._attentionEl) {
         recompute(this.attentionState).then((state) => {
-          this._actualDirection = state?.actualDirection
-        })
-      } 
-      else {
-        this._actualDirection = this._initialPlacement
+          this._actualDirection = state?.actualDirection;
+        });
+      } else {
+        this._actualDirection = this._initialPlacement;
       }
-    })
+    });
   }
 
   get _actualDirection() {
@@ -133,33 +130,27 @@ class WarpAttention extends kebabCaseAttributes(WarpElement) {
   }
 
   set _actualDirection(v) {
-      this.placement = v
+    this.placement = v;
   }
 
   get _arrowEl() {
-    return this.renderRoot.querySelector('#arrow')
+    return this.renderRoot.querySelector('#arrow');
   }
 
   get _arrowDirection() {
-    return opposites[this._actualDirection]
+    return opposites[this._actualDirection];
   }
 
   get _arrowClasses() {
     return classes({
       [ccAttention.arrowBase]: true,
       [ccAttention[`arrowDirection${arrowDirectionClassname(this._arrowDirection)}`]]: true,
-      [this._activeVariantClasses.arrow]: true
+      [this._activeVariantClasses.arrow]: true,
     });
   }
 
   get _arrowHtml() {
-    return this.noArrow
-      ? ''
-      : html`<div
-          id="arrow"
-          role="img"
-          class="${this._arrowClasses}"
-        />`
+    return this.noArrow ? '' : html`<div id="arrow" role="img" class="${this._arrowClasses}" />`;
   }
 
   get _activeVariantClasses() {
@@ -167,20 +158,19 @@ class WarpAttention extends kebabCaseAttributes(WarpElement) {
       callout: this.callout,
       popover: this.popover,
       tooltip: this.tooltip,
-      highlight: this.highlight
-    }
+      highlight: this.highlight,
+    };
 
-    const activeVariant = Object.keys(variantProps).find(b => !!variantProps[b]) || '';
+    const activeVariant = Object.keys(variantProps).find((b) => !!variantProps[b]) || '';
 
     return {
       wrapper: ccAttention[activeVariant],
-      arrow: ccAttention[`arrow${activeVariant.charAt(0).toUpperCase() + activeVariant.slice(1)}`]
-    }
-  };
+      arrow: ccAttention[`arrow${activeVariant.charAt(0).toUpperCase() + activeVariant.slice(1)}`],
+    };
+  }
 
   get _attentionEl() {
-    return this.renderRoot
-    .querySelector('#attention')
+    return this.renderRoot.querySelector('#attention');
   }
 
   get _targetEl() {
@@ -189,14 +179,14 @@ class WarpAttention extends kebabCaseAttributes(WarpElement) {
   }
 
   get _messageEl() {
-    const messageSlot = this.renderRoot.querySelector("slot[name='message']")
+    const messageSlot = this.renderRoot.querySelector("slot[name='message']");
     return messageSlot ? messageSlot.assignedNodes()[0] : null;
   }
 
   get _wrapperClasses() {
     return classes({
       [ccAttention.base]: true,
-      [this._activeVariantClasses.wrapper]: true
+      [this._activeVariantClasses.wrapper]: true,
     });
   }
 
@@ -204,58 +194,46 @@ class WarpAttention extends kebabCaseAttributes(WarpElement) {
     return i18n._({
       id: 'attention.aria.close',
       message: 'Close',
-      comment:
-        'Aria label for the close button in attention',
-    })
+      comment: 'Aria label for the close button in attention',
+    });
   }
-  
+
   get _closeBtnHtml() {
     return html`
-      <button 
-        aria-label="${this._ariaClose}"
-        @click="${this.close}"
-        @keydown=${this.keypressed}
-        class="${ccAttention.closeBtn}"
-      >
+      <button aria-label="${this._ariaClose}" @click="${this.close}" @keydown=${this.keypressed} class="${ccAttention.closeBtn}">
         <w-icon-close-16 />
       </button>
-    `
+    `;
   }
 
-  updated () {
-      if (!this.callout) {
-        this._attentionEl.style.setProperty(
-          '--attention-visibility',
-          this.show ? '' : 'hidden'
-        )
-      }
-  
-      if (!this.tooltip) {
-        this._attentionEl.style.setProperty(
-          '--attention-display',
-          this.show ? 'flex' : 'none'
-        )
-      }
-      
-      this.attentionState = {
-        isShowing: this.show,
-        isCallout: this.callout,
-        actualDirection: this._actualDirection,
-        directionName: this.placement, 
-        arrowEl: this._arrowEl,
-        attentionEl: this._attentionEl,
-        targetEl: this._targetEl,
-        noArrow: this.noArrow,
-        distance: this.distance,
-        skidding: this.skidding,
-        flip: this.flip,
-        fallbackPlacements: this.fallbackPlacements
-      }
-
-      // We need to recompute here as well if this._actualDirection gets updated immediately when this.show is true (in this.handleDone()).
-      // Otherwise this._arrowDirection will get this._initialPlacement's value and will only be updated on next click/scroll/resize
-      recompute(this.attentionState)
+  updated() {
+    if (!this.callout) {
+      this._attentionEl.style.setProperty('--attention-visibility', this.show ? '' : 'hidden');
     }
+
+    if (!this.tooltip) {
+      this._attentionEl.style.setProperty('--attention-display', this.show ? 'flex' : 'none');
+    }
+
+    this.attentionState = {
+      isShowing: this.show,
+      isCallout: this.callout,
+      actualDirection: this._actualDirection,
+      directionName: this.placement,
+      arrowEl: this._arrowEl,
+      attentionEl: this._attentionEl,
+      targetEl: this._targetEl,
+      noArrow: this.noArrow,
+      distance: this.distance,
+      skidding: this.skidding,
+      flip: this.flip,
+      fallbackPlacements: this.fallbackPlacements,
+    };
+
+    // We need to recompute here as well if this._actualDirection gets updated immediately when this.show is true (in this.handleDone()).
+    // Otherwise this._arrowDirection will get this._initialPlacement's value and will only be updated on next click/scroll/resize
+    recompute(this.attentionState);
+  }
 
   pointingAtDirection() {
     switch (opposites[this._actualDirection]) {
@@ -265,27 +243,24 @@ class WarpAttention extends kebabCaseAttributes(WarpElement) {
         return i18n._({
           id: 'attention.aria.pointingUp',
           message: 'pointing up',
-          comment:
-            'Default screenreader message for top direction in the attention component',
-        })
+          comment: 'Default screenreader message for top direction in the attention component',
+        });
       case 'right-start':
       case 'right':
       case 'right-end':
         return i18n._({
           id: 'attention.aria.pointingRight',
           message: 'pointing right',
-          comment:
-            'Default screenreader message for right direction in the attention component',
-        })
+          comment: 'Default screenreader message for right direction in the attention component',
+        });
       case 'bottom-start':
       case 'bottom':
       case 'bottom-end':
         return i18n._({
           id: 'attention.aria.pointingDown',
           message: 'pointing down',
-          comment:
-            'Default screenreader message for bottom direction in the attention component',
-        })
+          comment: 'Default screenreader message for bottom direction in the attention component',
+        });
       case 'left-start':
       case 'left':
       case 'left-end':
@@ -339,18 +314,17 @@ class WarpAttention extends kebabCaseAttributes(WarpElement) {
       this._targetEl.setAttribute('aria-details', attentionMessageId);
     }
   }
-  
 
   firstUpdated() {
-    this._initialPlacement = this.placement
-    this.setAriaLabels()
+    this._initialPlacement = this.placement;
+    this.setAriaLabels();
 
     // Attention of "callout" type should always be used inline
     if (this.callout) {
       this._attentionEl.style.position = 'relative';
     }
   }
-  
+
   close() {
     const event = new CustomEvent('close', {
       bubbles: true,
@@ -367,7 +341,6 @@ class WarpAttention extends kebabCaseAttributes(WarpElement) {
     }
   }
 
-  
   render() {
     return html`
       <div class=${ifDefined(this.className ? this.className : undefined)}>
