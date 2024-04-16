8265ba13fc7c43b1013f7c4c317898eb961343a1
felicia-haggqvist <felicia-haggqvist@users.noreply.github.com>
2024-04-16 12:37:00 +0000

    Deploy preview for PR 135 🛫

---
+++ pr-preview/pr-135/assets/card-DtA0kQET.js
@@ -0,0 +1,1 @@
+import"./sidebar-element-CsAcA3QQ.js";window.addEventListener("load",()=>{document.querySelector("#property-example w-card").selected=!0});
---
+++ pr-preview/pr-135/assets/index-DYsgjAZu.js
@@ -0,0 +1,1 @@
+import"./sidebar-element-CsAcA3QQ.js";import"./main-D5ovdCXf.js";
