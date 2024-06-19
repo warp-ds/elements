ca96a947c4ed67113ec122508cd26709c334f539
BalbinaK <balbuhhha@gmail.com>
2024-06-19 10:01:07 +0200

    Replace svgs with Elements icons

--- packages/modal/modal-header.js
+++ packages/modal/modal-header.js
@@ -2,6 +2,8 @@ import { html, css, nothing } from 'lit';
 
 import { Move } from '@itsy/animate/move';
 import { modalElement as ccModal } from '@warp-ds/css/component-classes';
+import '@warp-ds/icons/elements/arrow-left-16';
+import '@warp-ds/icons/elements/close-16';
 import WarpElement from '@warp-ds/elements-core';
 import { createRef, ref } from 'lit/directives/ref.js';
 
@@ -54,16 +56,7 @@ export class ModalHeader extends CanCloseMixin(WarpElement) {
   get backButton() {
     return this.back && !this._hasTopContent // Not showing back button when there is a top image
       ? html` <button type="button" class="${ccModal.headerButton} ${ccModal.headerButtonLeft}" @click="${this.emitBack}">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 16 16">
-            <title>Leftward-pointing arrow</title>
-            <path stroke="currentColor" stroke-linecap="round" stroke-width="1.5" d="M5.222 8h6.667"></path>
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="1.5"
-              d="M7.444 11.75 4.111 8l3.333-3.75"></path>
-          </svg>
+          <w-icon-arrow-left-16></w-icon-arrow-left-16>
         </button>`
       : nothing;
   }
@@ -73,9 +66,7 @@ export class ModalHeader extends CanCloseMixin(WarpElement) {
       type="button"
       class="${ccModal.headerButton} ${this._hasTopContent ? ccModal.headerCloseButtonOnImage : ccModal.headerCloseButton}"
       @click="${this.close}">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 16 16">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m12.5 3.5-9 9M3.5 3.5l9 9"></path>
-      </svg>
+      <w-icon-close-16></w-icon-close-16>
     </button>`;
   }
   emitBack() {
