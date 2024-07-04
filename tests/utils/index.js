8aca5ffff4368cc3b0c397257fcc0e7b4e297e9d
imprashast <imprashast@gmail.com>
2024-07-04 10:45:24 +0200

    more v2

--- tests/utils/index.js
+++ tests/utils/index.js
@@ -3,8 +3,8 @@ export async function addContentToPage({ page, content }) {
   await page.setContent(content);
   await page.addScriptTag({ path: './dist/index.js', type: 'module' });
   await page.addStyleTag({ url: 'https://assets.finn.no/pkg/@warp-ds/fonts/v1/finn-no.css' });
-  await page.addStyleTag({ url: 'https://assets.finn.no/pkg/@warp-ds/css/v1/tokens/finn-no.css' });
-  await page.addStyleTag({ url: 'https://assets.finn.no/pkg/@warp-ds/css/v1/resets.css' });
+  await page.addStyleTag({ url: 'https://assets.finn.no/pkg/@warp-ds/css/v2/tokens/finn-no.css' });
+  await page.addStyleTag({ url: 'https://assets.finn.no/pkg/@warp-ds/css/v2/resets.css' });
 
   return updatedPage;
 }
