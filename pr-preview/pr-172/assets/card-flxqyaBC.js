6510ddd8e3098aa2ee71e3df56aa8ab189bbdcd1
felicia-haggqvist <felicia-haggqvist@users.noreply.github.com>
2024-07-29 08:44:06 +0000

    Deploy preview for PR 172 🛫

---
+++ pr-preview/pr-172/assets/card-flxqyaBC.js
@@ -0,0 +1,1 @@
+import"./sidebar-element-BRy6nOrY.js";window.addEventListener("load",()=>{document.querySelector("#property-example w-card").selected=!0});
---
+++ pr-preview/pr-172/assets/pages-components-CvGwUdxG.js
@@ -0,0 +1,1 @@
+import"./sidebar-element-BRy6nOrY.js";import"./expandable-hQuWYafU.js";
