2b49eff0f6acb161c2fdc2a6b8a26024884b4d51
BalbinaK <BalbinaK@users.noreply.github.com>
2024-08-15 10:10:05 +0000

    Deploy preview for PR 182 🛫

---
+++ pr-preview/pr-182/assets/card-zwmm-MR6.js
@@ -0,0 +1,1 @@
+import"./sidebar-element-BjIu_R34.js";window.addEventListener("load",()=>{document.querySelector("#property-example w-card").selected=!0});
---
+++ pr-preview/pr-182/assets/index-BaCiot6t.js
@@ -0,0 +1,1 @@
+import"./sidebar-element-BjIu_R34.js";import"./main-Cx3-QuP0.js";
