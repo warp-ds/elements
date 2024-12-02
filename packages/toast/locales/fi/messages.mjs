6d1a3d5aa52d8c69e7b5e6e7aa5fe6ba4824b2cf
felicia-haggqvist <144792260+felicia-haggqvist@users.noreply.github.com>
2024-12-02 11:51:42 +0100

    feat(lingui): add support for Swedish ('sv') translations (#197)
    * feat(lingui): add support for Swedish ('sv') translations
    
    * feat: add mjs-files to gitignore
    
    * feat: remove already committed messages.mjs-files
    
    * fix: linting issue
    
    * feat: add linguie compile to build:docs script
    
    * feat: add && to build:docs script
    
    * feat: add lingui compile to build script
    
    * feat: add postinstall script for lingui compile

--- packages/toast/locales/fi/messages.mjs
+++
@@ -1,1 +0,0 @@
-/*eslint-disable*/export const messages=JSON.parse("{\"toast.aria.error\":\"Virhe\",\"toast.aria.successful\":\"Onnistui\",\"toast.aria.warning\":\"Varoitus\"}");
--- packages/toast/locales/nb/messages.mjs
+++
@@ -1,1 +0,0 @@
-/*eslint-disable*/export const messages=JSON.parse("{\"toast.aria.error\":\"Feil\",\"toast.aria.successful\":\"Vellykket\",\"toast.aria.warning\":\"Advarsel\"}");
