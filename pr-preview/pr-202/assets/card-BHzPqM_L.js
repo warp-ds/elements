d1f22b8af5fff7b4d48a9c7556d2304c9f2944de
wkillerud <wkillerud@users.noreply.github.com>
2024-12-17 13:37:50 +0000

    Deploy preview for PR 202 🛫

--- pr-preview/pr-202/assets/card-BHzPqM_L.js
+++
@@ -1,1 +0,0 @@
-import"./sidebar-element-Q5faqbi3.js";window.addEventListener("load",()=>{document.querySelector("#property-example w-card").selected=!0});
---
+++ pr-preview/pr-202/assets/index-DoXLfD9r.js
@@ -0,0 +1,1 @@
+import"./sidebar-element-Dm6PnReB.js";import"./main-DPs4nVYP.js";
