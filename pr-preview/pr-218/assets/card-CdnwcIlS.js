902df3e51f87db78c52801fb226ac6c71c03412b
flatoy <flatoy@users.noreply.github.com>
2025-08-29 09:16:29 +0000

    Deploy preview for PR 218 🛫

---
+++ pr-preview/pr-218/assets/card-CdnwcIlS.js
@@ -0,0 +1,1 @@
+import"./sidebar-element-sSBvK2nN.js";window.addEventListener("load",()=>{document.querySelector("#property-example w-card").selected=!0});
---
+++ pr-preview/pr-218/assets/pages-components-DBDDsNDM.js
@@ -0,0 +1,1 @@
+import"./sidebar-element-sSBvK2nN.js";import"./expandable-DLb2keM4.js";
