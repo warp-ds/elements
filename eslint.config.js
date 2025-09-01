860b6072698cc24bb1c2e889b80b1343d142e2a1
William Killerud <william.killerud@m10s.io>
2025-09-01 13:52:20 +0200

    chore: setup eslint with TS

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import config from "@warp-ds/eslint-config";
+import tseslint from "typescript-eslint";
+
+export default [
+  ...config,
+	...tseslint.configs.recommended,
+  {
+		rules: {
+      // Disabled in the ts variant of imports, but we get it via warp-ds/eslint-config
+      // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/named.md
+			"import/named": "off",
+		},
+	},
+  {
+    ignores: ["site", "dist", "eik", "pages"],
+  },
+];
