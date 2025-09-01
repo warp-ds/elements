547c8fa84e4a44e0974c86a938e300bc94ff4c71
William Killerud <william.killerud@m10s.io>
2025-09-01 14:50:39 +0200

    chore: liiiiint

--- packages/expandable/expandable.test.ts
+++ packages/expandable/expandable.test.ts
@@ -1,10 +1,10 @@
-import { html } from "lit";
+import { html } from 'lit';
 
-import { expect, test } from "vitest";
-import { render } from "vitest-browser-lit";
-import "./index.js";
+import { expect, test } from 'vitest';
+import { render } from 'vitest-browser-lit';
+import './index.js';
 
-test("renders the given title prop and hides the slotted content", async () => {
+test('renders the given title prop and hides the slotted content', async () => {
   const component = html`
     <w-expandable title="I'm expandable">
       <p>with expanded content</p>
@@ -13,10 +13,10 @@ test("renders the given title prop and hides the slotted content", async () => {
 
   const screen = render(component);
   await expect.element(screen.getByText("I'm expandable")).toBeVisible();
-  await expect.element(screen.getByText("with expanded content")).not.toBeVisible();
+  await expect.element(screen.getByText('with expanded content')).not.toBeVisible();
 });
 
-test("clicking the expandable shows the slotted content", async () => {
+test('clicking the expandable shows the slotted content', async () => {
   const component = html`
     <w-expandable title="I'm expandable">
       <p>with expanded content</p>
@@ -24,6 +24,6 @@ test("clicking the expandable shows the slotted content", async () => {
   `;
 
   const screen = render(component);
-  await screen.getByRole("button").click();
-  await expect.element(screen.getByText("with expanded content")).toBeVisible();
+  await screen.getByRole('button').click();
+  await expect.element(screen.getByText('with expanded content')).toBeVisible();
 });
--- packages/expandable/index.ts
+++ packages/expandable/index.ts
@@ -1,17 +1,13 @@
-import { css, html, LitElement, PropertyValues } from "lit";
+import { css, html, LitElement, PropertyValues } from 'lit';
 
-import { classNames } from "@chbphone55/classnames";
-import {
-  box as ccBox,
-  expandable as ccExpandable,
-} from "@warp-ds/css/component-classes";
-import { ifDefined } from "lit/directives/if-defined.js";
+import { classNames } from '@chbphone55/classnames';
+import { box as ccBox, expandable as ccExpandable } from '@warp-ds/css/component-classes';
+import { property } from 'lit/decorators.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
+import '@warp-ds/icons/elements/chevron-down-16';
+import '@warp-ds/icons/elements/chevron-up-16';
 
-import "@warp-ds/icons/elements/chevron-down-16";
-import "@warp-ds/icons/elements/chevron-up-16";
-import { property } from "lit/decorators.js";
-
-import { components, reset } from "../styles.js";
+import { components, reset } from '../styles.js';
 
 class WarpExpandable extends LitElement {
   @property({ type: Boolean, reflect: true })
@@ -26,19 +22,19 @@ class WarpExpandable extends LitElement {
   @property({ type: Boolean })
   bleed: boolean = false;
 
-  @property({ attribute: "button-class", type: String })
+  @property({ attribute: 'button-class', type: String })
   buttonClass: string;
 
-  @property({ attribute: "content-class", type: String })
+  @property({ attribute: 'content-class', type: String })
   contentClass: string;
 
-  @property({ attribute: "no-chevron", type: Boolean })
+  @property({ attribute: 'no-chevron', type: Boolean })
   noChevron: boolean = false;
 
   @property({ type: Boolean })
   animated: boolean = false;
 
-  @property({ attribute: "heading-level", type: Number })
+  @property({ attribute: 'heading-level', type: Number })
   headingLevel: number;
 
   @property({ type: Boolean, state: true })
@@ -66,7 +62,7 @@ class WarpExpandable extends LitElement {
 
   updated(changedProperties: PropertyValues<this>) {
     // We need a slight delay for the animation since it has a transition-duration of 150ms:
-    if (changedProperties.has("expanded")) {
+    if (changedProperties.has('expanded')) {
       setTimeout(() => {
         this._showChevronUp = this.expanded;
       }, 200);
@@ -75,69 +71,44 @@ class WarpExpandable extends LitElement {
 
   firstUpdated() {
     const hasTitleProp = Boolean(this.title);
-    const hasTitleSlot =
-      (
-        this.renderRoot.querySelector("slot[name='title']") as HTMLSlotElement
-      )?.assignedNodes().length > 0;
+    const hasTitleSlot = (this.renderRoot.querySelector("slot[name='title']") as HTMLSlotElement)?.assignedNodes().length > 0;
 
     this._hasTitle = hasTitleProp || hasTitleSlot;
   }
 
   get #wrapperClasses() {
-    return classNames([
-      ccExpandable.wrapper,
-      this.box && ccExpandable.box,
-      this.bleed && ccExpandable.bleed,
-    ]);
+    return classNames([ccExpandable.wrapper, this.box && ccExpandable.box, this.bleed && ccExpandable.bleed]);
   }
 
   get #buttonClasses() {
-    return classNames(this.buttonClass, [
-      ccExpandable.button,
-      this.box && ccExpandable.buttonBox,
-    ]);
+    return classNames(this.buttonClass, [ccExpandable.button, this.box && ccExpandable.buttonBox]);
   }
 
   get #chevronClasses() {
-    return classNames([
-      ccExpandable.chevron,
-      !this.box && ccExpandable.chevronNonBox,
-    ]);
+    return classNames([ccExpandable.chevron, !this.box && ccExpandable.chevronNonBox]);
   }
 
   get #chevronIcon() {
     const upClasses = classNames([
       ccExpandable.elementsChevronUpTransform,
-      !this.expanded &&
-        this._showChevronUp &&
-        ccExpandable.elementsChevronCollapse,
+      !this.expanded && this._showChevronUp && ccExpandable.elementsChevronCollapse,
     ]);
     const downClasses = classNames([
       ccExpandable.elementsChevronDownTransform,
-      this.expanded &&
-        !this._showChevronUp &&
-        ccExpandable.elementsChevronExpand,
+      this.expanded && !this._showChevronUp && ccExpandable.elementsChevronExpand,
     ]);
 
     return this._showChevronUp
       ? html`<w-icon-chevron-up-16 class="${upClasses}"></w-icon-chevron-up-16>`
-      : html`<w-icon-chevron-down-16
-          class="${downClasses}"
-        ></w-icon-chevron-down-16>`;
+      : html`<w-icon-chevron-down-16 class="${downClasses}"></w-icon-chevron-down-16>`;
   }
 
   get #contentClasses() {
-    return classNames(this.contentClass, [
-      this.box && ccBox.base,
-      this._hasTitle && this.box && ccExpandable.contentWithTitle,
-    ]);
+    return classNames(this.contentClass, [this.box && ccBox.base, this._hasTitle && this.box && ccExpandable.contentWithTitle]);
   }
 
   get #expansionClasses() {
-    return classNames([
-      ccExpandable.expansion,
-      !this.expanded && ccExpandable.expansionNotExpanded,
-    ]);
+    return classNames([ccExpandable.expansion, !this.expanded && ccExpandable.expansionNotExpanded]);
   }
 
   get _expandableSlot() {
@@ -154,39 +125,25 @@ class WarpExpandable extends LitElement {
               type="button"
               aria-expanded="${this.expanded}"
               class="${this.#buttonClasses}"
-              @click=${() => (this.expanded = !this.expanded)}
-            >
+              @click=${() => (this.expanded = !this.expanded)}>
               <div class="${ccExpandable.title}">
-                ${this.title
-                  ? html`<span class="${ccExpandable.titleType}"
-                      >${this.title}</span
-                    >`
-                  : html`<slot name="title"></slot>`}
-                ${this.noChevron
-                  ? ""
-                  : html`<div class="${this.#chevronClasses}">
-                      ${this.#chevronIcon}
-                    </div>`}
+                ${this.title ? html`<span class="${ccExpandable.titleType}">${this.title}</span>` : html`<slot name="title"></slot>`}
+                ${this.noChevron ? '' : html`<div class="${this.#chevronClasses}">${this.#chevronIcon}</div>`}
               </div>
             </button>
           </w-unstyled-heading>`
-        : ""}
+        : ''}
       ${this.animated
-        ? html`<w-expand-transition ?show=${this.expanded}>
-            ${this._expandableSlot}
-          </w-expand-transition>`
-        : html`<div
-            class="${this.#expansionClasses}"
-            aria-hidden=${ifDefined(!this.expanded ? true : undefined)}
-          >
+        ? html`<w-expand-transition ?show=${this.expanded}> ${this._expandableSlot} </w-expand-transition>`
+        : html`<div class="${this.#expansionClasses}" aria-hidden=${ifDefined(!this.expanded ? true : undefined)}>
             ${this._expandableSlot}
           </div>`}
     </div>`;
   }
 }
 
-if (!customElements.get("w-expandable")) {
-  customElements.define("w-expandable", WarpExpandable);
+if (!customElements.get('w-expandable')) {
+  customElements.define('w-expandable', WarpExpandable);
 }
 
 export { WarpExpandable };
