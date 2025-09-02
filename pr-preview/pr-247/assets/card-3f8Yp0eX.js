fe8406ab0a90d10f44169fc45ee6b048b9673350
digitalsadhu <digitalsadhu@users.noreply.github.com>
2025-09-02 08:39:36 +0000

    Deploy preview for PR 247 🛫

---
+++ pr-preview/pr-247/assets/card-3f8Yp0eX.js
@@ -0,0 +1,1 @@
+import"./sidebar-element-CxHHI1Ej.js";window.addEventListener("load",()=>{document.querySelector("#property-example w-card").selected=!0});
---
+++ pr-preview/pr-247/assets/pages-components-Rsh4MryE.js
@@ -0,0 +1,1 @@
+import"./sidebar-element-CxHHI1Ej.js";import"./expandable-DqGDRv6r.js";
