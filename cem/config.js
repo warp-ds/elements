aa13b24e0d4513669c00f3152d90f2c06c2ccb05
Richard Walker <digitalsadhu@gmail.com>
2025-09-02 10:38:04 +0200

    chore: add custom element manifest

---
+++ cem/config.js
@@ -0,0 +1,30 @@
+export default {
+  // Globs to analyze
+  globs: ['packages/**/index.ts'],
+
+  // Globs to exclude
+  exclude: [],
+  
+  // Directory to output CEM to
+  outdir: 'dist/',
+  
+  // Run in dev mode, provides extra logging
+  dev: false,
+  
+  // Run in watch mode, runs on file changes
+  watch: false,
+  
+  // Include third party custom elements manifests
+  dependencies: true,
+  
+  // Output CEM path to `package.json`, defaults to true
+  packagejson: true,
+  
+  // Enable special handling for litelement
+  litelement: true,
+
+  // Plugins to use
+  plugins: [
+    
+  ],
+}
