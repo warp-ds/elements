467a8b85ca320bf1c300e31b37e67ff456535cf7
digitalsadhu <digitalsadhu@users.noreply.github.com>
2025-09-02 10:43:42 +0000

    Deploy preview for PR 250 🛫

---
+++ pr-preview/pr-250/assets/card-hF8fbq3d.js
@@ -0,0 +1,1 @@
+import"./sidebar-element-CMMviGst.js";window.addEventListener("load",()=>{document.querySelector("#property-example w-card").selected=!0});
---
+++ pr-preview/pr-250/assets/index-83QYtMii.js
@@ -0,0 +1,1 @@
+import"./sidebar-element-CMMviGst.js";import"./main-C6G_jrkb.js";
