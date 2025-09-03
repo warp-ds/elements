f2a45e89eba9947dd1832ba966294545212d192d
William Killerud <william.killerud@m10s.io>
2025-09-03 11:05:51 +0200

    chore: lint

--- packages/select/index.ts
+++ packages/select/index.ts
@@ -11,13 +11,13 @@ import { when } from 'lit/directives/when.js';
 
 import { activateI18n } from '../i18n.js';
 import { reset } from '../styles.js';
-import { styles } from './styles.js';
 
 import { messages as daMessages } from './locales/da/messages.mjs';
 import { messages as enMessages } from './locales/en/messages.mjs';
 import { messages as fiMessages } from './locales/fi/messages.mjs';
 import { messages as nbMessages } from './locales/nb/messages.mjs';
 import { messages as svMessages } from './locales/sv/messages.mjs';
+import { styles } from './styles.js';
 
 import '@warp-ds/icons/elements/chevron-down-16';
 
