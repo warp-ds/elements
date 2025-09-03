61c3d81285fae04bd40e5bfb47644e7cce3fe980
Jo Emil Holen <jo.emil.holen@schibsted.com>
2025-09-03 10:06:53 +0200

    add tests

---
+++ packages/box/box.test.ts
@@ -0,0 +1,12 @@
+import { html } from 'lit';
+
+import { page } from '@vitest/browser/context';
+import { expect, test } from 'vitest';
+import './index.js';
+
+test('renders the slotted label', async () => {
+  const component = html`<w-box>This is not a button</w-box>`;
+
+  const screen = await page.render(component);
+  await expect.element(screen.getByText('This is not a button')).toBeVisible();
+});
