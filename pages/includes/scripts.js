5ef0ba284a9dab9a3cf30ed94eebb2ef8518338c
William Killerud <william.killerud@m10s.io>
2025-09-04 12:44:01 +0200

    Revert "docs: remove the old stuff"
    This reverts commit 35554a7a71961f19f971b5efea432f753dfad5e8.

---
+++ pages/includes/scripts.js
@@ -0,0 +1,12 @@
+import { toast, updateToast, removeToast } from '../../index.js';
+import 'uno.css';
+import { WarpToastContainer } from '../../packages/toast/toast-container.js';
+import { windowExists } from '../../packages/utils/window-exists';
+import '@warp-ds/icons/elements/bag-16';
+
+if (windowExists) {
+  window.WarpToastContainer = WarpToastContainer;
+  window.toast = toast;
+  window.updateToast = updateToast;
+  window.removeToast = removeToast;
+}
