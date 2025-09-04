5effa8f8ea00e0e5f443abc6da8780689b4e246f
wkillerud <william@killerud.com>
2025-09-04 12:55:33 +0000

    Deploying to gh-pages from @ warp-ds/elements@66055f3e81abdc00575c7d4a0c2e5763e4c23ded 🚀

---
+++ assets/select.stories-Ca8e8ZDa.js
@@ -0,0 +1,11 @@
+import{g as s,k as a}from"./iframe-JIkZ_xUY.js";import{s as o,p as t}from"./utilities-DIiVBuYh.js";import"./preload-helper-Dch09mLN.js";const{events:p,args:i,argTypes:n}=s("w-select"),d={title:"Forms/Select",render(r){return a`
+      <w-select ${o(t(r))}>
+        <option value="strawberries">Strawberries</option>
+        <option value="raspberries" selected>Raspberries</option>
+        <option value="cloudberries">Cloudberries</option>
+      </w-select>
+    `},args:i,argTypes:n,parameters:{actions:{handles:p}}},e={args:{label:"Berries"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Berries'
+  }
+}`,...e.parameters?.docs?.source}}};const u=["Primary"];export{e as Primary,u as __namedExportsOrder,d as default};
