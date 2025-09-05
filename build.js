b5c6632569c6bce3f9fb8a716d1de91afb99bd2c
William Killerud <william.killerud@m10s.io>
2025-09-05 16:15:22 +0200

    docs: not a little gotcha [skip ci]

--- build.js
+++ build.js
@@ -9,6 +9,8 @@ import manifest from './dist/custom-elements.json' with { type: 'json' };
 
 // See https://github.com/break-stuff/cem-tools/tree/main/packages/vs-code-integration#implementation
 // for how to configure VS Code as a consumer of these files.
+//
+// NB! You need to restart VS Code after a build to see updates to these files!
 generateVsCodeCustomElementData(manifest, {
   outdir: './dist',
   typesSrc: 'parsedType', // since we use @wc-toolkit/type-parser the values for enum types are found here, not on `type`
