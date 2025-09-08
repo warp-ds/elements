460619157e1d487b77b05aec17d5f4041b4cfc38
Vegard Flatoy <vegard.flatoy@schibsted.com>
2025-09-08 14:07:38 +0200

    use correct component name

--- packages/attention/react.ts
+++ packages/attention/react.ts
@@ -5,7 +5,7 @@ import { createComponent } from '@lit/react';
 import { WarpAttention } from './index.js';
 
 export const Attention = createComponent({
-  tagName: 'w-button',
+  tagName: 'w-attention',
   // @ts-expect-error WarpAttention has a popover attribute which conflicts with global HTML popover attribute, will be changed
   elementClass: WarpAttention,
   react: React,
