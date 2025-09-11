26081339c2063f25159cbe70d522a0e69f163c6b
wkillerud <wkillerud@users.noreply.github.com>
2025-09-11 13:45:38 +0000

    Remove preview for PR 322 🛬

--- pr-preview/pr-322/assets/Modal-B7_9vDIA.js
+++
@@ -1,8 +0,0 @@
-import{j as t}from"./index-BX4tXiAa.js";import{useMDXComponents as i}from"./index-DnE6RDA0.js";import{M as s,C as a}from"./blocks-BKj5ViKL.js";import{M as r,D as h,W as m}from"./modal.stories-DyYSw4oS.js";import"./preload-helper-Dch09mLN.js";import"./iframe-DrOQ8Mmm.js";import"./utilities-CkDYE3Mv.js";function e(o){const n={h1:"h1",h2:"h2",h3:"h3",p:"p",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:r,name:"Docs"}),`
-`,t.jsx(n.h1,{id:"modal",children:"Modal"}),`
-`,t.jsx(n.p,{children:"Modals (or dialogs) display important information that users need to acknowledge."}),`
-`,t.jsx(n.h2,{id:"variants",children:"Variants"}),`
-`,t.jsx(n.h3,{id:"with-a-title-bar",children:"With a title bar"}),`
-`,t.jsx(a,{of:h}),`
-`,t.jsx(n.h3,{id:"with-an-image-at-the-top",children:"With an image at the top"}),`
-`,t.jsx(a,{of:m})]})}function u(o={}){const{wrapper:n}={...i(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(e,{...o})}):e(o)}export{u as default};
