87e705afa59f6ca78b0e9235bd4cd6a550c93a29
leftieFriele <leftieFriele@users.noreply.github.com>
2025-09-11 10:55:41 +0000

    Deploy preview for PR 316 🛫

---
+++ pr-preview/pr-316/assets/Select-B-s_IaGV.js
@@ -0,0 +1,4 @@
+import{j as n}from"./index-BX4tXiAa.js";import{useMDXComponents as s}from"./index-DnE6RDA0.js";import{M as r,C as m}from"./blocks-Bx39lUxf.js";import{S as a,D as c}from"./select.stories-B_ydw6si.js";import"./preload-helper-Dch09mLN.js";import"./iframe-ByFBtrIp.js";import"./utilities-7pmNWMLg.js";function o(t){const e={h1:"h1",p:"p",...s(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:a,name:"Docs"}),`
+`,n.jsx(e.h1,{id:"select",children:"Select"}),`
+`,n.jsx(e.p,{children:"A dropdown component for selecting a single value."}),`
+`,n.jsx(m,{of:c})]})}function j(t={}){const{wrapper:e}={...s(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{j as default};
