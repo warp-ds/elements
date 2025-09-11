73a226917ec797f7d3350967706ac069a72dd50c
Prash Mehra <imprashast@gmail.com>
2025-09-11 13:40:00 +0200

    chore: add WarpPageIndicator component with styles and stories (#314)
    * chore: add WarpPageIndicator component with styles and stories
    
    * lint
    
    * upgrade pnpm to 10.x
    
    * fix some review comments

---
+++ packages/pageindicator/style.ts
@@ -0,0 +1,27 @@
+import { css } from 'lit';
+
+export const styles = css`
+  .w-pageindicator {
+    display: grid;
+    justify-content: center;
+    height: max-content;
+    pointer-events: none;
+  }
+
+  .w-pageindicator--container {
+    display: grid;
+    grid-auto-flow: column;
+    gap: 8px;
+  }
+
+  .w-pageindicator--dot {
+    background-color: var(--w-s-color-background-disabled);
+    border-radius: 5px;
+    width: 10px;
+    height: 10px;
+  }
+
+  .w-pageindicator--selecteddot {
+    background-color: var(--w-s-color-icon-selected);
+  }
+`;
