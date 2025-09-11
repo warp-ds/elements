5baf4cb12896d41ed4ba0178f7b84b489f6ca4b2
wkillerud <wkillerud@users.noreply.github.com>
2025-09-11 12:23:48 +0000

    Deploy preview for PR 322 🛫

---
+++ pr-preview/pr-322/assets/Affix-BfKM_X8d.js
@@ -0,0 +1,4 @@
+import{j as e}from"./index-BX4tXiAa.js";import{useMDXComponents as s}from"./index-DnE6RDA0.js";import{M as r,C as i}from"./blocks-DICdoq_F.js";import{A as f,D as a}from"./affix.stories-DFveV7rl.js";import"./preload-helper-Dch09mLN.js";import"./iframe-Di7MMZTf.js";import"./utilities-DNZBxL3l.js";function n(t){const o={a:"a",h1:"h1",p:"p",...s(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:f,name:"Docs"}),`
+`,e.jsx(o.h1,{id:"affix",children:"Affix"}),`
+`,e.jsxs(o.p,{children:["This component is usually used in other components like form elements to show a prefix or suffix. See for example ",e.jsx(o.a,{href:"https://warp-ds.github.io/elements/?path=/story/forms-textfield--with-affix",rel:"nofollow",children:"w-textfield"}),"."]}),`
+`,e.jsx(i,{of:a})]})}function d(t={}){const{wrapper:o}={...s(),...t.components};return o?e.jsx(o,{...t,children:e.jsx(n,{...t})}):n(t)}export{d as default};
