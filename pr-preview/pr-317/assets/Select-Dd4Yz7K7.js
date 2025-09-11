c1743aed8a797e26d4b281ad4f5408cd62e44894
wkillerud <wkillerud@users.noreply.github.com>
2025-09-11 09:03:29 +0000

    Deploy preview for PR 317 🛫

--- pr-preview/pr-317/assets/Select-Dd4Yz7K7.js
+++
@@ -1,4 +0,0 @@
-import{j as n}from"./index-BX4tXiAa.js";import{useMDXComponents as s}from"./index-DnE6RDA0.js";import{M as r,C as m}from"./blocks-BRDmLm0E.js";import{S as a,D as c}from"./select.stories-Bqi6z6jH.js";import"./preload-helper-Dch09mLN.js";import"./iframe-BpPs0TWn.js";import"./utilities-B2ig1q7l.js";function o(t){const e={h1:"h1",p:"p",...s(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:a,name:"Docs"}),`
-`,n.jsx(e.h1,{id:"select",children:"Select"}),`
-`,n.jsx(e.p,{children:"A dropdown component for selecting a single value."}),`
-`,n.jsx(m,{of:c})]})}function j(t={}){const{wrapper:e}={...s(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{j as default};
