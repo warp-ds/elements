9017c3d777935e87846778dfc9faad470ff0a443
imprashast <imprashast@users.noreply.github.com>
2025-09-19 07:07:43 +0000

    Remove preview for PR 327 🛬

--- pr-preview/pr-327/assets/Modal-DtrJJLtd.js
+++
@@ -1,8 +0,0 @@
-import{j as t}from"./index-C8ADKtso.js";import{useMDXComponents as i}from"./index-BwRnHMUQ.js";import{M as s,C as a}from"./blocks-zmvb68Fe.js";import{M as r,D as h,W as m}from"./modal.stories-DEv6IKs3.js";import"./preload-helper-DvrX7W50.js";import"./iframe-BcCnWFnD.js";import"./utilities-B5me8Aot.js";function e(o){const n={h1:"h1",h2:"h2",h3:"h3",p:"p",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:r,name:"Docs"}),`
-`,t.jsx(n.h1,{id:"modal",children:"Modal"}),`
-`,t.jsx(n.p,{children:"Modals (or dialogs) display important information that users need to acknowledge."}),`
-`,t.jsx(n.h2,{id:"variants",children:"Variants"}),`
-`,t.jsx(n.h3,{id:"with-a-title-bar",children:"With a title bar"}),`
-`,t.jsx(a,{of:h}),`
-`,t.jsx(n.h3,{id:"with-an-image-at-the-top",children:"With an image at the top"}),`
-`,t.jsx(a,{of:m})]})}function u(o={}){const{wrapper:n}={...i(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(e,{...o})}):e(o)}export{u as default};
