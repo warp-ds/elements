8c79b82c4838c10c6cfac3e3749adbabb3384c97
flatoy <flatoy@users.noreply.github.com>
2025-09-23 11:15:33 +0000

    Deploy preview for PR 278 🛫

--- pr-preview/pr-278/assets/affix.stories-BpIph96h.js
+++
@@ -1,5 +0,0 @@
-import{g as a,x as s}from"./iframe-CG66TtRE.js";import{s as t,p as o}from"./utilities-HvjOcmGo.js";const{events:n,args:f,argTypes:p}=a("w-affix"),l={title:"Forms/Affix",render(r){return s`<w-affix ${t(o(r))}></w-affix>`},args:f,argTypes:p,parameters:{actions:{handles:n}}},e={args:{label:"kr"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    label: 'kr'
-  }
-}`,...e.parameters?.docs?.source}}};const c=["Default"],d=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:c,default:l},Symbol.toStringTag,{value:"Module"}));export{d as A,e as D};
