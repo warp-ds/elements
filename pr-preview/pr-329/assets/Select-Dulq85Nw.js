6febc918da350c2a4621d5ec1523b740e1c62db7
wkillerud <wkillerud@users.noreply.github.com>
2025-09-24 13:39:12 +0000

    Deploy preview for PR 329 🛫

--- pr-preview/pr-329/assets/Select-Dulq85Nw.js
+++
@@ -1,4 +0,0 @@
-import{j as n}from"./index-C8ADKtso.js";import{useMDXComponents as s}from"./index-BwRnHMUQ.js";import{M as r,C as m}from"./blocks-Ci-3jlHY.js";import{S as a,D as c}from"./select.stories-D4gAhOgj.js";import"./preload-helper-DvrX7W50.js";import"./iframe-CIScE9_C.js";import"./utilities-DVxkTOis.js";function o(t){const e={h1:"h1",p:"p",...s(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:a,name:"Docs"}),`
-`,n.jsx(e.h1,{id:"select",children:"Select"}),`
-`,n.jsx(e.p,{children:"A dropdown component for selecting a single value."}),`
-`,n.jsx(m,{of:c})]})}function j(t={}){const{wrapper:e}={...s(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{j as default};
