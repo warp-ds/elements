1b547ca7ba4f47e765d6ad65c800175c67e55561
wkillerud <wkillerud@users.noreply.github.com>
2025-09-24 09:05:41 +0000

    Deploy preview for PR 329 🛫

--- pr-preview/pr-329/assets/Datepicker-DZpv_tEm.js
+++
@@ -1,4 +0,0 @@
-import{j as e}from"./index-C8ADKtso.js";import{useMDXComponents as r}from"./index-BwRnHMUQ.js";import{M as s,C as a}from"./blocks-Cg7i35h-.js";import{D as i,a as c}from"./datepicker.stories-B-wkpYp2.js";import"./preload-helper-DvrX7W50.js";import"./iframe-UZ3nd3z1.js";import"./utilities-CGdC7qjr.js";function o(t){const n={h1:"h1",p:"p",...r(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:i,name:"Docs"}),`
-`,e.jsx(n.h1,{id:"datepicker",children:"Datepicker"}),`
-`,e.jsx(n.p,{children:"A date picker allows the user to select a specific calendar date."}),`
-`,e.jsx(a,{of:c})]})}function j(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{j as default};
