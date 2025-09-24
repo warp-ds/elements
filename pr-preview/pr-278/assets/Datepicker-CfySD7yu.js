f239651a2cb60a9191b46e89247dba5719a51222
flatoy <flatoy@users.noreply.github.com>
2025-09-24 08:41:11 +0000

    Deploy preview for PR 278 🛫

---
+++ pr-preview/pr-278/assets/Datepicker-CfySD7yu.js
@@ -0,0 +1,4 @@
+import{j as e}from"./index-BX4tXiAa.js";import{useMDXComponents as r}from"./index-DnE6RDA0.js";import{M as s,C as a}from"./blocks-D9pThveb.js";import{D as i,a as c}from"./datepicker.stories-jI91c3Yz.js";import"./preload-helper-HrrhqRgO.js";import"./iframe-sCw3phej.js";import"./utilities-BaxNIjAM.js";function o(t){const n={h1:"h1",p:"p",...r(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:i,name:"Docs"}),`
+`,e.jsx(n.h1,{id:"datepicker",children:"Datepicker"}),`
+`,e.jsx(n.p,{children:"A date picker allows the user to select a specific calendar date."}),`
+`,e.jsx(a,{of:c})]})}function j(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{j as default};
