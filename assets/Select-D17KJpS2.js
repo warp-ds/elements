fb420eb948780c34835e76f2b8c00d51dbc4cdaa
wkillerud <william@killerud.com>
2025-09-25 10:28:12 +0000

    Deploying to gh-pages from @ warp-ds/elements@9c27db89cd3d458dcab51b35c684523607bc9a37 🚀

--- assets/Select-D17KJpS2.js
+++
@@ -1,4 +0,0 @@
-import{j as n}from"./index-C8ADKtso.js";import{useMDXComponents as s}from"./index-BwRnHMUQ.js";import{M as r,C as m}from"./blocks-CnYQa9jh.js";import{S as a,D as c}from"./select.stories-CErVN41i.js";import"./preload-helper-DvrX7W50.js";import"./iframe-C2CydlFf.js";import"./utilities-DuMQfoJZ.js";function o(t){const e={h1:"h1",p:"p",...s(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:a,name:"Docs"}),`
-`,n.jsx(e.h1,{id:"select",children:"Select"}),`
-`,n.jsx(e.p,{children:"A dropdown component for selecting a single value."}),`
-`,n.jsx(m,{of:c})]})}function j(t={}){const{wrapper:e}={...s(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{j as default};
