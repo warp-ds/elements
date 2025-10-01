d6c1df869f84970cd200fe481e4a7d6da4b065b1
imprashast <imprashast@users.noreply.github.com>
2025-10-01 11:12:58 +0000

    Deploy preview for PR 339 🛫

--- pr-preview/pr-339/assets/Datepicker-BO7eDf8G.js
+++
@@ -1,4 +0,0 @@
-import{j as e}from"./index-C8ADKtso.js";import{useMDXComponents as r}from"./index-BwRnHMUQ.js";import{M as s,C as a}from"./blocks-fXU6KD94.js";import{D as i,a as c}from"./datepicker.stories-DUJyDmfT.js";import"./preload-helper-Ct5FWWRu.js";import"./iframe-Cy-UY58g.js";import"./utilities-B31i9vTx.js";function o(t){const n={h1:"h1",p:"p",...r(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:i,name:"Docs"}),`
-`,e.jsx(n.h1,{id:"datepicker",children:"Datepicker"}),`
-`,e.jsx(n.p,{children:"A date picker allows the user to select a specific calendar date."}),`
-`,e.jsx(a,{of:c})]})}function j(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{j as default};
