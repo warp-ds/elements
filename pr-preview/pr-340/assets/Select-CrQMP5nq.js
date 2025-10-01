7a978b24630fe441ca40d5044be84efb9aa28e33
digitalsadhu <digitalsadhu@users.noreply.github.com>
2025-10-01 09:55:52 +0000

    Remove preview for PR 340 🛬

--- pr-preview/pr-340/assets/Select-CrQMP5nq.js
+++
@@ -1,4 +0,0 @@
-import{j as n}from"./index-C8ADKtso.js";import{useMDXComponents as s}from"./index-BwRnHMUQ.js";import{M as r,C as m}from"./blocks-Be42QCY7.js";import{S as a,D as c}from"./select.stories-CDFJS2iq.js";import"./preload-helper-DvrX7W50.js";import"./iframe-CTZFvX7I.js";import"./utilities-C1002IEs.js";function o(t){const e={h1:"h1",p:"p",...s(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:a,name:"Docs"}),`
-`,n.jsx(e.h1,{id:"select",children:"Select"}),`
-`,n.jsx(e.p,{children:"A dropdown component for selecting a single value."}),`
-`,n.jsx(m,{of:c})]})}function j(t={}){const{wrapper:e}={...s(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{j as default};
