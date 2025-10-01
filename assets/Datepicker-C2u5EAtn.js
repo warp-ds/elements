a1db54cfc1ac214ee9f4621019f34cf59076ba33
digitalsadhu <digitalsadhu@gmail.com>
2025-10-01 09:55:44 +0000

    Deploying to gh-pages from @ warp-ds/elements@ac84f17ee0071f26e166bd2feb03b236e30d458b 🚀

---
+++ assets/Datepicker-C2u5EAtn.js
@@ -0,0 +1,4 @@
+import{j as e}from"./index-C8ADKtso.js";import{useMDXComponents as r}from"./index-BwRnHMUQ.js";import{M as s,C as a}from"./blocks-BK9XCk2u.js";import{D as i,a as c}from"./datepicker.stories-DfDryidj.js";import"./preload-helper-DvrX7W50.js";import"./iframe-CPQjOfUO.js";import"./utilities-DZibGfys.js";function o(t){const n={h1:"h1",p:"p",...r(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:i,name:"Docs"}),`
+`,e.jsx(n.h1,{id:"datepicker",children:"Datepicker"}),`
+`,e.jsx(n.p,{children:"A date picker allows the user to select a specific calendar date."}),`
+`,e.jsx(a,{of:c})]})}function j(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{j as default};
