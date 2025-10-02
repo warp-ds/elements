235bc701976b444d1e17c06e06b173b0bf5600ec
wkillerud <wkillerud@users.noreply.github.com>
2025-10-02 12:42:46 +0000

    Remove preview for PR 342 🛬

--- pr-preview/pr-342/assets/Pill-DFJZFkSh.js
+++
@@ -1,9 +0,0 @@
-import{j as n}from"./index-C8ADKtso.js";import{useMDXComponents as a}from"./index-BwRnHMUQ.js";import{M as i,C as s}from"./blocks-DVHlfvvJ.js";import{P as r,D as l,S as u,C as c}from"./pill.stories-BS8mnesT.js";import"./preload-helper-Ct5FWWRu.js";import"./iframe-CHAp8gQH.js";import"./utilities-Ji1LSYxw.js";function e(o){const t={h1:"h1",h2:"h2",p:"p",...a(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(i,{of:r,name:"Docs"}),`
-`,n.jsx(t.h1,{id:"pill",children:"Pill"}),`
-`,n.jsx(t.p,{children:"Pill is a type of button that is often used as a filter, but can also be used as a rounded button for overlays, etc."}),`
-`,n.jsx(t.h2,{id:"default",children:"Default"}),`
-`,n.jsx(s,{of:l}),`
-`,n.jsx(t.h2,{id:"suggestion",children:"Suggestion"}),`
-`,n.jsx(s,{of:u}),`
-`,n.jsx(t.h2,{id:"closable",children:"Closable"}),`
-`,n.jsx(s,{of:c})]})}function C(o={}){const{wrapper:t}={...a(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(e,{...o})}):e(o)}export{C as default};
