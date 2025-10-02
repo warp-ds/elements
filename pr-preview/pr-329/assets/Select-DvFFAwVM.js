6acf1ab361350ec347515d145c9a1d2d374af11c
wkillerud <wkillerud@users.noreply.github.com>
2025-10-02 10:52:59 +0000

    Deploy preview for PR 329 🛫

---
+++ pr-preview/pr-329/assets/Select-DvFFAwVM.js
@@ -0,0 +1,4 @@
+import{j as t}from"./index-C8ADKtso.js";import{useMDXComponents as r}from"./index-BwRnHMUQ.js";import{M as s,C as m}from"./blocks-CIsUS5Ij.js";import{S as i,D as a}from"./select.stories-BShFAfDT.js";import"./preload-helper-Ct5FWWRu.js";import"./iframe-C2TmMrwz.js";import"./utilities-Dx6QX0UB.js";import"./if-defined-CNynj8_o.js";import"./chevron-down-16-CAdgDB_A.js";import"./FormControlMixin-DJnJZbro.js";function e(o){const n={h1:"h1",p:"p",...r(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:i,name:"Docs"}),`
+`,t.jsx(n.h1,{id:"select",children:"Select"}),`
+`,t.jsx(n.p,{children:"A dropdown component for selecting a single value."}),`
+`,t.jsx(m,{of:a})]})}function M(o={}){const{wrapper:n}={...r(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(e,{...o})}):e(o)}export{M as default};
