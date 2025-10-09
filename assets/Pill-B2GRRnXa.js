7c3e6c5cdb0237e5ed8db7ef3a0482d4dfc9e101
digitalsadhu <digitalsadhu@gmail.com>
2025-10-09 15:36:56 +0000

    Deploying to gh-pages from @ warp-ds/elements@89b93ef2e45af6564a70bc5c317c02f5102c5723 🚀

--- assets/Pill-B2GRRnXa.js
+++
@@ -1,9 +0,0 @@
-import{j as n}from"./index-hdMx_IMH.js";import{useMDXComponents as a}from"./index-DH_uTf8R.js";import{M as i,C as s}from"./blocks-BleaZwe2.js";import{P as r,D as l,S as u,C as c}from"./pill.stories-9T2Rr7pA.js";import"./preload-helper-DvrX7W50.js";import"./iframe-X_FrMHF-.js";import"./utilities-BccyQ6tO.js";function e(o){const t={h1:"h1",h2:"h2",p:"p",...a(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(i,{of:r,name:"Docs"}),`
-`,n.jsx(t.h1,{id:"pill",children:"Pill"}),`
-`,n.jsx(t.p,{children:"Pill is a type of button that is often used as a filter, but can also be used as a rounded button for overlays, etc."}),`
-`,n.jsx(t.h2,{id:"default",children:"Default"}),`
-`,n.jsx(s,{of:l}),`
-`,n.jsx(t.h2,{id:"suggestion",children:"Suggestion"}),`
-`,n.jsx(s,{of:u}),`
-`,n.jsx(t.h2,{id:"closable",children:"Closable"}),`
-`,n.jsx(s,{of:c})]})}function C(o={}){const{wrapper:t}={...a(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(e,{...o})}):e(o)}export{C as default};
