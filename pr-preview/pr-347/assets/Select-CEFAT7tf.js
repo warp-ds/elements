3be1ed017169709d930db47eda7b3043947bde60
digitalsadhu <digitalsadhu@users.noreply.github.com>
2025-10-09 06:34:08 +0000

    Remove preview for PR 347 🛬

--- pr-preview/pr-347/assets/Select-CEFAT7tf.js
+++
@@ -1,4 +0,0 @@
-import{j as t}from"./index-hdMx_IMH.js";import{useMDXComponents as r}from"./index-DH_uTf8R.js";import{M as s,C as m}from"./blocks-CPvLtEUq.js";import{S as i,D as a}from"./select.stories-B3RJ7AFd.js";import"./preload-helper-Ct5FWWRu.js";import"./iframe-r0hxqDkq.js";import"./utilities-DcLQUf2-.js";import"./if-defined-CPtQeV4L.js";import"./chevron-down-16-CZ-2pvR_.js";import"./FormControlMixin-DJnJZbro.js";function e(o){const n={h1:"h1",p:"p",...r(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:i,name:"Docs"}),`
-`,t.jsx(n.h1,{id:"select",children:"Select"}),`
-`,t.jsx(n.p,{children:"A dropdown component for selecting a single value."}),`
-`,t.jsx(m,{of:a})]})}function M(o={}){const{wrapper:n}={...r(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(e,{...o})}):e(o)}export{M as default};
