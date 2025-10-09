174c815abc574541da2cfee8eef2a7c50dafcb75
digitalsadhu <digitalsadhu@gmail.com>
2025-10-09 07:54:24 +0000

    Deploying to gh-pages from @ warp-ds/elements@8a356b864c982e06c7800e099c2348aa50c047aa 🚀

---
+++ assets/Link-Cz8Gauac.js
@@ -0,0 +1,27 @@
+import{j as e}from"./index-hdMx_IMH.js";import{useMDXComponents as o}from"./index-DH_uTf8R.js";import{M as a,C as n}from"./blocks-BleaZwe2.js";import{L as r,P as l,S as d,N as h,U as c,a as u,b as f,c as p,Q as b,d as x,e as j,F as m}from"./link.stories-mrR_lQ1q.js";import"./preload-helper-DvrX7W50.js";import"./iframe-X_FrMHF-.js";import"./utilities-BccyQ6tO.js";import"./index-C1fFciYC.js";import"./class-map-C66oMe1M.js";import"./FormControlMixin-DJnJZbro.js";function s(i){const t={h1:"h1",h2:"h2",p:"p",...o(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:r,name:"Docs"}),`
+`,e.jsx(t.h1,{id:"link",children:"Link"}),`
+`,e.jsx(t.p,{children:"Link buttons are used to perform actions, with different visuals for different needs."}),`
+`,e.jsx(t.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsx(t.p,{children:"If the button doesn't have visible text content, such as when used with only an icon, an aria-label prop must be provided for accessibility."}),`
+`,e.jsx(t.h2,{id:"variants",children:"Variants"}),`
+`,e.jsx(t.p,{children:"The primary button is a call to action."}),`
+`,e.jsx(n,{of:l}),`
+`,e.jsx(t.p,{children:'The secondary button is for secondary or tertiary actions. "secondary" is the default value of the "variant" attribute.'}),`
+`,e.jsx(n,{of:d}),`
+`,e.jsx(t.p,{children:"The negative button is for emphasizing actions that can be destructive or have negative consequences if taken."}),`
+`,e.jsx(n,{of:h}),`
+`,e.jsx(n,{of:c}),`
+`,e.jsx(n,{of:u}),`
+`,e.jsx(n,{of:f}),`
+`,e.jsx(t.p,{children:"All of the above buttons have a small variant."}),`
+`,e.jsx(n,{of:p}),`
+`,e.jsx(t.h2,{id:"quiet",children:"Quiet"}),`
+`,e.jsx(t.p,{children:"By default, buttons have a visible stroke and fill. Quiet buttons do not have this visible stroke or fill and should only be used for secondary actions within a button group. Quiet buttons have the same padding rules as other action buttons and reveal a button-like background color when hovered."}),`
+`,e.jsx(n,{of:b}),`
+`,e.jsx(n,{of:x}),`
+`,e.jsx(n,{of:j}),`
+`,e.jsx(t.h2,{id:"full-width",children:"Full width"}),`
+`,e.jsx(t.p,{children:"Button will take the parent's width instead of content width. Useful on mobile when button should take full width."}),`
+`,e.jsx(n,{of:m}),`
+`,e.jsx(t.h2,{id:"disabled-state",children:"Disabled state"}),`
+`,e.jsx(t.p,{children:"Disabled is an anti-pattern and is not supported. There will ALWAYS be users who don't understand why an element is disabled, or users who can't even see that it is disabled because of poor lighting conditions or other reasons."})]})}function S(i={}){const{wrapper:t}={...o(),...i.components};return t?e.jsx(t,{...i,children:e.jsx(s,{...i})}):s(i)}export{S as default};
---
+++ assets/Select-BtK6ig9A.js
@@ -0,0 +1,4 @@
+import{j as t}from"./index-hdMx_IMH.js";import{useMDXComponents as r}from"./index-DH_uTf8R.js";import{M as s,C as m}from"./blocks-BleaZwe2.js";import{S as i,D as a}from"./select.stories-DEAjrMO-.js";import"./preload-helper-DvrX7W50.js";import"./iframe-X_FrMHF-.js";import"./utilities-BccyQ6tO.js";import"./if-defined-CcC9Flk_.js";import"./chevron-down-16-DylQ37xA.js";import"./FormControlMixin-DJnJZbro.js";function e(o){const n={h1:"h1",p:"p",...r(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:i,name:"Docs"}),`
+`,t.jsx(n.h1,{id:"select",children:"Select"}),`
+`,t.jsx(n.p,{children:"A dropdown component for selecting a single value."}),`
+`,t.jsx(m,{of:a})]})}function M(o={}){const{wrapper:n}={...r(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(e,{...o})}):e(o)}export{M as default};
