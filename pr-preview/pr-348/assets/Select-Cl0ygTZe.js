4f39a6d0d9b420df96c0d96e57b440d6ab9a2fe1
pearofducks <pearofducks@users.noreply.github.com>
2025-10-12 05:46:56 +0000

    Deploy preview for PR 348 🛫

---
+++ pr-preview/pr-348/assets/Select-Cl0ygTZe.js
@@ -0,0 +1,4 @@
+import{j as t}from"./index-hdMx_IMH.js";import{useMDXComponents as r}from"./index-DH_uTf8R.js";import{M as s,C as m}from"./blocks-B5sESc-r.js";import{S as i,D as a}from"./select.stories-Ci5iCw0u.js";import"./preload-helper-Ct5FWWRu.js";import"./iframe-DvzodPjT.js";import"./utilities-eWSlf40W.js";import"./if-defined-BSullmgO.js";import"./chevron-down-16-DbbDopyJ.js";import"./FormControlMixin-DJnJZbro.js";function e(o){const n={h1:"h1",p:"p",...r(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:i,name:"Docs"}),`
+`,t.jsx(n.h1,{id:"select",children:"Select"}),`
+`,t.jsx(n.p,{children:"A dropdown component for selecting a single value."}),`
+`,t.jsx(m,{of:a})]})}function M(o={}){const{wrapper:n}={...r(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(e,{...o})}):e(o)}export{M as default};
