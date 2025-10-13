3f60efa4702dfef5e7a8a0a2a08bf6c84547bb57
pearofducks <pearofducks@users.noreply.github.com>
2025-10-13 07:01:12 +0000

    Deploy preview for PR 348 🛫

---
+++ pr-preview/pr-348/assets/Datepicker-DPSLsw_p.js
@@ -0,0 +1,4 @@
+import{j as t}from"./index-hdMx_IMH.js";import{useMDXComponents as n}from"./index-DH_uTf8R.js";import{M as i,C as s}from"./blocks-C0It3hgO.js";import{D as a,a as m}from"./datepicker.stories-DvPypTwl.js";import"./preload-helper-Ct5FWWRu.js";import"./iframe-YpWuL_i-.js";import"./utilities-Bio9qadi.js";import"./index-COC16vS8.js";import"./index-CghdxUPH.js";import"./class-map-BipA5VY2.js";import"./FormControlMixin-DJnJZbro.js";import"./query-NYTjQBeo.js";import"./if-defined-B13rv3Dq.js";import"./chevron-right-16-yyPGJ6bt.js";function r(e){const o={h1:"h1",p:"p",...n(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:a,name:"Docs"}),`
+`,t.jsx(o.h1,{id:"datepicker",children:"Datepicker"}),`
+`,t.jsx(o.p,{children:"A date picker allows the user to select a specific calendar date."}),`
+`,t.jsx(s,{of:m})]})}function w(e={}){const{wrapper:o}={...n(),...e.components};return o?t.jsx(o,{...e,children:t.jsx(r,{...e})}):r(e)}export{w as default};
---
+++ pr-preview/pr-348/assets/Pill-0doAcquI.js
@@ -0,0 +1,9 @@
+import{j as n}from"./index-hdMx_IMH.js";import{useMDXComponents as a}from"./index-DH_uTf8R.js";import{M as i,C as s}from"./blocks-C0It3hgO.js";import{P as r,D as l,S as u,C as c}from"./pill.stories-CW-ObiSw.js";import"./preload-helper-Ct5FWWRu.js";import"./iframe-YpWuL_i-.js";import"./utilities-Bio9qadi.js";function e(o){const t={h1:"h1",h2:"h2",p:"p",...a(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(i,{of:r,name:"Docs"}),`
+`,n.jsx(t.h1,{id:"pill",children:"Pill"}),`
+`,n.jsx(t.p,{children:"Pill is a type of button that is often used as a filter, but can also be used as a rounded button for overlays, etc."}),`
+`,n.jsx(t.h2,{id:"default",children:"Default"}),`
+`,n.jsx(s,{of:l}),`
+`,n.jsx(t.h2,{id:"suggestion",children:"Suggestion"}),`
+`,n.jsx(s,{of:u}),`
+`,n.jsx(t.h2,{id:"closable",children:"Closable"}),`
+`,n.jsx(s,{of:c})]})}function C(o={}){const{wrapper:t}={...a(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(e,{...o})}):e(o)}export{C as default};
