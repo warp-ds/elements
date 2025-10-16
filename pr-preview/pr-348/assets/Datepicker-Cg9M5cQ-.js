3cf94c0b41c8c41fb5bce714937213173336fffd
pearofducks <pearofducks@users.noreply.github.com>
2025-10-16 09:39:57 +0000

    Deploy preview for PR 348 🛫

---
+++ pr-preview/pr-348/assets/Datepicker-Cg9M5cQ-.js
@@ -0,0 +1,4 @@
+import{j as t}from"./index-hdMx_IMH.js";import{useMDXComponents as n}from"./index-DH_uTf8R.js";import{M as i,C as s}from"./blocks-BK0AFJbj.js";import{D as a,a as m}from"./datepicker.stories-DR_3iB27.js";import"./preload-helper-Ct5FWWRu.js";import"./iframe-Dy-oT3U6.js";import"./utilities-ClVV2U5g.js";import"./index-DG6aWi2W.js";import"./index-DZxFPq6K.js";import"./class-map-D25pzeG6.js";import"./FormControlMixin-DJnJZbro.js";import"./query-NYTjQBeo.js";import"./if-defined-CXVF8sIa.js";import"./chevron-right-16-BHXvP1m7.js";function r(e){const o={h1:"h1",p:"p",...n(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:a,name:"Docs"}),`
+`,t.jsx(o.h1,{id:"datepicker",children:"Datepicker"}),`
+`,t.jsx(o.p,{children:"A date picker allows the user to select a specific calendar date."}),`
+`,t.jsx(s,{of:m})]})}function w(e={}){const{wrapper:o}={...n(),...e.components};return o?t.jsx(o,{...e,children:t.jsx(r,{...e})}):r(e)}export{w as default};
