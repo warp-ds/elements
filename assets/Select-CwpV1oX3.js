ce0bdd9686d4ba82264c43476d21ecb6ee242422
imprashast <imprashast@gmail.com>
2025-10-22 07:08:33 +0000

    Deploying to gh-pages from @ warp-ds/elements@15c46aa48879acf017a2ff99a47c2aa41eb3b466 🚀

--- assets/Select-CwpV1oX3.js
+++
@@ -1,4 +0,0 @@
-import{j as t}from"./index-DhQHbkge.js";import{useMDXComponents as r}from"./index-D2LXvqQ0.js";import{M as s,C as m}from"./blocks-YdpYUoBz.js";import{S as i,D as a}from"./select.stories-CmyUyaHZ.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preload-helper-DvrX7W50.js";import"./iframe-DdYps7yf.js";import"./utilities-flMuTxN1.js";import"./if-defined-HmG21hF1.js";import"./chevron-down-16-DyKCcdCP.js";import"./FormControlMixin-DJnJZbro.js";function e(o){const n={h1:"h1",p:"p",...r(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:i,name:"Docs"}),`
-`,t.jsx(n.h1,{id:"select",children:"Select"}),`
-`,t.jsx(n.p,{children:"A dropdown component for selecting a single value."}),`
-`,t.jsx(m,{of:a})]})}function C(o={}){const{wrapper:n}={...r(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(e,{...o})}):e(o)}export{C as default};
