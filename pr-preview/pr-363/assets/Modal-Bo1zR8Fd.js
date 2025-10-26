57cccfba0e7823eeb3922062c088b0dbe8044973
pearofducks <pearofducks@users.noreply.github.com>
2025-10-26 15:23:32 +0000

    Deploy preview for PR 363 🛫

---
+++ pr-preview/pr-363/assets/Modal-Bo1zR8Fd.js
@@ -0,0 +1,8 @@
+import{j as t}from"./index-DhQHbkge.js";import{useMDXComponents as e}from"./index-D2LXvqQ0.js";import{M as r,C as a}from"./blocks-CmgUF-dc.js";import{M as s,D as m,W as h}from"./modal.stories-BWkBEuxh.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preload-helper-Ct5FWWRu.js";import"./iframe-DZgAOCU5.js";import"./utilities-BdSTV7tJ.js";import"./query-NYTjQBeo.js";function i(o){const n={h1:"h1",h2:"h2",h3:"h3",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:s,name:"Docs"}),`
+`,t.jsx(n.h1,{id:"modal",children:"Modal"}),`
+`,t.jsx(n.p,{children:"Modals (or dialogs) display important information that users need to acknowledge."}),`
+`,t.jsx(n.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(n.h3,{id:"with-a-title-bar",children:"With a title bar"}),`
+`,t.jsx(a,{of:m}),`
+`,t.jsx(n.h3,{id:"with-an-image-at-the-top",children:"With an image at the top"}),`
+`,t.jsx(a,{of:h})]})}function g(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(i,{...o})}):i(o)}export{g as default};
