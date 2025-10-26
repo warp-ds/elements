00b4e6fb3ad0be5847113cc571e9351a3acde7b6
pearofducks <pearofducks@users.noreply.github.com>
2025-10-26 15:19:25 +0000

    Deploy preview for PR 363 🛫

---
+++ pr-preview/pr-363/assets/Select-DOlfXKmh.js
@@ -0,0 +1,4 @@
+import{j as t}from"./index-DhQHbkge.js";import{useMDXComponents as r}from"./index-D2LXvqQ0.js";import{M as s,C as m}from"./blocks-AhdRuqNG.js";import{S as i,D as a}from"./select.stories-DOf972Br.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preload-helper-Ct5FWWRu.js";import"./iframe-B51Q3Rrt.js";import"./utilities-8qzRQfrN.js";import"./if-defined-CopMy4Xj.js";import"./chevron-down-16-zVTvCnvx.js";import"./FormControlMixin-DJnJZbro.js";function e(o){const n={h1:"h1",p:"p",...r(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:i,name:"Docs"}),`
+`,t.jsx(n.h1,{id:"select",children:"Select"}),`
+`,t.jsx(n.p,{children:"A dropdown component for selecting a single value."}),`
+`,t.jsx(m,{of:a})]})}function C(o={}){const{wrapper:n}={...r(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(e,{...o})}):e(o)}export{C as default};
