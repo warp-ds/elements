cb9928f4c0b0c7f651cbc4cd0a0b6ba980e50ebe
William Killerud <william@killerud.com>
2025-11-06 08:01:15 +0100

    chore: drop generating VSCode custom data (#410)
    The maintainer is considering dropping support for it, instead
    focusing on the language server (which makes sense, and is easier
    to use for end-users).

--- build/editor.js
+++ build/editor.js
@@ -3,18 +3,8 @@
 // custom-elements-manifest analyze --config .cem/custom-element-manifest.config.js
 
 import { generateJetBrainsWebTypes } from 'custom-element-jet-brains-integration';
-import { generateVsCodeCustomElementData } from 'custom-element-vs-code-integration';
 
 import manifest from '../dist/custom-elements.json' with { type: 'json' };
-
-// See https://github.com/break-stuff/cem-tools/tree/main/packages/vs-code-integration#implementation
-// for how to configure VS Code as a consumer of these files.
-//
-// NB! You need to restart VS Code after a build to see updates to these files!
-generateVsCodeCustomElementData(manifest, {
-  outdir: new URL('../dist', import.meta.url).pathname,
-  typesSrc: 'parsedType', // since we use @wc-toolkit/type-parser the values for enum types are found here, not on `type`
-});
 
 // See https://github.com/break-stuff/cem-tools/tree/main/packages/jet-brains-integration#implementation
 // for an explanation. We set `web-types` in `package.json` so users shouldn't need to do anything.
