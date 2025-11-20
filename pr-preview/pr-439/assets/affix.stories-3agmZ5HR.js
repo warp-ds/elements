51fd365bdf5a4d4bc10b320b10d079f6f1ed9016
wkillerud <wkillerud@users.noreply.github.com>
2025-11-20 12:32:29 +0000

    Deploy preview for PR 439 🛫

---
+++ pr-preview/pr-439/assets/affix.stories-3agmZ5HR.js
@@ -0,0 +1,5 @@
+import{g as a,x as s}from"./iframe-HgC5Ph9D.js";import{s as t,p as o}from"./utilities-6FM8U_C5.js";import"./index-DZtjB7nA.js";const{events:n,args:p,argTypes:f}=a("w-affix"),l={title:"Forms/Affix",render(r){return s`<w-affix ${t(o(r))}></w-affix>`},args:p,argTypes:f,parameters:{actions:{handles:n}}},e={args:{label:"kr"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'kr'
+  }
+}`,...e.parameters?.docs?.source}}};const i=["Default"],u=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:i,default:l},Symbol.toStringTag,{value:"Module"}));export{u as A,e as D};
