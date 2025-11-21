22dc96761b693390372fe45be98af0baaeb2aec9
leftieFriele <leftieFriele@users.noreply.github.com>
2025-11-21 12:54:50 +0000

    Deploy preview for PR 440 🛫

--- pr-preview/pr-440/assets/affix.stories-B7D5GdB2.js
+++
@@ -1,5 +0,0 @@
-import{g as a,x as s}from"./iframe-CA8o6K5a.js";import{s as t,p as o}from"./utilities-C5TXeG4z.js";import"./index-BQ03l0fx.js";const{events:n,args:p,argTypes:f}=a("w-affix"),l={title:"Forms/Affix",render(r){return s`<w-affix ${t(o(r))}></w-affix>`},args:p,argTypes:f,parameters:{actions:{handles:n}}},e={args:{label:"kr"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    label: 'kr'
-  }
-}`,...e.parameters?.docs?.source}}};const i=["Default"],u=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:i,default:l},Symbol.toStringTag,{value:"Module"}));export{u as A,e as D};
