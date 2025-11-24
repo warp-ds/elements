7ddd85f73d859a67ddfcbb4d7542b1019c384f90
leftieFriele <leftie.friele@gmail.com>
2025-11-24 10:10:59 +0000

    Deploying to gh-pages from @ warp-ds/elements@2f4d151ca007f58b08b957eae1cd87dd7da9f4e0 🚀

--- assets/affix.stories-B1QV3WtP.js
+++
@@ -1,5 +0,0 @@
-import{g as a,x as s}from"./iframe-B5cRU1kT.js";import{s as t,p as o}from"./utilities-CLPHCoBj.js";import"./index-C86qETwr.js";const{events:n,args:p,argTypes:f}=a("w-affix"),l={title:"Forms/Affix",render(r){return s`<w-affix ${t(o(r))}></w-affix>`},args:p,argTypes:f,parameters:{actions:{handles:n}}},e={args:{label:"kr"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    label: 'kr'
-  }
-}`,...e.parameters?.docs?.source}}};const i=["Default"],u=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:i,default:l},Symbol.toStringTag,{value:"Module"}));export{u as A,e as D};
