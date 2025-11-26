ee07a24f8a395a1bfb67ac6622b2eff5386a7b76
joms <joms@users.noreply.github.com>
2025-11-26 11:22:11 +0000

    Deploy preview for PR 445 🛫

--- pr-preview/pr-445/assets/Breadcrumbs-B7a8Owgd.js
+++
@@ -1,9 +0,0 @@
-import{j as n}from"./index-DhQHbkge.js";import{useMDXComponents as o}from"./index-D2LXvqQ0.js";import{M as i,C as e}from"./blocks-CMtKVRMq.js";import{B as a,D as l,W as m,a as c}from"./breadcrumbs.stories-DmyqF7w0.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preload-helper-Ct5FWWRu.js";import"./iframe-D341Inaa.js";import"./utilities-ChO4BkgR.js";function r(t){const s={h1:"h1",h2:"h2",h3:"h3",p:"p",...o(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(i,{of:a,name:"Docs"}),`
-`,n.jsx(s.h1,{id:"breadcrumbs",children:"Breadcrumbs"}),`
-`,n.jsx(s.p,{children:"Breadcrumbs show the navigation structure for the current location."}),`
-`,n.jsx(e,{of:l}),`
-`,n.jsx(s.h2,{id:"visual-options",children:"Visual options"}),`
-`,n.jsx(s.h3,{id:"all-segments-linkable",children:"All segments linkable"}),`
-`,n.jsx(e,{of:m}),`
-`,n.jsx(s.h3,{id:"disabled-last-link",children:"Disabled last link"}),`
-`,n.jsx(e,{of:c})]})}function D(t={}){const{wrapper:s}={...o(),...t.components};return s?n.jsx(s,{...t,children:n.jsx(r,{...t})}):r(t)}export{D as default};
