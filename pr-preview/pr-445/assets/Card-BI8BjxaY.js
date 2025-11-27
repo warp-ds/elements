0ecf14fedbfcc5b8187c851c7a3314f05691f8ae
joms <joms@users.noreply.github.com>
2025-11-27 13:16:21 +0000

    Deploy preview for PR 445 🛫

---
+++ pr-preview/pr-445/assets/Card-BI8BjxaY.js
@@ -0,0 +1,12 @@
+import{j as e}from"./index-DhQHbkge.js";import{useMDXComponents as r}from"./index-D2LXvqQ0.js";import{M as i,C as t}from"./blocks-5sAY2C3V.js";import{C as a,D as l,S as c,F as d,a as p,b as m,P as x}from"./card.stories-BrwROamT.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preload-helper-Ct5FWWRu.js";import"./iframe-DFO_q9NB.js";import"./utilities-E1Nl2jR1.js";import"./if-defined-CjyjJywx.js";function n(s){const o={a:"a",code:"code",h1:"h1",h2:"h2",p:"p",...r(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:a,name:"Docs"}),`
+`,e.jsx(o.h1,{id:"card",children:"Card"}),`
+`,e.jsx(o.p,{children:"Card is a layout component used for separating content areas on a page."}),`
+`,e.jsx(o.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(o.p,{children:["Use the ARIA role attribute to provide semantic meaning to a box element. If no role property is supplied, it will default to ",e.jsx(o.code,{children:'role="region"'}),". To remove the default role attribute, supply an empty role (",e.jsx(o.code,{children:'role=""'}),"). Consider using more suitable roles for your specific Box, as described on ",e.jsx(o.a,{href:"https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles#aria_role_types",rel:"nofollow",children:"MDN WAI-ARIA Roles"}),"."]}),`
+`,e.jsx(o.h2,{id:"visual-options",children:"Visual options"}),`
+`,e.jsx(t,{of:l}),`
+`,e.jsx(t,{of:c}),`
+`,e.jsx(t,{of:d}),`
+`,e.jsx(t,{of:p}),`
+`,e.jsx(t,{of:m}),`
+`,e.jsx(t,{of:x})]})}function D(s={}){const{wrapper:o}={...r(),...s.components};return o?e.jsx(o,{...s,children:e.jsx(n,{...s})}):n(s)}export{D as default};
