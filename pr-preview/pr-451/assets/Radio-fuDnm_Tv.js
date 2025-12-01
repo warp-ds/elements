5ec381a79d76dfab6d0a0275effc082648f1dba2
flatoy <flatoy@users.noreply.github.com>
2025-12-01 10:51:38 +0000

    Deploy preview for PR 451 🛫

---
+++ pr-preview/pr-451/assets/Radio-fuDnm_Tv.js
@@ -0,0 +1,11 @@
+import{j as o}from"./index-DhQHbkge.js";import{useMDXComponents as r}from"./index-D2LXvqQ0.js";import{M as t,C as e}from"./blocks-tUwuarmU.js";import{R as a,D as d,a as m,b as l,c as p}from"./radio.stories-DRFmrY1d.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preload-helper-Ct5FWWRu.js";import"./iframe-Cm5Fr1Rf.js";import"./utilities-b8ymJw4e.js";import"./toggle-styles-D_bly94H.js";import"./slot-L4FTwZ71.js";import"./query-NYTjQBeo.js";import"./class-map-yMKIBq7K.js";function n(s){const i={h1:"h1",h2:"h2",p:"p",...r(),...s.components};return o.jsxs(o.Fragment,{children:[o.jsx(t,{of:a,name:"Docs"}),`
+`,o.jsx(i.h1,{id:"radio",children:"Radio"}),`
+`,o.jsx(i.p,{children:"Radios allow users to select a single option from a list."}),`
+`,o.jsx(e,{of:d}),`
+`,o.jsx(i.h2,{id:"required",children:"Required"}),`
+`,o.jsx(e,{of:m}),`
+`,o.jsx(i.h2,{id:"disabled",children:"Disabled"}),`
+`,o.jsx(i.h2,{id:"disabled-group",children:"Disabled Group"}),`
+`,o.jsx(e,{of:l}),`
+`,o.jsx(i.h2,{id:"disabled-radio",children:"Disabled Radio"}),`
+`,o.jsx(e,{of:p})]})}function q(s={}){const{wrapper:i}={...r(),...s.components};return i?o.jsx(i,{...s,children:o.jsx(n,{...s})}):n(s)}export{q as default};
