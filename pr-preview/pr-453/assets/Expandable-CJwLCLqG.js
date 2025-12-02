cbf57519733e17f717d2442f76c385571f619c47
joms <joms@users.noreply.github.com>
2025-12-02 10:09:40 +0000

    Deploy preview for PR 453 🛫

---
+++ pr-preview/pr-453/assets/Expandable-CJwLCLqG.js
@@ -0,0 +1,22 @@
+import{j as t}from"./index-DhQHbkge.js";import{useMDXComponents as a}from"./index-D2LXvqQ0.js";import{M as s,C as e}from"./blocks-DC2__CQw.js";import{E as r,D as h,a as d,B as l,A as p,W as c,C as x,N as u,b as m}from"./expandable.stories-BhJKE_TC.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preload-helper-Ct5FWWRu.js";import"./iframe-BpaSZEaq.js";import"./utilities-Bhme5Tk0.js";import"./expand-transition-CUeZ4-f1.js";import"./if-defined-DsiCh5yg.js";import"./chevron-down-16-DyZjOlqj.js";function o(i){const n={h1:"h1",h2:"h2",h3:"h3",p:"p",...a(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:r,name:"Docs"}),`
+`,t.jsx(n.h1,{id:"expandable",children:"Expandable"}),`
+`,t.jsx(n.p,{children:"Expandable is a layout component used for creating expandable content areas on a page."}),`
+`,t.jsx(e,{of:h}),`
+`,t.jsx(n.h2,{id:"visual-options",children:"Visual options"}),`
+`,t.jsx(n.h3,{id:"start-expanded",children:"Start expanded"}),`
+`,t.jsx(e,{of:d}),`
+`,t.jsx(n.h3,{id:"with-or-without-animation",children:"With or without animation"}),`
+`,t.jsx(n.p,{children:"By default the box expands immediately, but you can animate it."}),`
+`,t.jsx(e,{of:l}),`
+`,t.jsx(e,{of:p}),`
+`,t.jsx(n.h3,{id:"with-a-heading-wrapping-the-toggle-button",children:"With a heading wrapping the toggle button"}),`
+`,t.jsx(n.p,{children:"This can be used to wrap the toggle button in a heading for navigational and structural purposes. It's a feature that is needed typically in scenarios where multiple consecutive Expandable components are used."}),`
+`,t.jsx(e,{of:c}),`
+`,t.jsx(n.h3,{id:"with-custom-title",children:"With custom title"}),`
+`,t.jsx(n.p,{children:"This can be used if more control over styling is needed than the title prop allows"}),`
+`,t.jsx(e,{of:x}),`
+`,t.jsx(n.h3,{id:"without-a-title",children:"Without a title"}),`
+`,t.jsx(n.p,{children:"This can be used if more control over styling is needed than the title prop allows"}),`
+`,t.jsx(e,{of:u}),`
+`,t.jsx(n.h3,{id:"without-chevron-icon",children:"Without chevron icon"}),`
+`,t.jsx(e,{of:m})]})}function M(i={}){const{wrapper:n}={...a(),...i.components};return n?t.jsx(n,{...i,children:t.jsx(o,{...i})}):o(i)}export{M as default};
