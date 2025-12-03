dbf76a2b946d3b49996d170daab73f5c415c598f
wkillerud <wkillerud@users.noreply.github.com>
2025-12-03 11:36:55 +0000

    Deploy preview for PR 458 🛫

---
+++ pr-preview/pr-458/assets/Badge-oM8j1pYS.js
@@ -0,0 +1,15 @@
+import{j as n}from"./index-DhQHbkge.js";import{useMDXComponents as i}from"./index-D2LXvqQ0.js";import{M as r,C as o}from"./blocks-CQ9dDgxL.js";import{B as d,N as a,I as c,P as f,W as m,a as x,b as j,S as h,c as l}from"./badge.stories-BjvL-UbP.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preload-helper-Ct5FWWRu.js";import"./iframe-Bw8IHuFa.js";import"./utilities-ku2obmXA.js";function e(t){const s={h1:"h1",h2:"h2",h3:"h3",p:"p",...i(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:d,name:"Docs"}),`
+`,n.jsx(s.h1,{id:"badge",children:"Badge"}),`
+`,n.jsx(s.p,{children:"Badge is used for showing a small amount of non-interactive color-categorized metadata, like a status or count."}),`
+`,n.jsx(s.h2,{id:"visual-options",children:"Visual options"}),`
+`,n.jsx(s.h3,{id:"default",children:"Default"}),`
+`,n.jsx(o,{of:a}),`
+`,n.jsx(s.h3,{id:"variants",children:"Variants"}),`
+`,n.jsx(o,{of:a}),`
+`,n.jsx(o,{of:c}),`
+`,n.jsx(o,{of:f}),`
+`,n.jsx(o,{of:m}),`
+`,n.jsx(o,{of:x}),`
+`,n.jsx(o,{of:j}),`
+`,n.jsx(o,{of:h}),`
+`,n.jsx(o,{of:l})]})}function P(t={}){const{wrapper:s}={...i(),...t.components};return s?n.jsx(s,{...t,children:n.jsx(e,{...t})}):e(t)}export{P as default};
