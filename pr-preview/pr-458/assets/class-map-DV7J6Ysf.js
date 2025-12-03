eeed849a1021ec0a6f1b817040fa2c052d296cb3
wkillerud <wkillerud@users.noreply.github.com>
2025-12-03 11:53:54 +0000

    Deploy preview for PR 458 🛫

---
+++ pr-preview/pr-458/assets/class-map-DV7J6Ysf.js
@@ -0,0 +1,5 @@
+import{d as i,f as o,t as a,T as h}from"./iframe-BBl4hpCo.js";/**
+ * @license
+ * Copyright 2018 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */const c=i(class extends o{constructor(s){if(super(s),s.type!==a.ATTRIBUTE||s.name!=="class"||s.strings?.length>2)throw Error("`classMap()` can only be used in the `class` attribute and must be the only part in the attribute.")}render(s){return" "+Object.keys(s).filter((e=>s[e])).join(" ")+" "}update(s,[e]){if(this.st===void 0){this.st=new Set,s.strings!==void 0&&(this.nt=new Set(s.strings.join(" ").split(/\s/).filter((t=>t!==""))));for(const t in e)e[t]&&!this.nt?.has(t)&&this.st.add(t);return this.render(e)}const n=s.element.classList;for(const t of this.st)t in e||(n.remove(t),this.st.delete(t));for(const t in e){const r=!!e[t];r===this.st.has(t)||this.nt?.has(t)||(r?(n.add(t),this.st.add(t)):(n.remove(t),this.st.delete(t)))}return h}});export{c as e};
