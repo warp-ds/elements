9f9c448b0aa60c3178ddc8b436fc82f3bfc62e42
joms <jo-emil@outlook.com>
2025-12-15 14:43:20 +0000

    Deploying to gh-pages from @ warp-ds/elements@d60702214486254491019ea1c6933bbaf4e05c36 🚀

--- assets/affix.stories-D8x93T4x.js
+++
@@ -1,5 +0,0 @@
-import{g as a,x as s}from"./iframe-DCH5CIb_.js";import{s as t,p as o}from"./utilities-biMu8cYY.js";import"./index-B4dz3c13.js";const{events:n,args:p,argTypes:f}=a("w-affix"),l={title:"Forms/Affix",render(r){return s`<w-affix ${t(o(r))}></w-affix>`},args:p,argTypes:f,parameters:{actions:{handles:n}}},e={args:{label:"kr"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    label: 'kr'
-  }
-}`,...e.parameters?.docs?.source}}};const i=["Default"],u=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:i,default:l},Symbol.toStringTag,{value:"Module"}));export{u as A,e as D};
