5d43c0da926a879908f0e4f927aa7fe3f9e8a86e
Richard Walker <digitalsadhu@gmail.com>
2025-12-17 16:13:57 +0100

    fix: add form associated to switch and refactor API surface

---
+++ packages/switch/switch.test.ts
@@ -0,0 +1,51 @@
+import { html } from 'lit';
+import { expect, test } from 'vitest';
+import { render } from 'vitest-browser-lit';
+
+import './index.js';
+
+test('can reset switch by resetting surrounding form', async () => {
+  const label = 'Test label';
+
+  render(html`
+    <form>
+      <w-switch
+        label=${label}
+        name="test"
+        value="some value"
+        checked
+      >
+      </w-switch>
+    </form>
+  `);
+
+  const form = document.querySelector('form') as HTMLFormElement;
+  const wSwitch = document.querySelector('w-switch') as HTMLElement & { value: string };
+
+  // sanity
+  expect(form).not.toBeNull();
+  expect(wSwitch).not.toBeNull();
+
+  // Initial value is "some value"
+  expect(wSwitch.value).toBe("some value");
+
+  // value should be associated with the form
+  const data = new FormData(form);
+  expect(Array.from(data.entries()).length).toBe(1);
+  
+  // uncheck
+  wSwitch.checked = false;
+  await wSwitch.updateComplete;
+  
+  // value should no longer be associated with the form
+  const data2 = new FormData(form);
+  expect(Array.from(data2.entries()).length).toBe(0);
+  
+  // Reset the form
+  form.reset();
+  await wSwitch.updateComplete;
+
+  // value should be associated with the form again after reset
+  const data3 = new FormData(form);
+  expect(Array.from(data3.entries()).length).toBe(1);
+});
