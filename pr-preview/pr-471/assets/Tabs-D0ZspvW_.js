e90b9ce78ea2a7e7ff9158bef47c29a7e713e74c
digitalsadhu <digitalsadhu@users.noreply.github.com>
2025-12-22 09:21:46 +0000

    Remove preview for PR 471 🛬

--- pr-preview/pr-471/assets/Tabs-D0ZspvW_.js
+++
@@ -1,8 +0,0 @@
-import{j as n}from"./index-_uh9JCM-.js";import{useMDXComponents as e}from"./index-pIcGub2r.js";import{M as i,C as o}from"./blocks-B1JXQEDB.js";import{T as r,D as m,W as c,M as h}from"./tabs.stories-Tz8_jI_s.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preload-helper-Ct5FWWRu.js";import"./iframe-Ci0W3GxM.js";import"./info-16-LIBuhj12.js";import"./query-NYTjQBeo.js";function a(s){const t={h1:"h1",h2:"h2",...e(),...s.components};return n.jsxs(n.Fragment,{children:[n.jsx(i,{of:r,name:"Docs"}),`
-`,n.jsx(t.h1,{id:"tabs",children:"Tabs"}),`
-`,n.jsx(t.h2,{id:"default",children:"Default"}),`
-`,n.jsx(o,{of:m}),`
-`,n.jsx(t.h2,{id:"withicons",children:"WithIcons"}),`
-`,n.jsx(o,{of:c}),`
-`,n.jsx(t.h2,{id:"manytabs",children:"ManyTabs"}),`
-`,n.jsx(o,{of:h})]})}function b(s={}){const{wrapper:t}={...e(),...s.components};return t?n.jsx(t,{...s,children:n.jsx(a,{...s})}):a(s)}export{b as default};
