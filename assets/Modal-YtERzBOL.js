5f8a321a97a05b2251b7e542ab5fc2d402e26c61
digitalsadhu <digitalsadhu@gmail.com>
2025-12-22 09:21:36 +0000

    Deploying to gh-pages from @ warp-ds/elements@1d025a66ec42e2c14999f4216d98e12e7cf812b8 🚀

---
+++ assets/Modal-YtERzBOL.js
@@ -0,0 +1,8 @@
+import{j as t}from"./index-_uh9JCM-.js";import{useMDXComponents as e}from"./index-pIcGub2r.js";import{M as r,C as a}from"./blocks-CUyLXFFw.js";import{M as s,D as m,W as h}from"./modal.stories-D8Oz6Q_y.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preload-helper-DvrX7W50.js";import"./iframe-CdiVsGC5.js";import"./utilities-BV-5S5bc.js";import"./query-NYTjQBeo.js";function i(o){const n={h1:"h1",h2:"h2",h3:"h3",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:s,name:"Docs"}),`
+`,t.jsx(n.h1,{id:"modal",children:"Modal"}),`
+`,t.jsx(n.p,{children:"Modals (or dialogs) display important information that users need to acknowledge."}),`
+`,t.jsx(n.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(n.h3,{id:"with-a-title-bar",children:"With a title bar"}),`
+`,t.jsx(a,{of:m}),`
+`,t.jsx(n.h3,{id:"with-an-image-at-the-top",children:"With an image at the top"}),`
+`,t.jsx(a,{of:h})]})}function g(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(i,{...o})}):i(o)}export{g as default};
