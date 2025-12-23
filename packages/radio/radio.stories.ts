c3bf5b7aaba865b80a60dae8f505dda78792c4af
Richard Walker <digitalsadhu@gmail.com>
2025-12-23 12:23:48 +0100

    docs: add stories for radio

--- packages/radio/radio.stories.ts
+++ packages/radio/radio.stories.ts
@@ -1,12 +1,22 @@
+import { spread } from '@open-wc/lit-helpers';
 import type { Meta, StoryObj } from '@storybook/web-components-vite';
+import { getStorybookHelpers } from '@wc-toolkit/storybook-helpers';
 import { html } from 'lit';
-import './index';
+import { prespread } from '../../.storybook/utilities.js';
+import type { WRadio } from './radio.js';
+import type { WRadioGroup } from './radio-group.js';
 
-export default {
+import './radio';
+import './radio-group';
+
+const { events: radioEvents, args: radioArgs, argTypes: radioArgTypes } = getStorybookHelpers<WRadio>('w-radio');
+const { events, args, argTypes } = getStorybookHelpers<WRadioGroup>('w-radio-group');
+
+const meta: Meta<typeof args> = {
   title: 'Forms/Radio',
-  render: () => {
+  render: (args) => {
     return html`
-      <w-radio-group label="Group" name="foobar" value="foo">
+      <w-radio-group ${spread(prespread(args))}>
         <w-radio value="foo">foo</w-radio>
         <w-radio value="bar">bar</w-radio>
       </w-radio-group>
@@ -14,4 +24,66 @@ export default {
   },
 } satisfies Meta;
 
-export const Default: StoryObj = {};
+export default meta;
+type Story = StoryObj<typeof args>;
+
+export const Default: Story = {
+  args: {
+    label: 'Group',
+    name: 'foobar',
+    value: 'foo',
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    label: 'Disabled',
+    disabled: true,
+  },
+};
+
+export const Required: Story = {
+  args: {},
+  render() {
+    return html`
+      <form>
+        <w-radio-group required>
+          <w-radio value="foo">foo</w-radio>
+          <w-radio value="bar">bar</w-radio>
+        </w-radio-group>
+        <button>click me</button>
+      </form>
+    `;
+  },
+};
+
+export const Appearance: Story = {
+  args: {},
+  render() {
+    return html`
+      <form>
+        <w-radio-group>
+          <w-radio value="foo" appearance="button">button</w-radio>
+          <w-radio value="bar" appearance="clickable">clickable</w-radio>
+        </w-radio-group>
+        <button>click me</button>
+      </form>
+    `;
+  },
+};
+
+export const DisabledGroup: Story = {
+  args: { disabled: true },
+};
+
+export const DisabledRadio: Story = {
+  args: {},
+  render() {
+    return html`
+      <w-radio-group>
+        <w-radio value="foo" disabled appearance="button">disabled</w-radio>
+        <w-radio value="bar" appearance="clickable">not disabled</w-radio>
+      </w-radio-group>
+    `;
+  },
+};
