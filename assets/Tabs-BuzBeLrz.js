d85fd4bc2b00ae761f8a3c40d55e746071cbbdcc
joms <jo-emil@outlook.com>
2025-12-29 10:42:50 +0000

    Deploying to gh-pages from @ warp-ds/elements@df0aba97fdd7fc6c24a3ae1b79e0cc795c18635d 🚀

---
+++ assets/Tabs-BuzBeLrz.js
@@ -0,0 +1,8 @@
+import{j as n}from"./index-_uh9JCM-.js";import{useMDXComponents as e}from"./index-pIcGub2r.js";import{M as i,C as o}from"./blocks-D9VcUqd7.js";import{T as r,D as m,W as c,M as h}from"./tabs.stories-Cv2tcQoC.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preload-helper-DvrX7W50.js";import"./iframe-Cho51tvR.js";import"./info-16-DenNW4Dd.js";import"./query-NYTjQBeo.js";function a(s){const t={h1:"h1",h2:"h2",...e(),...s.components};return n.jsxs(n.Fragment,{children:[n.jsx(i,{of:r,name:"Docs"}),`
+`,n.jsx(t.h1,{id:"tabs",children:"Tabs"}),`
+`,n.jsx(t.h2,{id:"default",children:"Default"}),`
+`,n.jsx(o,{of:m}),`
+`,n.jsx(t.h2,{id:"withicons",children:"WithIcons"}),`
+`,n.jsx(o,{of:c}),`
+`,n.jsx(t.h2,{id:"manytabs",children:"ManyTabs"}),`
+`,n.jsx(o,{of:h})]})}function b(s={}){const{wrapper:t}={...e(),...s.components};return t?n.jsx(t,{...s,children:n.jsx(a,{...s})}):a(s)}export{b as default};
