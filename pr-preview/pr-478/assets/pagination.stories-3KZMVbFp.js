667fca31db42fc6bd6bd9ff84ba21b3553050c28
digitalsadhu <digitalsadhu@users.noreply.github.com>
2026-01-09 08:52:51 +0000

    Deploy preview for PR 478 🛫

---
+++ pr-preview/pr-478/assets/pagination.stories-3KZMVbFp.js
@@ -0,0 +1,82 @@
+import{b as s,i as P,s as N,u as _,a as k,p as B,r as L,E as f,x as c,n as m,g as O}from"./iframe-CktIo7pi.js";import{s as D,p as M}from"./utilities-CTFLsIda.js";import"./chevron-left-16-RAARIUwH.js";var E={},F=JSON.parse('{"icon.title.chevron-double-left":["Dobbel pil til venstre"]}'),J=JSON.parse('{"icon.title.chevron-double-left":["Double leftward arrow"]}'),j=JSON.parse('{"icon.title.chevron-double-left":["Kaksi nuolta vasemmalle"]}'),C=JSON.parse('{"icon.title.chevron-double-left":["Dobbelt venstrepil"]}'),I=JSON.parse('{"icon.title.chevron-double-left":["Dubbel vänsterpil"]}'),y=["en","nb","fi","da","sv"],S="en",z=()=>{var a;let e;switch((a=process==null?void 0:E)==null?void 0:a.NMP_BRAND){case"FINN":e="nb";break;case"TORI":e="fi";break;case"BLOCKET":e="sv";break;case"DBA":e="da";break;default:e="en"}return e},$=()=>{var a;const e=(a=document?.location)==null?void 0:a.hostname;return e?.includes("finn")?"nb":e.includes("tori")?"fi":e.includes("blocket")?"sv":e.includes("dba")?"da":S},w=a=>y.find(e=>a===e||a.toLowerCase().includes(e))||$();function U(){var a;if(typeof window>"u"){const e=z();return w(e)}try{const e=(a=document?.documentElement)==null?void 0:a.lang,r=$();return y.includes(e)?w(e??r):(console.warn("Unsupported locale set in html lang tag, falling back to detection by hostname"),w(r))}catch(e){return console.warn("could not detect locale, falling back to source locale",e),S}}var A=(a,e,r,i,n,t)=>a==="nb"?r:a==="fi"?i:a==="da"?n:a==="sv"?t:e,T=(a,e,r,i,n)=>{const t=U(),o=A(t,a,e,r,i,n);s.load(t,o),s.activate(t)};T(J,F,j,C,I);var W=class extends P{render(){const a=s.t({message:"Double leftward arrow",id:"icon.title.chevron-double-left",comment:"Title for chevron-double-left icon"});return _`<svg xmlns="http://www.w3.org/2000/svg"width="16"height="16"fill="none"viewBox="0 0 16 16" part="w-icon-chevron-double-left-16-part">${N(`<title>${a}</title>`)}<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M7 13 2 8l5-5m7 10L9 8l5-5"></path></svg>`}};customElements.get("w-icon-chevron-double-left-16")||customElements.define("w-icon-chevron-double-left-16",W);const H=JSON.parse('{"pagination.aria.first-page":["Første side"],"pagination.aria.icon-suffix":["ikon"],"pagination.aria.next-page":["Næste side"],"pagination.aria.page":["Side ",["currentPage"]],"pagination.aria.pagination":["Sider"],"pagination.aria.prev-page":["Forrige side"]}'),K=JSON.parse('{"pagination.aria.first-page":["First page"],"pagination.aria.icon-suffix":["icon"],"pagination.aria.next-page":["Next page"],"pagination.aria.page":["Page ",["currentPage"]],"pagination.aria.pagination":["Pages"],"pagination.aria.prev-page":["Previous page"]}'),R=JSON.parse('{"pagination.aria.first-page":["Ensimmäinen sivu"],"pagination.aria.icon-suffix":["kuvake"],"pagination.aria.next-page":["Seuraava sivu"],"pagination.aria.page":["Sivu ",["currentPage"]],"pagination.aria.pagination":["Sivut"],"pagination.aria.prev-page":["Edellinen sivu"]}'),V=JSON.parse('{"pagination.aria.first-page":["Første side"],"pagination.aria.icon-suffix":["ikon"],"pagination.aria.next-page":["Neste side"],"pagination.aria.page":["Side ",["currentPage"]],"pagination.aria.pagination":["Sider"],"pagination.aria.prev-page":["Forrige side"]}'),q=JSON.parse('{"pagination.aria.first-page":["Första sidan"],"pagination.aria.icon-suffix":["ikon"],"pagination.aria.next-page":["Nästa sida"],"pagination.aria.page":["Sida ",["currentPage"]],"pagination.aria.pagination":["Sidor"],"pagination.aria.prev-page":["Föregående sida"]}'),G=k`*,:before,:after{--w-rotate:0;--w-rotate-x:0;--w-rotate-y:0;--w-rotate-z:0;--w-scale-x:1;--w-scale-y:1;--w-scale-z:1;--w-skew-x:0;--w-skew-y:0;--w-translate-x:0;--w-translate-y:0;--w-translate-z:0}.hover\\:bg-clip-padding:hover{-webkit-background-clip:padding-box;background-clip:padding-box}.hover\\:bg-\\[--w-color-button-pill-background-hover\\]:hover{background-color:var(--w-color-button-pill-background-hover)}.active\\:bg-\\[--w-color-button-pill-background-active\\]:active{background-color:var(--w-color-button-pill-background-active)}.border-0{border-width:0}.rounded-full{border-radius:9999px}.flex{display:flex}.inline-flex{display:inline-flex}.hover\\:no-underline:hover,.focus\\:no-underline:focus{text-decoration:none}.focusable:focus{outline:2px solid var(--w-s-color-border-focus);outline-offset:var(--w-outline-offset,1px)}.focusable:focus-visible{outline:2px solid var(--w-s-color-border-focus);outline-offset:var(--w-outline-offset,1px)}.focusable:not(:focus-visible){outline:none}.items-center{align-items:center}.justify-center{justify-content:center}.static{position:static}.s-bg-primary{background-color:var(--w-s-color-background-primary)}.s-text-inverted{color:var(--w-s-color-text-inverted)}.s-text-link{color:var(--w-s-color-text-link)}.s-icon{color:var(--w-s-color-icon)}.min-h-\\[44px\\]{min-height:44px}.min-w-\\[44px\\]{min-width:44px}.p-4{padding:.4rem}.p-8{padding:.8rem}.visible{visibility:visible}.pointer-events-none{pointer-events:none}.sr-only{clip:rect(0,0,0,0);white-space:nowrap;border-width:0;width:1px;height:1px;margin:-1px;padding:0;position:absolute;overflow:hidden}.transition-colors{transition-property:color,background-color,border-color,text-decoration-color,fill,stroke;transition-duration:.15s;transition-timing-function:cubic-bezier(.4,0,.2,1)}.ease-in-out{transition-timing-function:cubic-bezier(.4,0,.2,1)}`;var Q=Object.defineProperty,h=(a,e,r,i)=>{for(var n=void 0,t=a.length-1,o;t>=0;t--)(o=a[t])&&(n=o(e,r,n)||n);return n&&Q(e,r,n),n};const x=s._({id:"pagination.aria.icon-suffix",message:"icon",comment:"Suffix added at the end of icon titles when img semantics are lost on an html element"}),v="hover:no-underline focus:no-underline focusable inline-flex justify-center items-center transition-colors ease-in-out min-h-[44px] min-w-[44px] p-4 rounded-full border-0 hover:bg-clip-padding";class b extends P{constructor(){super(),this.currentPageNumber=1,this.visiblePages=7,B(K,V,R,H,q)}static{this.styles=[L,G,k`
+      w-icon-chevron-left-16,
+      w-icon-chevron-double-left-16,
+      w-icon-chevron-right-16 {
+        height: 16px;
+      }
+    `]}get shouldShowShowFirstPageButton(){return this.currentPageNumber-2>0}get shouldShowPreviousPageButton(){return this.currentPageNumber-1>0}get shouldShowNextPageButton(){return this.currentPageNumber<this.pages}get currentPageIndex(){return this.currentPageNumber-1}get visiblePageNumbers(){if(this.pages<=this.visiblePages)return Array.from({length:this.pages},(n,t)=>t+1);const e=Math.floor(this.visiblePages/2);let r=Math.max(1,this.currentPageNumber-e);const i=Math.min(this.pages,r+this.visiblePages-1);return i-r+1<this.visiblePages&&(r=Math.max(1,i-this.visiblePages+1)),Array.from({length:i-r+1},(n,t)=>r+t)}#e(e){const r=e.target.getAttribute("data-page-number");this.dispatchEvent(new CustomEvent("page-click",{detail:{clickedPage:r},bubbles:!0,composed:!0}))}render(){const e=this.visiblePageNumbers;return c`<nav class="flex items-center justify-center p-8" @click="${this.#e}">
+      <h1 class="sr-only">
+        ${s._({id:"pagination.aria.pagination",message:"Pages",comment:"Default screenreader message for pagination container in the pagination component"})}
+      </h1>
+      <div class="flex items-center s-text-link">
+        ${this.shouldShowShowFirstPageButton?c`<a
+              data-page-number="1"
+              href="${this.baseUrl}1"
+              class="${v+" s-icon hover:bg-[--w-color-button-pill-background-hover] active:bg-[--w-color-button-pill-background-active]"}">
+              <span class="sr-only"
+                >${s._({id:"pagination.aria.first-page",message:"First page",comment:"Default screenreader message for first page link in the pagination component"})},</span
+              >
+              <w-icon-chevron-double-left-16 class="pointer-events-none flex items-center"></w-icon-chevron-double-left-16>
+              <span class="sr-only">${x}</span>
+            </a>`:f}
+        ${this.shouldShowPreviousPageButton?c`<a
+              data-page-number="${this.currentPageNumber-1}"
+              href="${this.baseUrl}${this.currentPageNumber-1}"
+              class="${v+" s-icon hover:bg-[--w-color-button-pill-background-hover] active:bg-[--w-color-button-pill-background-active]"}">
+              <span class="sr-only"
+                >${s._({id:"pagination.aria.prev-page",message:"Previous page",comment:"Default screenreader message for previous page link in the pagination component"})},</span
+              >
+              <w-icon-chevron-left-16 class="pointer-events-none flex items-center"></w-icon-chevron-left-16>
+              <span class="sr-only">${x}</span>
+            </a>`:f}
+        ${e.map(r=>{const i=r===this.currentPageNumber,n=`${this.baseUrl}${r}`;let t=v;i?t+=" s-bg-primary s-text-inverted":t+=" hover:bg-[--w-color-button-pill-background-hover] active:bg-[--w-color-button-pill-background-active]";const o=s._({id:"pagination.aria.page",message:"Page {currentPage}",values:{currentPage:r},comment:"Default screenreader message for page link in the pagination component"});return c`<a
+            data-page-number="${r}"
+            aria-label="${o}"
+            href="${n}"
+            class="${t}"
+            aria-current="${i?"page":""}"
+            >${r}</a
+          >`})}
+        ${this.shouldShowNextPageButton?c`<a
+              data-page-number="${this.currentPageNumber+1}"
+              href="${this.baseUrl}${this.currentPageNumber+1}"
+              class="${v+" s-icon hover:bg-[--w-color-button-pill-background-hover] active:bg-[--w-color-button-pill-background-active]"}">
+              <span class="sr-only">
+                ${s._({id:"pagination.aria.next-page",message:"Next page",comment:"Default screenreader message for next page link in the pagination component"})},</span
+              >
+              <w-icon-chevron-right-16 class="pointer-events-none flex items-center"></w-icon-chevron-right-16>
+              <span class="sr-only">${x}</span>
+            </a>`:f}
+      </div>
+    </nav>`}}h([m({type:String,reflect:!0,attribute:"base-url"})],b.prototype,"baseUrl");h([m({type:Number,reflect:!0})],b.prototype,"pages");h([m({type:Number,reflect:!0,attribute:"current-page"})],b.prototype,"currentPageNumber");h([m({type:Number,reflect:!0,attribute:"visible-pages"})],b.prototype,"visiblePages");customElements.get("w-pagination")||customElements.define("w-pagination",b);const{events:X,args:Y,argTypes:Z}=O("w-pagination"),ee={title:"Navigation/Pagination",render(a){return c`<w-pagination ${D(M(a))}></w-pagination>`},args:Y,argTypes:Z,parameters:{actions:{handles:X}}},l={args:{"current-page-number":1,pages:5,"base-url":"/search?page="}},g={args:{"current-page-number":3,pages:7,"base-url":"/search?page="}},u={args:{"current-page-number":10,pages:10,"base-url":"/search?page="}},p={args:{"current-page-number":15,pages:50,"visible-pages":5,"base-url":"/search?page="}},d={args:{"current-page-number":1,pages:1,"base-url":"/search?page="}};l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    'current-page-number': 1,
+    pages: 5,
+    'base-url': '/search?page='
+  }
+}`,...l.parameters?.docs?.source}}};g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
+  args: {
+    'current-page-number': 3,
+    pages: 7,
+    'base-url': '/search?page='
+  }
+}`,...g.parameters?.docs?.source}}};u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    'current-page-number': 10,
+    pages: 10,
+    'base-url': '/search?page='
+  }
+}`,...u.parameters?.docs?.source}}};p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    'current-page-number': 15,
+    pages: 50,
+    'visible-pages': 5,
+    'base-url': '/search?page='
+  }
+}`,...p.parameters?.docs?.source}}};d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    'current-page-number': 1,
+    pages: 1,
+    'base-url': '/search?page='
+  }
+}`,...d.parameters?.docs?.source}}};const ae=["BasicPagination","MiddlePageSelected","LastPageSelected","ManyPagesWithLimitedVisible","SinglePage"],ie=Object.freeze(Object.defineProperty({__proto__:null,BasicPagination:l,LastPageSelected:u,ManyPagesWithLimitedVisible:p,MiddlePageSelected:g,SinglePage:d,__namedExportsOrder:ae,default:ee},Symbol.toStringTag,{value:"Module"}));export{l as B,u as L,g as M,ie as P,d as S,p as a};
