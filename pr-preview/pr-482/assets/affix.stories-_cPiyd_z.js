0e8ca8f5812d68f6056aac7d032b57bcbc8ce396
digitalsadhu <digitalsadhu@users.noreply.github.com>
2026-01-09 14:15:39 +0000

    Remove preview for PR 482 🛬

--- pr-preview/pr-482/assets/affix.stories-_cPiyd_z.js
+++
@@ -1,5 +0,0 @@
-import{g as a,x as s}from"./iframe-D0WE18GI.js";import{s as t,p as o}from"./utilities-BQqZe3ue.js";import"./index-DI0cpkCL.js";const{events:n,args:p,argTypes:f}=a("w-affix"),l={title:"Forms/Affix",render(r){return s`<w-affix ${t(o(r))}></w-affix>`},args:p,argTypes:f,parameters:{actions:{handles:n}}},e={args:{label:"kr"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    label: 'kr'
-  }
-}`,...e.parameters?.docs?.source}}};const i=["Default"],u=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:i,default:l},Symbol.toStringTag,{value:"Module"}));export{u as A,e as D};
