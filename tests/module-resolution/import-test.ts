ab0262d51a221d678df75c705e137f0149f29080
Richard Walker <digitalsadhu@gmail.com>
2026-01-09 15:14:54 +0100

    chore: remove all unreleased components from release (#482)
    * chore: remove all unreleased components from release
    
    * chore: also set correct import tests
    
    ---------
    
    Co-authored-by: Richard Walker <richard.walker@m10s.io>

--- tests/module-resolution/import-test.ts
+++ tests/module-resolution/import-test.ts
@@ -6,23 +6,25 @@ import { Box } from '@warp-ds/elements/react/box';
 import { Breadcrumbs } from '@warp-ds/elements/react/breadcrumbs';
 import { Button } from '@warp-ds/elements/react/button';
 import { Card } from '@warp-ds/elements/react/card';
-import { Combobox } from '@warp-ds/elements/react/combobox';
 import { DatePicker } from '@warp-ds/elements/react/datepicker';
-import { DeadToggle } from '@warp-ds/elements/react/deadtoggle';
 import { Expandable } from '@warp-ds/elements/react/expandable';
 import { Link } from '@warp-ds/elements/react/link';
 import { Modal } from '@warp-ds/elements/react/modal';
-import { PageIndicator } from '@warp-ds/elements/react/pageindicator';
-import { Pagination } from '@warp-ds/elements/react/pagination';
 import { Pill } from '@warp-ds/elements/react/pill';
-import { Checkbox } from '@warp-ds/elements/react/checkbox';
-import { Radio } from '@warp-ds/elements/react/radio';
 import { Select } from '@warp-ds/elements/react/select';
-import { Slider } from '@warp-ds/elements/react/slider';
-import { StepIndicator } from "@warp-ds/elements/react/stepindicator";
-import { Switch } from '@warp-ds/elements/react/switch';
-import { Tabs } from '@warp-ds/elements/react/tabs';
 import { TextField } from '@warp-ds/elements/react/textfield';
+
+// unreleased
+// import { Combobox } from '@warp-ds/elements/react/combobox';
+// import { DeadToggle } from '@warp-ds/elements/react/deadtoggle';
+// import { PageIndicator } from '@warp-ds/elements/react/pageindicator';
+// import { Pagination } from '@warp-ds/elements/react/pagination';
+// import { Checkbox } from '@warp-ds/elements/react/checkbox';
+// import { Radio } from '@warp-ds/elements/react/radio';
+// import { Slider } from '@warp-ds/elements/react/slider';
+// import { StepIndicator } from "@warp-ds/elements/react/stepindicator";
+// import { Switch } from '@warp-ds/elements/react/switch';
+// import { Tabs } from '@warp-ds/elements/react/tabs';
 
 // Type assertions to ensure imports are working
 const _affix: typeof Affix = Affix;
@@ -33,22 +35,24 @@ const _box: typeof Box = Box;
 const _breadcrumbs: typeof Breadcrumbs = Breadcrumbs;
 const _button: typeof Button = Button;
 const _card: typeof Card = Card;
-const _combobox: typeof Combobox = Combobox;
 const _datepicker: typeof DatePicker = DatePicker;
-const _deadToggle: typeof DeadToggle = DeadToggle;
 const _expandable: typeof Expandable = Expandable;
 const _link: typeof Link = Link;
 const _modal: typeof Modal = Modal;
-const _pageIndicator: typeof PageIndicator = PageIndicator;
-const _pagination: typeof Pagination = Pagination;
 const _pill: typeof Pill = Pill;
-const _checkbox: typeof Checkbox = Checkbox;
-const _radio: typeof Radio = Radio;
 const _select: typeof Select = Select;
-const _slider: typeof Slider = Slider;
-const _stepIndicator: typeof StepIndicator = StepIndicator;
-const _switch: typeof Switch = Switch;
-const _tabs: typeof Tabs = Tabs;
 const _textField: typeof TextField = TextField;
+
+// unreleased
+// const _combobox: typeof Combobox = Combobox;
+// const _deadToggle: typeof DeadToggle = DeadToggle;
+// const _pageIndicator: typeof PageIndicator = PageIndicator;
+// const _pagination: typeof Pagination = Pagination;
+// const _checkbox: typeof Checkbox = Checkbox;
+// const _radio: typeof Radio = Radio;
+// const _slider: typeof Slider = Slider;
+// const _stepIndicator: typeof StepIndicator = StepIndicator;
+// const _switch: typeof Switch = Switch;
+// const _tabs: typeof Tabs = Tabs;
 
 console.log('✓ All imports resolved successfully');
