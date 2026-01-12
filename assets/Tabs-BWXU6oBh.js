dbf889ff451d85bd1803a3bf7c848be68a5d6199
digitalsadhu <digitalsadhu@gmail.com>
2026-01-12 11:22:46 +0000

    Deploying to gh-pages from @ warp-ds/elements@69711550c333dea16cdda194311d6798796c5d7a 🚀

---
+++ assets/Tabs-BWXU6oBh.js
@@ -0,0 +1,8 @@
+import{j as n}from"./index-_uh9JCM-.js";import{useMDXComponents as e}from"./index-pIcGub2r.js";import{M as i,C as o}from"./blocks-DoRaW1R2.js";import{T as r,D as m,W as c,M as h}from"./tabs.stories-DF0N03cr.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preload-helper-DvrX7W50.js";import"./iframe-DJZ5ZW4w.js";import"./info-16-DxUM1JVw.js";import"./query-NYTjQBeo.js";function a(s){const t={h1:"h1",h2:"h2",...e(),...s.components};return n.jsxs(n.Fragment,{children:[n.jsx(i,{of:r,name:"Docs"}),`
+`,n.jsx(t.h1,{id:"tabs",children:"Tabs"}),`
+`,n.jsx(t.h2,{id:"default",children:"Default"}),`
+`,n.jsx(o,{of:m}),`
+`,n.jsx(t.h2,{id:"withicons",children:"WithIcons"}),`
+`,n.jsx(o,{of:c}),`
+`,n.jsx(t.h2,{id:"manytabs",children:"ManyTabs"}),`
+`,n.jsx(o,{of:h})]})}function b(s={}){const{wrapper:t}={...e(),...s.components};return t?n.jsx(t,{...s,children:n.jsx(a,{...s})}):a(s)}export{b as default};
