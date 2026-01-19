24b2d5d4efb4a6ccd4fcbb28e9557d0edbe4f4d5
Richard Walker <richard.walker@m10s.io>
2026-01-19 17:20:38 +0100

    chore: fixes to refactor

--- packages/modal/index.ts
+++ packages/modal/index.ts
@@ -1,3 +1,9 @@
+// Side-effect imports to register custom elements
+import './modal.js';
+import '../modal-header/modal-header.js';
+import '../modal-footer/modal-footer.js';
+
+// Re-exports for backward compatibility
 import { ModalFooter } from '../modal-footer/modal-footer.js';
 import { ModalHeader } from '../modal-header/modal-header.js';
 import { ModalMain } from './modal.js';
--- packages/toast/index.ts
+++ packages/toast/index.ts
@@ -1,3 +1,8 @@
+// Side-effect imports to register custom elements
+import './toast.js';
+import '../toast-container/toast-container.js';
+
+// Re-exports for backward compatibility
 export { removeToast, toast, updateToast } from './api.js';
 export { WarpToast } from './toast.js';
 export { WarpToastContainer } from '../toast-container/toast-container.js';
