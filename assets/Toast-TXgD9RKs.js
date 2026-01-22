921ddbc845cc1451047d83a37702bfa3ed9d7d02
imprashast <imprashast@gmail.com>
2026-01-22 10:09:32 +0000

    Deploying to gh-pages from @ warp-ds/elements@7b78621231967b7b0be1c7e557ace67ed62ac04e 🚀

---
+++ assets/Toast-TXgD9RKs.js
@@ -0,0 +1,12 @@
+import{j as e}from"./index-D7-HiEGT.js";import{useMDXComponents as s}from"./index-B332Pdph.js";import{M as a,C as r}from"./blocks-I46I7xwa.js";import{T as i,D as p}from"./toast.stories-tNe_7jXn.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preload-helper-DvrX7W50.js";import"./iframe-Cd5OrALp.js";import"./utilities-D-7ba41A.js";import"./button-DtbxvCKz.js";import"./link-CBPSmiq8.js";import"./class-map-DuE6Iz2t.js";import"./FormControlMixin-DJnJZbro.js";function n(o){const t={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...s(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:i,name:"Docs"}),`
+`,e.jsx(t.h1,{id:"toast",children:"Toast"}),`
+`,e.jsx(t.p,{children:"Toast is a bit different from other packages in Warp Elements. You need to import functions from the package and call them as needed."}),`
+`,e.jsx(t.p,{children:"The toast is intended to be used programmatically. JavaScript APIs are provided to create, update and remove toasts from a page while managing things like placement on the page for you."}),`
+`,e.jsx(t.h2,{id:"import-functions-for-working-with-toasts",children:"Import functions for working with toasts"}),`
+`,e.jsx(t.p,{children:"Be sure to import the elements package first as the toast APIs depend on this package."}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-js",children:`import from '@warp-ds/elements/components/toast';
+`})}),`
+`,e.jsx(t.p,{children:"Once you have imported the elements package, import the toast api package."}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-js",children:`import { toast, removeToast, updateToast } from '@warp-ds/elements/toast';
+`})}),`
+`,e.jsx(r,{of:p})]})}function T(o={}){const{wrapper:t}={...s(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(n,{...o})}):n(o)}export{T as default};
