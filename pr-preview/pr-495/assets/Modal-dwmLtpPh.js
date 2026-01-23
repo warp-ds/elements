e6afd05cf9ac67adec7da602f6e6dbbc7b3e39bf
digitalsadhu <digitalsadhu@users.noreply.github.com>
2026-01-23 09:51:09 +0000

    Deploy preview for PR 495 🛫

--- pr-preview/pr-495/assets/Modal-dwmLtpPh.js
+++
@@ -1,8 +0,0 @@
-import{j as t}from"./index-D7-HiEGT.js";import{useMDXComponents as e}from"./index-B332Pdph.js";import{M as r,C as a}from"./blocks-DtcHAPkb.js";import{M as s,D as m,W as h}from"./modal.stories-vun0O9nU.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preload-helper-Ct5FWWRu.js";import"./iframe-MOGdmR_E.js";import"./utilities-DiJiVRk7.js";import"./query-NYTjQBeo.js";function i(o){const n={h1:"h1",h2:"h2",h3:"h3",p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:s,name:"Docs"}),`
-`,t.jsx(n.h1,{id:"modal",children:"Modal"}),`
-`,t.jsx(n.p,{children:"Modals (or dialogs) display important information that users need to acknowledge."}),`
-`,t.jsx(n.h2,{id:"variants",children:"Variants"}),`
-`,t.jsx(n.h3,{id:"with-a-title-bar",children:"With a title bar"}),`
-`,t.jsx(a,{of:m}),`
-`,t.jsx(n.h3,{id:"with-an-image-at-the-top",children:"With an image at the top"}),`
-`,t.jsx(a,{of:h})]})}function g(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(i,{...o})}):i(o)}export{g as default};
