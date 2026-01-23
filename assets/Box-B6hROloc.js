2fea0792b6e2edd208941d97c3f418784513acb8
digitalsadhu <digitalsadhu@gmail.com>
2026-01-23 10:39:25 +0000

    Deploying to gh-pages from @ warp-ds/elements@96de461a5dc9f8f1b898d546da8b8502d386ad03 🚀

---
+++ assets/Box-B6hROloc.js
@@ -0,0 +1,11 @@
+import{j as e}from"./index-D7-HiEGT.js";import{useMDXComponents as r}from"./index-B332Pdph.js";import{M as i,C as n}from"./blocks-B2yeHzPE.js";import{B as a,D as l,I as c,N as d,a as p,b as m}from"./box.stories-DlJ52K8k.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preload-helper-DvrX7W50.js";import"./iframe-6VD7Kkfo.js";import"./utilities-El2wpYtL.js";function t(s){const o={a:"a",code:"code",h1:"h1",h2:"h2",p:"p",...r(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:a,name:"Docs"}),`
+`,e.jsx(o.h1,{id:"box",children:"Box"}),`
+`,e.jsx(o.p,{children:"Box is a layout component used for separating content areas on a page."}),`
+`,e.jsx(o.h2,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(o.p,{children:["Use the ARIA role attribute to provide semantic meaning to a box element. If no role property is supplied, it will default to ",e.jsx(o.code,{children:'role="region"'}),". To remove the default role attribute, supply an empty role (",e.jsx(o.code,{children:'role=""'}),"). Consider using more suitable roles for your specific Box, as described on ",e.jsx(o.a,{href:"https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles#aria_role_types",rel:"nofollow",children:"MDN WAI-ARIA Roles"}),"."]}),`
+`,e.jsx(o.h2,{id:"visual-options",children:"Visual options"}),`
+`,e.jsx(n,{of:l}),`
+`,e.jsx(n,{of:c}),`
+`,e.jsx(n,{of:d}),`
+`,e.jsx(n,{of:p}),`
+`,e.jsx(n,{of:m})]})}function g(s={}){const{wrapper:o}={...r(),...s.components};return o?e.jsx(o,{...s,children:e.jsx(t,{...s})}):t(s)}export{g as default};
