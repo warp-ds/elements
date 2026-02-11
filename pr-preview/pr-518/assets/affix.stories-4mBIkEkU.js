cce8ac4aa38372fab1baf1f8704c5a047e50b41b
digitalsadhu <digitalsadhu@users.noreply.github.com>
2026-02-11 14:43:54 +0000

    Deploy preview for PR 518 🛫

--- pr-preview/pr-518/assets/affix.stories-4mBIkEkU.js
+++
@@ -1,5 +0,0 @@
-import{g as a,b as s}from"./iframe-DYb0rld_.js";import{s as t,p as o}from"./utilities-BIbceZT9.js";import"./affix-YZXDTVEQ.js";const{events:n,args:p,argTypes:f}=a("w-affix"),l={title:"Forms/Affix",render(r){return s`<w-affix ${t(o(r))}></w-affix>`},args:p,argTypes:f,parameters:{actions:{handles:n}}},e={args:{label:"kr"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    label: 'kr'
-  }
-}`,...e.parameters?.docs?.source}}};const i=["Default"],u=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:i,default:l},Symbol.toStringTag,{value:"Module"}));export{u as A,e as D};
--- pr-preview/pr-518/assets/if-defined-D7GrYlZk.js
+++
@@ -1,5 +0,0 @@
-import{A as r}from"./iframe-DYb0rld_.js";/**
- * @license
- * Copyright 2018 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */const m=o=>o??r;export{m as o};
