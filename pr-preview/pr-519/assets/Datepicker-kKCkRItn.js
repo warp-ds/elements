6d13b35f4a52391209dc606a53a0ccddb18a713c
wkillerud <wkillerud@users.noreply.github.com>
2026-02-12 11:10:18 +0000

    Deploy preview for PR 519 🛫

--- pr-preview/pr-519/assets/Datepicker-kKCkRItn.js
+++
@@ -1,4 +0,0 @@
-import{j as t}from"./index-D7-HiEGT.js";import{useMDXComponents as n}from"./index-B332Pdph.js";import{M as i,C as s}from"./blocks-B2NBx-QB.js";import{D as a,a as m}from"./datepicker.stories-CfMkMgBe.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preload-helper-Ct5FWWRu.js";import"./iframe-C4irmNs9.js";import"./utilities-BBKY5x3d.js";import"./button-8h9S6Qmi.js";import"./link-QvLpSAMZ.js";import"./FormControlMixin-DJnJZbro.js";import"./query-NYTjQBeo.js";import"./if-defined-DDCy1ZqI.js";function r(e){const o={h1:"h1",p:"p",...n(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:a,name:"Docs"}),`
-`,t.jsx(o.h1,{id:"datepicker",children:"Datepicker"}),`
-`,t.jsx(o.p,{children:"A date picker allows the user to select a specific calendar date."}),`
-`,t.jsx(s,{of:m})]})}function X(e={}){const{wrapper:o}={...n(),...e.components};return o?t.jsx(o,{...e,children:t.jsx(r,{...e})}):r(e)}export{X as default};
