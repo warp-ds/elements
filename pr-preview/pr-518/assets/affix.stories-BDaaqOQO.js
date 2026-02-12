5bf0468900177b58674d981225b4b12201eca90c
digitalsadhu <digitalsadhu@users.noreply.github.com>
2026-02-12 13:55:52 +0000

    Deploy preview for PR 518 🛫

--- pr-preview/pr-518/assets/affix.stories-BDaaqOQO.js
+++
@@ -1,5 +0,0 @@
-import{g as a,b as s}from"./iframe-BAyV81Ij.js";import{s as t,p as o}from"./utilities-BhvVD4fm.js";import"./affix-Chj0ZXiJ.js";const{events:n,args:p,argTypes:f}=a("w-affix"),l={title:"Forms/Affix",render(r){return s`<w-affix ${t(o(r))}></w-affix>`},args:p,argTypes:f,parameters:{actions:{handles:n}}},e={args:{label:"kr"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    label: 'kr'
-  }
-}`,...e.parameters?.docs?.source}}};const i=["Default"],u=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:i,default:l},Symbol.toStringTag,{value:"Module"}));export{u as A,e as D};
