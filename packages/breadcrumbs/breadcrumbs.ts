1de957070f150b678a6c4d0f8b0f09c702f07eec
William Killerud <william.killerud@m10s.io>
2026-02-23 13:34:58 +0100

    fix: add role, label in the light DOM

--- packages/breadcrumbs/breadcrumbs.ts
+++ packages/breadcrumbs/breadcrumbs.ts
@@ -1,26 +1,26 @@
 // @warp-css;
 
-import { i18n } from '@lingui/core';
-import { interleave } from '@warp-ds/core/breadcrumbs';
-import { html, LitElement, TemplateResult } from 'lit';
-import { property } from 'lit/decorators.js';
+import { i18n } from "@lingui/core";
+import { interleave } from "@warp-ds/core/breadcrumbs";
+import { html, LitElement, TemplateResult } from "lit";
+import { property } from "lit/decorators.js";
 
-import { activateI18n } from '../i18n';
-import { reset } from '../styles.js';
+import { activateI18n } from "../i18n";
+import { reset } from "../styles.js";
 
-import { messages as daMessages } from './locales/da/messages.mjs';
-import { messages as enMessages } from './locales/en/messages.mjs';
-import { messages as fiMessages } from './locales/fi/messages.mjs';
-import { messages as nbMessages } from './locales/nb/messages.mjs';
-import { messages as svMessages } from './locales/sv/messages.mjs';
-import { styles } from './styles.js';
+import { messages as daMessages } from "./locales/da/messages.mjs";
+import { messages as enMessages } from "./locales/en/messages.mjs";
+import { messages as fiMessages } from "./locales/fi/messages.mjs";
+import { messages as nbMessages } from "./locales/nb/messages.mjs";
+import { messages as svMessages } from "./locales/sv/messages.mjs";
+import { styles } from "./styles.js";
 
 const ccBreadcrumbs = {
-  wrapper: 'flex space-x-8',
-  text: 's-text',
-  link: 's-text-link',
-  separator: 'select-none s-icon',
-  a11y: 'sr-only',
+  wrapper: "flex space-x-8",
+  text: "s-text",
+  link: "s-text-link",
+  separator: "select-none s-icon",
+  a11y: "sr-only",
 };
 
 const separator = html`<span class=${ccBreadcrumbs.separator}>/</span>`;
@@ -31,7 +31,7 @@ const separator = html`<span class=${ccBreadcrumbs.separator}>/</span>`;
  * [See Storybook for usage examples](https://warp-ds.github.io/elements/?path=/docs/navigation-breadcrumbs--docs)
  */
 class WarpBreadcrumbs extends LitElement {
-  @property({ attribute: 'aria-label', type: String })
+  @property({ attribute: "aria-label", type: String, reflect: true })
   ariaLabel: string;
 
   static styles = [reset, styles];
@@ -39,12 +39,6 @@ class WarpBreadcrumbs extends LitElement {
   constructor() {
     super();
     activateI18n(enMessages, nbMessages, fiMessages, daMessages, svMessages);
-
-    this.ariaLabel = i18n._({
-      id: 'breadcrumbs.ariaLabel',
-      message: 'You are here',
-      comment: 'Default screen reader message for the breadcrumb component',
-    });
   }
 
   /** @internal */
@@ -52,16 +46,32 @@ class WarpBreadcrumbs extends LitElement {
 
   connectedCallback() {
     super.connectedCallback();
+    this.setAttribute("role", "navigation");
+    this.setAttribute(
+      "aria-label",
+      this.ariaLabel ||
+        i18n._({
+          id: "breadcrumbs.ariaLabel",
+          message: "You are here",
+          comment: "Default screen reader message for the breadcrumb component",
+        }),
+    );
     // Grab existing children at the point that the component is added to the page
     const flattenedChildren = Array.from(this.children)
       .flat(Number.POSITIVE_INFINITY)
       .filter((child) => child);
     const styledChildren = flattenedChildren.map((child, index) => {
-      if (typeof child === 'string') {
+      if (typeof child === "string") {
         const isLastEl = index === this.children.length - 1;
-        return html`<span class=${ccBreadcrumbs.text} aria-current=${isLastEl ? 'page' : undefined}>${child}</span>`;
+        return html`<span
+          class=${ccBreadcrumbs.text}
+          aria-current=${isLastEl ? "page" : undefined}
+          >${child}</span
+        >`;
       }
-      child.classList.add(child.tagName === 'A' ? ccBreadcrumbs.link : ccBreadcrumbs.text);
+      child.classList.add(
+        child.tagName === "A" ? ccBreadcrumbs.link : ccBreadcrumbs.text,
+      );
       return child;
     });
 
@@ -72,15 +82,17 @@ class WarpBreadcrumbs extends LitElement {
   render() {
     return html`
       <nav aria-labelledby="breadCrumbLabel">
-        <h2 id="breadCrumbLabel" class=${ccBreadcrumbs.a11y}>${this.ariaLabel}</h2>
+        <h2 id="breadCrumbLabel" class=${ccBreadcrumbs.a11y}>
+          ${this.ariaLabel}
+        </h2>
         <div class=${ccBreadcrumbs.wrapper}>${this._children}</div>
       </nav>
     `;
   }
 }
 
-if (!customElements.get('w-breadcrumbs')) {
-  customElements.define('w-breadcrumbs', WarpBreadcrumbs);
+if (!customElements.get("w-breadcrumbs")) {
+  customElements.define("w-breadcrumbs", WarpBreadcrumbs);
 }
 
 export { WarpBreadcrumbs };
