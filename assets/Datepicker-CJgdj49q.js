10b7c03e4f80faef9c495ed1664670a1c8f668b6
digitalsadhu <digitalsadhu@gmail.com>
2026-02-23 07:18:09 +0000

    Deploying to gh-pages from @ warp-ds/elements@3f1402bf581ec85f14877f1dba0a4482176394e9 🚀

---
+++ assets/Datepicker-CJgdj49q.js
@@ -0,0 +1,4 @@
+import{j as t}from"./index-D7-HiEGT.js";import{useMDXComponents as n}from"./index-B332Pdph.js";import{M as i,C as s}from"./blocks-BaHRHNVY.js";import{D as a,a as m}from"./datepicker.stories-D_cNxIUJ.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preload-helper-DvrX7W50.js";import"./iframe-DKnO4LDC.js";import"./utilities-CDS8QV12.js";import"./button-DODOx4JQ.js";import"./link-Dl_9_384.js";import"./FormControlMixin-DJnJZbro.js";import"./query-NYTjQBeo.js";import"./if-defined-CNyhSGLU.js";function r(e){const o={h1:"h1",p:"p",...n(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:a,name:"Docs"}),`
+`,t.jsx(o.h1,{id:"datepicker",children:"Datepicker"}),`
+`,t.jsx(o.p,{children:"A date picker allows the user to select a specific calendar date."}),`
+`,t.jsx(s,{of:m})]})}function X(e={}){const{wrapper:o}={...n(),...e.components};return o?t.jsx(o,{...e,children:t.jsx(r,{...e})}):r(e)}export{X as default};
