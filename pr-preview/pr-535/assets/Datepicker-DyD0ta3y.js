b4312b1cccd5a45e481d0cab279b61248312930d
digitalsadhu <digitalsadhu@users.noreply.github.com>
2026-02-27 07:47:05 +0000

    Remove preview for PR 535 🛬

--- pr-preview/pr-535/assets/Datepicker-DyD0ta3y.js
+++
@@ -1,4 +0,0 @@
-import{j as t}from"./index-D7-HiEGT.js";import{useMDXComponents as n}from"./index-B332Pdph.js";import{M as i,C as s}from"./blocks-DbgzvQsb.js";import{D as a,a as m}from"./datepicker.stories-ZkVEfXr7.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preload-helper-Ct5FWWRu.js";import"./iframe-BZGQ9dA9.js";import"./utilities-DxQ0BlYP.js";import"./button-f5m4HERl.js";import"./link-BcYpa3nF.js";import"./FormControlMixin-DJnJZbro.js";import"./query-NYTjQBeo.js";import"./if-defined-DYwidIxu.js";function r(e){const o={h1:"h1",p:"p",...n(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:a,name:"Docs"}),`
-`,t.jsx(o.h1,{id:"datepicker",children:"Datepicker"}),`
-`,t.jsx(o.p,{children:"A date picker allows the user to select a specific calendar date."}),`
-`,t.jsx(s,{of:m})]})}function X(e={}){const{wrapper:o}={...n(),...e.components};return o?t.jsx(o,{...e,children:t.jsx(r,{...e})}):r(e)}export{X as default};
