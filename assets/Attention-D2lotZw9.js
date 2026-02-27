db17d17b45fe718cd4e947cfd9c0ec5f798a12d2
digitalsadhu <digitalsadhu@gmail.com>
2026-02-27 10:59:26 +0000

    Deploying to gh-pages from @ warp-ds/elements@9877e476c54c37f8abd516ed4257512b9bd9014d 🚀

---
+++ assets/Attention-D2lotZw9.js
@@ -0,0 +1,15 @@
+import{j as t}from"./index-D7-HiEGT.js";import{useMDXComponents as s}from"./index-B332Pdph.js";import{M as l,C as o}from"./blocks-C6XIbgOK.js";import{A as a,T as r,C as h,H as c,D as d,a as p}from"./attention.stories-CsDgm5ju.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preload-helper-DvrX7W50.js";import"./iframe-DC7xWhUy.js";import"./utilities-hrxOnjn_.js";import"./attention-Dzj-w30j.js";import"./if-defined-CO4gJtOW.js";import"./button-30EW8T6i.js";import"./link-rlYoPVzy.js";import"./FormControlMixin-DJnJZbro.js";function n(i){const e={code:"code",h1:"h1",h2:"h2",p:"p",...s(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(l,{of:a,name:"Docs"}),`
+`,t.jsx(e.h1,{id:"attention",children:"Attention"}),`
+`,t.jsx(e.h2,{id:"tooltip",children:"Tooltip"}),`
+`,t.jsx(o,{of:r}),`
+`,t.jsx(e.h2,{id:"callout",children:"Callout"}),`
+`,t.jsx(o,{of:h}),`
+`,t.jsx(e.h2,{id:"highlight",children:"Highlight"}),`
+`,t.jsx(o,{of:c}),`
+`,t.jsx(e.h2,{id:"dismissible-highlight",children:"Dismissible Highlight"}),`
+`,t.jsx(o,{of:d}),`
+`,t.jsx(e.h2,{id:"accessibility",children:"Accessibility"}),`
+`,t.jsx(e.p,{children:'If the Attention element has "left" or "top" position, it should be placed before the target element in the DOM.'}),`
+`,t.jsxs(e.p,{children:["Attention element handles accessibility automatically by wrapping its slotted content with a ",t.jsx(e.code,{children:"div"})," with either ",t.jsx(e.code,{children:'role="tooltip"'})," for tooltip or ",t.jsx(e.code,{children:'role="img"'})," for callout and popover, a default ",t.jsx(e.code,{children:"aria-label"})," and setting an ",t.jsx(e.code,{children:"aria-details"})," attribute on the target element. The default ",t.jsx(e.code,{children:"aria-label"})," also supports i18n."]}),`
+`,t.jsx(e.p,{children:"It is possible to tell assistive technologies to recognize only a part of Attention's text content. To do that set the role attribute on the relevant text element nested in w-attention and reference it by id through the use of aria-details. The aria-details attribute is on the target element, not on w-attention."}),`
+`,t.jsx(o,{of:p})]})}function w(i={}){const{wrapper:e}={...s(),...i.components};return e?t.jsx(e,{...i,children:t.jsx(n,{...i})}):n(i)}export{w as default};
--- assets/Datepicker-Bue8twRM.js
+++
@@ -1,4 +0,0 @@
-import{j as t}from"./index-D7-HiEGT.js";import{useMDXComponents as n}from"./index-B332Pdph.js";import{M as i,C as s}from"./blocks-9F7KFrXC.js";import{D as a,a as m}from"./datepicker.stories-CMTCe6Xa.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preload-helper-DvrX7W50.js";import"./iframe-BH76mriI.js";import"./utilities-BALQBJmL.js";import"./button-DanW1Zfk.js";import"./link-CtSiI6Ss.js";import"./FormControlMixin-DJnJZbro.js";import"./query-NYTjQBeo.js";import"./if-defined-TiP74FRN.js";function r(e){const o={h1:"h1",p:"p",...n(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:a,name:"Docs"}),`
-`,t.jsx(o.h1,{id:"datepicker",children:"Datepicker"}),`
-`,t.jsx(o.p,{children:"A date picker allows the user to select a specific calendar date."}),`
-`,t.jsx(s,{of:m})]})}function X(e={}){const{wrapper:o}={...n(),...e.components};return o?t.jsx(o,{...e,children:t.jsx(r,{...e})}):r(e)}export{X as default};
