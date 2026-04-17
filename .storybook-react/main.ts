0e126589a11f469b3d39334f1c4e95cd3a131778
William Killerud <william@killerud.com>
2026-04-17 14:55:34 +0200

    chore: upgrade vite, vitest, storybook

--- .storybook-react/main.ts
+++ .storybook-react/main.ts
@@ -2,6 +2,9 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   framework: '@storybook/react-vite',
+  core: {
+    disableTelemetry: true,
+  },
   stories: ['../packages/**/*.react.stories.tsx'],
   docs: {},
 };
