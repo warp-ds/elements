633d42f07d140958f403d80168e32ae5f6c8be00
wkillerud <wkillerud@users.noreply.github.com>
2026-04-27 11:41:31 +0000

    Remove preview for PR 620 🛬

--- pr-preview/pr-620/assets/affix.stories-D3-6kt9-.js
+++
@@ -1,5 +0,0 @@
-import{n as e,r as t}from"./chunk-BneVvdWh.js";import{s as n,t as r}from"./lit-C07f_Bua.js";import{n as i,t as a}from"./dist-WJrQKcXy.js";import{i as o,n as s,r as c,t as l}from"./utilities-pbJZRFp6.js";import{t as u}from"./affix-DymjbcjO.js";var d=t({Default:()=>g,__namedExportsOrder:()=>_,default:()=>h}),f,p,m,h,g,_,v=e((()=>{c(),i(),r(),l(),u(),{events:f,args:p,argTypes:m}=a(`w-affix`),h={title:`Forms/Affix`,render(e){return n`<w-affix ${o(s(e))}></w-affix>`},args:p,argTypes:m,parameters:{actions:{handles:f}}},g={args:{label:`kr`}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
-  args: {
-    label: 'kr'
-  }
-}`,...g.parameters?.docs?.source}}},_=[`Default`]}));v();export{g as Default,_ as __namedExportsOrder,h as default,v as n,d as t};
