21cb3a744cbb8ff30c53d83eae23cac36eefcce7
joms <jo-emil@outlook.com>
2026-05-06 07:24:43 +0000

    Deploying to gh-pages from @ warp-ds/elements@f9d5d731ed3cdcb832635fac84f98fcbbbeccdb3 🚀

---
+++ assets/Toast-FmGFwsM9.js
@@ -0,0 +1,12 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./jsx-runtime-CXZ2plg1.js";import{r as n}from"./react-DGxk81KK.js";import{t as r}from"./mdx-react-shim-fao0Lwfg.js";import{n as i,o as a,s as o}from"./blocks-DUoPTMhX.js";import{Default as s,n as c,t as l}from"./toast.stories-DInOpUli.js";function u(e){let t={code:`code`,h1:`h1`,h2:`h2`,p:`p`,pre:`pre`,...n(),...e.components};return(0,f.jsxs)(f.Fragment,{children:[(0,f.jsx)(a,{of:c,name:`Docs`}),`
+`,(0,f.jsx)(t.h1,{id:`toast`,children:`Toast`}),`
+`,(0,f.jsx)(t.p,{children:`Toast is a bit different from other packages in Warp Elements. You need to import functions from the package and call them as needed.`}),`
+`,(0,f.jsx)(t.p,{children:`The toast is intended to be used programmatically. JavaScript APIs are provided to create, update and remove toasts from a page while managing things like placement on the page for you.`}),`
+`,(0,f.jsx)(t.h2,{id:`import-functions-for-working-with-toasts`,children:`Import functions for working with toasts`}),`
+`,(0,f.jsx)(t.p,{children:`Be sure to import the elements package first as the toast APIs depend on this package.`}),`
+`,(0,f.jsx)(t.pre,{children:(0,f.jsx)(t.code,{className:`language-js`,children:`import from '@warp-ds/elements/components/toast';
+`})}),`
+`,(0,f.jsx)(t.p,{children:`Once you have imported the elements package, import the toast api package.`}),`
+`,(0,f.jsx)(t.pre,{children:(0,f.jsx)(t.code,{className:`language-js`,children:`import { toast, removeToast, updateToast } from '@warp-ds/elements/toast';
+`})}),`
+`,(0,f.jsx)(i,{of:s})]})}function d(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,f.jsx)(t,{...e,children:(0,f.jsx)(u,{...e})}):u(e)}var f;e((()=>{f=t(),r(),o(),l()}))();export{d as default};
