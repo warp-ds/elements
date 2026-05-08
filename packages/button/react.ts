4b0a7f489d0f929b05c88ba1bc34d565c2521c0b
Richard Walker <richard.walker@m10s.io>
2026-05-08 09:16:07 +0200

    fix(button): wrap react button wrapper and map camel case to snake case

--- packages/button/react.ts
+++ packages/button/react.ts
@@ -5,10 +5,45 @@ import React from 'react';
 import { WarpButton } from './button.js';
 
 // decouple from CDN by providing a dummy class
-class Component extends LitElement {}
+class Component extends LitElement {
+  get fullWidth(): boolean {
+    return false;
+  }
 
-export const Button = createComponent({
+  set fullWidth(_value: boolean) {}
+
+  get iconOnly(): boolean {
+    return false;
+  }
+
+  set iconOnly(_value: boolean) {}
+}
+
+const BaseButton = createComponent({
   tagName: 'w-button',
   elementClass: Component as unknown as typeof WarpButton,
   react: React,
 });
+
+type BaseButtonProps = React.ComponentPropsWithoutRef<typeof BaseButton>;
+
+type ButtonProps = Omit<BaseButtonProps, 'full-width' | 'icon-only'> & {
+  fullWidth?: boolean;
+  iconOnly?: boolean;
+};
+
+export const Button = React.forwardRef<WarpButton, ButtonProps>(({ fullWidth, iconOnly, ...props }, ref) =>
+  React.createElement(BaseButton, {
+    ...props,
+    ...(fullWidth !== undefined ? { fullWidth } : {}),
+    ...(iconOnly !== undefined ? { iconOnly } : {}),
+    ...(fullWidth ? { 'full-width': true } : {}),
+    ...(iconOnly ? { 'icon-only': true } : {}),
+    ref,
+  } as React.ComponentProps<typeof BaseButton> & {
+    'full-width'?: boolean;
+    'icon-only'?: boolean;
+  }),
+);
+
+Button.displayName = 'Button';
