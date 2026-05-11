98fb23ea644f9a91e905d21627b1d3b1b4d54e15
Joakim Schweizer <joakim.schweizer@vend.com>
2026-05-11 14:09:55 +0200

    fix(modal-header): set title on close and back buttons to prevent inheriting modal title tooltip

--- packages/modal-header/modal-header.ts
+++ packages/modal-header/modal-header.ts
@@ -64,14 +64,16 @@ export class ModalHeader extends CanCloseMixin(LitElement) {
   }
 
   get backButton() {
+    const backLabel = i18n._({
+      id: 'modal.aria.back',
+      message: 'Back',
+      comment: 'Aria label for the back button in modal',
+    });
     return this.back && !this._hasTopContent // Not showing back button when there is a top image
       ? html`<button
           type="button"
-          aria-label="${i18n._({
-            id: 'modal.aria.back',
-            message: 'Back',
-            comment: 'Aria label for the back button in modal',
-          })}"
+          title="${backLabel}"
+          aria-label="${backLabel}"
           class="header-button header-button-left"
           @click="${this.emitBack}">
           <w-icon name="ArrowLeft" size="small" locale="${detectLocale()}" style="display: flex;" class="flex"></w-icon>
@@ -80,14 +82,16 @@ export class ModalHeader extends CanCloseMixin(LitElement) {
   }
   get closeButton() {
     if (this.noClose) return nothing;
+    const closeLabel = i18n._({
+      id: 'modal.aria.close',
+      message: 'Close',
+      comment: 'Aria label for the close button in modal',
+    });
     return html`<div class="header-close-button-container">
         <w-button
             type="button"
-            aria-label="${i18n._({
-              id: 'modal.aria.close',
-              message: 'Close',
-              comment: 'Aria label for the close button in modal',
-            })}"
+            title="${closeLabel}"
+            aria-label="${closeLabel}"
             variant="pill"
             small=""
             @click="${this.close}">
