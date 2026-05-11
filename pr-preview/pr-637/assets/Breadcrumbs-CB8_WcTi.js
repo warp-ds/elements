f0118bd9562473151a985cdf9aaaad189bb00cb2
digitalsadhu <digitalsadhu@users.noreply.github.com>
2026-05-11 07:18:37 +0000

    Deploy preview for PR 637 🛫

---
+++ pr-preview/pr-637/assets/Breadcrumbs-CB8_WcTi.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./jsx-runtime-CXZ2plg1.js";import{r as n}from"./react-DGxk81KK.js";import{t as r}from"./mdx-react-shim-fao0Lwfg.js";import{n as i,o as a,s as o}from"./blocks-DGS3CJgI.js";import{Default as s,WithLinks as c,WithSpanForCurrentPage as l,n as u,t as d}from"./breadcrumbs.stories-DCdv9T9w.js";function f(e){let t={h1:`h1`,h2:`h2`,h3:`h3`,p:`p`,...n(),...e.components};return(0,m.jsxs)(m.Fragment,{children:[(0,m.jsx)(a,{of:d,name:`Docs`}),`
+`,(0,m.jsx)(t.h1,{id:`breadcrumbs`,children:`Breadcrumbs`}),`
+`,(0,m.jsx)(t.p,{children:`Breadcrumbs show the navigation structure for the current location.`}),`
+`,(0,m.jsx)(i,{of:s}),`
+`,(0,m.jsx)(t.h2,{id:`visual-options`,children:`Visual options`}),`
+`,(0,m.jsx)(t.h3,{id:`all-segments-linkable`,children:`All segments linkable`}),`
+`,(0,m.jsx)(i,{of:c}),`
+`,(0,m.jsx)(t.h3,{id:`disabled-last-link`,children:`Disabled last link`}),`
+`,(0,m.jsx)(i,{of:l})]})}function p(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,m.jsx)(t,{...e,children:(0,m.jsx)(f,{...e})}):f(e)}var m;e((()=>{m=t(),r(),o(),u()}))();export{p as default};
