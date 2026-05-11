e5c9ddac12be9ffae1dca138af9cc1f0a349671e
digitalsadhu <digitalsadhu@users.noreply.github.com>
2026-05-11 11:45:05 +0000

    Deploy preview for PR 637 🛫

---
+++ pr-preview/pr-637/assets/expandable.stories-C639NzU9.js
@@ -0,0 +1,155 @@
+import{n as e,r as t}from"./chunk-DnJy8xQt.js";import{D as n,E as r,Mt as i,O as a,Ot as o,Pt as s,T as c,_ as l,a as u,b as d,c as f,d as p,f as m,k as h,kt as g,l as _,o as v,u as y,w as b}from"./iframe-CEP5nfbG.js";import{i as x,n as S,r as C,t as w}from"./utilities-DrdE3t0T.js";import{n as T,t as E}from"./if-defined-BD_Rw-dV.js";import{t as D}from"./expand-transition-JcJ872up.js";import{n as O,t as k}from"./taggedTemplateLiteral-Cmj7rbCg.js";var A,j=e((()=>{o(),a(),A=class extends g{static properties={level:{type:Number}};get _markup(){return`<h${this.level}
+    style="margin: 0; font-weight: unset; font-size: unset; line-height: unset;"
+  >
+    <slot></slot>
+  </h${this.level}>
+`}render(){return this.level?h(this._markup):i`<slot></slot>`}},customElements.get(`w-unstyled-heading`)||customElements.define(`w-unstyled-heading`,A)})),M,N=e((()=>{o(),M=s`*,:before,:after{--w-rotate:0;--w-rotate-x:0;--w-rotate-y:0;--w-rotate-z:0;--w-scale-x:1;--w-scale-y:1;--w-scale-z:1;--w-skew-x:0;--w-skew-y:0;--w-translate-x:0;--w-translate-y:0;--w-translate-z:0}.t4{font-weight:700;font-size:var(--w-font-size-m);line-height:var(--w-line-height-m)}.text-left{text-align:left}.align-middle{vertical-align:middle}.bg-transparent{background-color:#0000}.appearance-none{-webkit-appearance:none;appearance:none}.will-change-height{will-change:height}.border-0{border-width:0}.border-2{border-width:2px}.rounded-8{border-radius:8px}.rounded-l-0{border-top-left-radius:0;border-bottom-left-radius:0}.rounded-r-0{border-top-right-radius:0;border-bottom-right-radius:0}.block{display:block}.inline-block{display:inline-block}.flex{display:flex}.inline-flex{display:inline-flex}.hover\\:underline:hover{text-decoration-line:underline}.focus-visible\\:underline:focus-visible{text-decoration-line:underline}.overflow-hidden{overflow:hidden}.focus\\:outline-none:focus{outline-offset:2px;outline:2px solid #0000}.items-center{align-items:center}.justify-between{justify-content:space-between}.relative{position:relative}.static{position:static}.s-bg{background-color:var(--w-s-color-background)}.s-bg-info-subtle{background-color:var(--w-s-color-background-info-subtle)}.s-bg-subtle{background-color:var(--w-s-color-background-subtle)}.hover\\:s-bg-subtle-hover:hover{background-color:var(--w-s-color-background-subtle-hover)}.active\\:s-bg-subtle-active:active{background-color:var(--w-s-color-background-subtle-active)}.s-text{color:var(--w-s-color-text)}.s-icon{color:var(--w-s-color-icon)}.s-border{border-color:var(--w-s-color-border)}.s-surface-sunken{background-color:var(--w-s-color-surface-sunken)}.h-0{height:0}.w-full{width:100%}.m-0{margin:0}.-mx-16{margin-left:-1.6rem;margin-right:-1.6rem}.last-child\\:mb-0>:last-child{margin-bottom:0}.ml-8{margin-left:.8rem}.px-0{padding-left:0;padding-right:0}.px-16{padding-left:1.6rem;padding-right:1.6rem}.py-0{padding-top:0;padding-bottom:0}.py-16{padding-top:1.6rem;padding-bottom:1.6rem}.pt-0{padding-top:0}.pt-16{padding-top:1.6rem}.pt-8{padding-top:.8rem}.invisible{visibility:hidden}.break-words{overflow-wrap:break-word}.cursor-pointer{cursor:pointer}.-rotate-180{--w-rotate-x:0;--w-rotate-y:0;--w-rotate-z:0;--w-rotate:-180deg;transform:translateX(var(--w-translate-x))translateY(var(--w-translate-y))translateZ(var(--w-translate-z))rotate(var(--w-rotate))rotateX(var(--w-rotate-x))rotateY(var(--w-rotate-y))rotateZ(var(--w-rotate-z))skewX(var(--w-skew-x))skewY(var(--w-skew-y))scaleX(var(--w-scale-x))scaleY(var(--w-scale-y))scaleZ(var(--w-scale-z))}.part-\\[w-icon-chevron-down-16-part\\]\\:-rotate-180::part(w-icon-chevron-down-16-part){--w-rotate-x:0;--w-rotate-y:0;--w-rotate-z:0;--w-rotate:-180deg;transform:translateX(var(--w-translate-x))translateY(var(--w-translate-y))translateZ(var(--w-translate-z))rotate(var(--w-rotate))rotateX(var(--w-rotate-x))rotateY(var(--w-rotate-y))rotateZ(var(--w-rotate-z))skewX(var(--w-skew-x))skewY(var(--w-skew-y))scaleX(var(--w-scale-x))scaleY(var(--w-scale-y))scaleZ(var(--w-scale-z))}.rotate-180{--w-rotate-x:0;--w-rotate-y:0;--w-rotate-z:0;--w-rotate:180deg;transform:translateX(var(--w-translate-x))translateY(var(--w-translate-y))translateZ(var(--w-translate-z))rotate(var(--w-rotate))rotateX(var(--w-rotate-x))rotateY(var(--w-rotate-y))rotateZ(var(--w-rotate-z))skewX(var(--w-skew-x))skewY(var(--w-skew-y))scaleX(var(--w-scale-x))scaleY(var(--w-scale-y))scaleZ(var(--w-scale-z))}.part-\\[w-icon-chevron-up-16-part\\]\\:rotate-180::part(w-icon-chevron-up-16-part){--w-rotate-x:0;--w-rotate-y:0;--w-rotate-z:0;--w-rotate:180deg;transform:translateX(var(--w-translate-x))translateY(var(--w-translate-y))translateZ(var(--w-translate-z))rotate(var(--w-rotate))rotateX(var(--w-rotate-x))rotateY(var(--w-rotate-y))rotateZ(var(--w-rotate-z))skewX(var(--w-skew-x))skewY(var(--w-skew-y))scaleX(var(--w-scale-x))scaleY(var(--w-scale-y))scaleZ(var(--w-scale-z))}.transform{transform:translateX(var(--w-translate-x))translateY(var(--w-translate-y))translateZ(var(--w-translate-z))rotate(var(--w-rotate))rotateX(var(--w-rotate-x))rotateY(var(--w-rotate-y))rotateZ(var(--w-rotate-z))skewX(var(--w-skew-x))skewY(var(--w-skew-y))scaleX(var(--w-scale-x))scaleY(var(--w-scale-y))scaleZ(var(--w-scale-z))}.part-\\[w-icon-chevron-down-16-part\\]\\:transform::part(w-icon-chevron-down-16-part){transform:translateX(var(--w-translate-x))translateY(var(--w-translate-y))translateZ(var(--w-translate-z))rotate(var(--w-rotate))rotateX(var(--w-rotate-x))rotateY(var(--w-rotate-y))rotateZ(var(--w-rotate-z))skewX(var(--w-skew-x))skewY(var(--w-skew-y))scaleX(var(--w-scale-x))scaleY(var(--w-scale-y))scaleZ(var(--w-scale-z))}.part-\\[w-icon-chevron-up-16-part\\]\\:transform::part(w-icon-chevron-up-16-part){transform:translateX(var(--w-translate-x))translateY(var(--w-translate-y))translateZ(var(--w-translate-z))rotate(var(--w-rotate))rotateX(var(--w-rotate-x))rotateY(var(--w-rotate-y))rotateZ(var(--w-rotate-z))skewX(var(--w-skew-x))skewY(var(--w-skew-y))scaleX(var(--w-scale-x))scaleY(var(--w-scale-y))scaleZ(var(--w-scale-z))}.transform-gpu{transform:translate3d(var(--w-translate-x),var(--w-translate-y),var(--w-translate-z))rotate(var(--w-rotate))rotateX(var(--w-rotate-x))rotateY(var(--w-rotate-y))rotateZ(var(--w-rotate-z))skewX(var(--w-skew-x))skewY(var(--w-skew-y))scaleX(var(--w-scale-x))scaleY(var(--w-scale-y))scaleZ(var(--w-scale-z))}.part-\\[w-icon-chevron-down-16-part\\]\\:transform-gpu::part(w-icon-chevron-down-16-part){transform:translate3d(var(--w-translate-x),var(--w-translate-y),var(--w-translate-z))rotate(var(--w-rotate))rotateX(var(--w-rotate-x))rotateY(var(--w-rotate-y))rotateZ(var(--w-rotate-z))skewX(var(--w-skew-x))skewY(var(--w-skew-y))scaleX(var(--w-scale-x))scaleY(var(--w-scale-y))scaleZ(var(--w-scale-z))}.part-\\[w-icon-chevron-up-16-part\\]\\:transform-gpu::part(w-icon-chevron-up-16-part){transform:translate3d(var(--w-translate-x),var(--w-translate-y),var(--w-translate-z))rotate(var(--w-rotate))rotateX(var(--w-rotate-x))rotateY(var(--w-rotate-y))rotateZ(var(--w-rotate-z))skewX(var(--w-skew-x))skewY(var(--w-skew-y))scaleX(var(--w-scale-x))scaleY(var(--w-scale-y))scaleZ(var(--w-scale-z))}.transition-transform{transition-property:transform;transition-duration:.15s;transition-timing-function:cubic-bezier(.4,0,.2,1)}.part-\\[w-icon-chevron-down-16-part\\]\\:transition-transform::part(w-icon-chevron-down-16-part){transition-property:transform;transition-duration:.15s;transition-timing-function:cubic-bezier(.4,0,.2,1)}.part-\\[w-icon-chevron-up-16-part\\]\\:transition-transform::part(w-icon-chevron-up-16-part){transition-property:transform;transition-duration:.15s;transition-timing-function:cubic-bezier(.4,0,.2,1)}.ease-in-out{transition-timing-function:cubic-bezier(.4,0,.2,1)}.part-\\[w-icon-chevron-down-16-part\\]\\:ease-in-out::part(w-icon-chevron-down-16-part){transition-timing-function:cubic-bezier(.4,0,.2,1)}.part-\\[w-icon-chevron-up-16-part\\]\\:ease-in-out::part(w-icon-chevron-up-16-part){transition-timing-function:cubic-bezier(.4,0,.2,1)}@media (min-width:480px){.sm\\:rounded-8{border-radius:8px}.sm\\:mx-0{margin-left:0;margin-right:0}}`})),P,F,I,L=e((()=>{b(),o(),l(),E(),y(),D(),_(),j(),u(),N(),m(),P={base:`group block relative break-words last-child:mb-0 px-16 rounded-8`,bleed:`-mx-16 sm:mx-0 rounded-l-0 rounded-r-0 sm:rounded-8`,info:`s-bg-info-subtle`,neutral:`s-surface-sunken`,bordered:`border-2 s-border s-bg`},F={wrapper:`will-change-height s-text py-16`,box:`s-bg-subtle hover:s-bg-subtle-hover active:s-bg-subtle-active py-0 px-0 group block relative break-words last-child:mb-0 rounded-8`,bleed:`-mx-16 rounded-l-0 rounded-r-0 sm:mx-0 sm:rounded-8`,chevron:`inline-block align-middle s-icon`,chevronNonBox:`ml-8`,chevronTransform:`transform transition-transform transform-gpu ease-in-out`,chevronExpand:`-rotate-180`,chevronCollapse:`rotate-180`,elementsChevronDownTransform:`part-[w-chevrondown]:transform part-[w-chevrondown]:transition-transform part-[w-chevrondown]:transform-gpu part-[w-chevrondown]:ease-in-out`,elementsChevronUpTransform:`part-[w-chevronup]:transform part-[w-chevronup]:transition-transform part-[w-chevronup]:transform-gpu part-[w-chevronup]:ease-in-out`,elementsChevronExpand:`part-[w-chevrondown]:-rotate-180`,elementsChevronCollapse:`part-[w-chevronup]:rotate-180`,expansion:`overflow-hidden`,expansionNotExpanded:`h-0 invisible`,button:`focus:outline-none appearance-none cursor-pointer bg-transparent border-0 m-0 hover:underline focus-visible:underline`,buttonBox:`w-full text-left relative inline-flex items-center justify-between group relative break-words last-child:mb-0 px-16 rounded-8`,contentWithTitle:`pt-0`,title:`flex w-full justify-between items-center`,titleType:`t4`},I=class extends g{constructor(...e){super(...e),this.expanded=!1,this.box=!1,this.bleed=!1,this.noChevron=!1,this.animated=!1,this._hasTitle=!0,this._showChevronUp=!1}static{this.styles=[v,M,s`
+      :host {
+        display: block;
+      }
+      ::slotted(:last-child) {
+        margin-bottom: 0px !important;
+      }
+    `]}updated(e){e.has(`expanded`)&&setTimeout(()=>{this._showChevronUp=this.expanded},200)}firstUpdated(){let e=!!this.title,t=this.renderRoot.querySelector(`slot[name='title']`)?.assignedNodes().length>0;this._hasTitle=e||t}get#e(){return c([F.wrapper,this.box&&F.box,this.bleed&&F.bleed])}get#t(){return c(this.buttonClass,[F.button,this.box&&F.buttonBox])}get#n(){return c([F.chevron,!this.box&&F.chevronNonBox])}get#r(){let e=c([F.elementsChevronUpTransform,`flex`,!this.expanded&&this._showChevronUp&&F.elementsChevronCollapse]),t=c([F.elementsChevronDownTransform,`flex`,this.expanded&&!this._showChevronUp&&F.elementsChevronExpand]),n=f();return this._showChevronUp?i`<w-icon name="ChevronUp" size="small" locale="${n}" class="${e}"></w-icon>`:i`<w-icon name="ChevronDown" size="small" locale="${n}" class="${t}"></w-icon>`}get#i(){return c(this.contentClass,[this.box?`pt-16`:`pt-8`,this.box&&P.base,this._hasTitle&&this.box&&F.contentWithTitle])}get#a(){return c([F.expansion,!this.expanded&&F.expansionNotExpanded])}get _expandableSlot(){return i`<div class="${this.#i}">
+      <slot></slot>
+    </div>`}render(){return i` <div class="${this.#e}">
+      ${this._hasTitle?i`<w-unstyled-heading level=${this.headingLevel}>
+            <button
+              type="button"
+              aria-expanded="${this.expanded}"
+              class="${this.#t}"
+              @click=${()=>this.expanded=!this.expanded}>
+              <div class="${F.title}">
+                ${this.title?i`<span class="${F.titleType}">${this.title}</span>`:i`<slot name="title"></slot>`}
+                ${this.noChevron?``:i`<div class="${this.#n}">${this.#r}</div>`}
+              </div>
+            </button>
+          </w-unstyled-heading>`:``}
+      ${this.animated?i`<w-expand-transition ?show=${this.expanded}> ${this._expandableSlot} </w-expand-transition>`:i`<div class="${this.#a}" aria-hidden=${T(this.expanded?void 0:!0)}>
+            ${this._expandableSlot}
+          </div>`}
+    </div>`}},p([d({type:Boolean,reflect:!0})],I.prototype,`expanded`,void 0),p([d({type:String})],I.prototype,`title`,void 0),p([d({type:Boolean})],I.prototype,`box`,void 0),p([d({type:Boolean})],I.prototype,`bleed`,void 0),p([d({attribute:`button-class`,type:String})],I.prototype,`buttonClass`,void 0),p([d({attribute:`content-class`,type:String})],I.prototype,`contentClass`,void 0),p([d({attribute:`no-chevron`,type:Boolean})],I.prototype,`noChevron`,void 0),p([d({type:Boolean})],I.prototype,`animated`,void 0),p([d({attribute:`heading-level`,type:Number})],I.prototype,`headingLevel`,void 0),p([d({type:Boolean,state:!0})],I.prototype,`_hasTitle`,void 0),p([d({type:Boolean,state:!0})],I.prototype,`_showChevronUp`,void 0),customElements.get(`w-expandable`)||customElements.define(`w-expandable`,I)})),R=t({Animated:()=>q,Box:()=>K,CustomTitle:()=>X,Default:()=>W,Expanded:()=>G,NoChevron:()=>Y,NoTitle:()=>Z,WithHeading:()=>J,__namedExportsOrder:()=>Q,default:()=>U}),z,B,V,H,U,W,G,K,q,J,Y,X,Z,Q,$=e((()=>{C(),n(),o(),w(),D(),j(),L(),O(),{events:B,args:V,argTypes:H}=r(`w-expandable`),U={title:`Layout/Expandable`,render(e){return i`
+      <w-expandable ${x(S(e))}>
+        <p>This is the expandable content that can be shown or hidden.</p>
+        <p>It can contain multiple paragraphs and other elements.</p>
+      </w-expandable>
+    `},args:V,argTypes:H,parameters:{actions:{handles:B}}},W={args:{title:`I'm expandable`,expanded:!1,box:!1,bleed:!1,animated:!1,"no-chevron":!1}},G={args:{title:`I'm expanded by default`,expanded:!0,box:!1,bleed:!1,animated:!1,"no-chevron":!1}},K={args:{title:`I'm a box expandable`,expanded:!1,box:!0,bleed:!1,animated:!1,"no-chevron":!1}},q={args:{title:`I'm animated`,expanded:!1,box:!0,bleed:!1,animated:!0,"no-chevron":!1}},J={args:{title:`I'm wrapped in h2`,expanded:!1,box:!0,bleed:!1,animated:!1,"no-chevron":!1,"heading-level":2}},Y={args:{title:`I have no chevron`,expanded:!1,box:!1,bleed:!1,animated:!1,"no-chevron":!0}},X={args:{expanded:!1,box:!0,bleed:!1,animated:!1,"no-chevron":!1},render(e){return i`
+      <w-expandable ${x(S(e))}>
+        <div slot="title" style="display: flex; align-items: center;">
+          <span>Custom title with icon</span>
+        </div>
+        <p>This expandable has a custom title slot with an icon.</p>
+      </w-expandable>
+    `}},Z={args:{expanded:!0,box:!0,animated:!0},render(e){return i(z||=k([`
+      <w-button aria-label="Toggle expandable content without title" aria-controls="expandableWithoutTitle">Toggle</w-button>
+      <w-expandable `,` id="expandableWithoutTitle">
+        <p>with expanded content</p>
+      </w-expandable>
+      <script>
+        document.querySelector('[aria-controls="expandableWithoutTitle"]').addEventListener('click', () => {
+          const expand = document.getElementById('expandableWithoutTitle');
+          if (expand.expanded) {
+            expand.removeAttribute('expanded');
+          } else {
+            expand.setAttribute('expanded', 'true');
+          }
+        });
+      <\/script>
+    `]),x(S(e)))}},W.parameters={...W.parameters,docs:{...W.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: "I'm expandable",
+    expanded: false,
+    box: false,
+    bleed: false,
+    animated: false,
+    'no-chevron': false
+  }
+}`,...W.parameters?.docs?.source}}},G.parameters={...G.parameters,docs:{...G.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: "I'm expanded by default",
+    expanded: true,
+    box: false,
+    bleed: false,
+    animated: false,
+    'no-chevron': false
+  }
+}`,...G.parameters?.docs?.source}}},K.parameters={...K.parameters,docs:{...K.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: "I'm a box expandable",
+    expanded: false,
+    box: true,
+    bleed: false,
+    animated: false,
+    'no-chevron': false
+  }
+}`,...K.parameters?.docs?.source}}},q.parameters={...q.parameters,docs:{...q.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: "I'm animated",
+    expanded: false,
+    box: true,
+    bleed: false,
+    animated: true,
+    'no-chevron': false
+  }
+}`,...q.parameters?.docs?.source}}},J.parameters={...J.parameters,docs:{...J.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: "I'm wrapped in h2",
+    expanded: false,
+    box: true,
+    bleed: false,
+    animated: false,
+    'no-chevron': false,
+    'heading-level': 2
+  }
+}`,...J.parameters?.docs?.source}}},Y.parameters={...Y.parameters,docs:{...Y.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: 'I have no chevron',
+    expanded: false,
+    box: false,
+    bleed: false,
+    animated: false,
+    'no-chevron': true
+  }
+}`,...Y.parameters?.docs?.source}}},X.parameters={...X.parameters,docs:{...X.parameters?.docs,source:{originalSource:`{
+  args: {
+    expanded: false,
+    box: true,
+    bleed: false,
+    animated: false,
+    'no-chevron': false
+  },
+  render(args) {
+    return html\`
+      <w-expandable \${spread(prespread(args))}>
+        <div slot="title" style="display: flex; align-items: center;">
+          <span>Custom title with icon</span>
+        </div>
+        <p>This expandable has a custom title slot with an icon.</p>
+      </w-expandable>
+    \`;
+  }
+}`,...X.parameters?.docs?.source}}},Z.parameters={...Z.parameters,docs:{...Z.parameters?.docs,source:{originalSource:`{
+  args: {
+    expanded: true,
+    box: true,
+    animated: true
+  },
+  render(args) {
+    return html\`
+      <w-button aria-label="Toggle expandable content without title" aria-controls="expandableWithoutTitle">Toggle</w-button>
+      <w-expandable \${spread(prespread(args))} id="expandableWithoutTitle">
+        <p>with expanded content</p>
+      </w-expandable>
+      <script>
+        document.querySelector('[aria-controls="expandableWithoutTitle"]').addEventListener('click', () => {
+          const expand = document.getElementById('expandableWithoutTitle');
+          if (expand.expanded) {
+            expand.removeAttribute('expanded');
+          } else {
+            expand.setAttribute('expanded', 'true');
+          }
+        });
+      <\/script>
+    \`;
+  }
+}`,...Z.parameters?.docs?.source}}},Q=[`Default`,`Expanded`,`Box`,`Animated`,`WithHeading`,`NoChevron`,`CustomTitle`,`NoTitle`]}));$();export{q as Animated,K as Box,X as CustomTitle,W as Default,G as Expanded,Y as NoChevron,Z as NoTitle,J as WithHeading,Q as __namedExportsOrder,U as default,$ as n,R as t};
