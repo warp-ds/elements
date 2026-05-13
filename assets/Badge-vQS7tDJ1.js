56c49034c6300c52f9a801d824c8e68a37a18f2c
wkillerud <william@killerud.com>
2026-05-13 12:52:47 +0000

    Deploying to gh-pages from @ warp-ds/elements@0fb22ab3b2d93d308f192680917ee0bd55669b6e 🚀

---
+++ assets/Badge-vQS7tDJ1.js
@@ -0,0 +1,15 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./jsx-runtime-BG42LmKs.js";import{r as n}from"./react-DShTf-Dy.js";import{t as r}from"./mdx-react-shim-np44v9yQ.js";import{n as i,o as a,s as o}from"./blocks-k0i1c6Oj.js";import{Info as s,Negative as c,Neutral as l,Positioned as u,Positive as d,Price as f,Sponsored as p,Warning as m,n as h,t as g}from"./badge.stories-hAFTofdp.js";function _(e){let t={h1:`h1`,h2:`h2`,h3:`h3`,p:`p`,...n(),...e.components};return(0,y.jsxs)(y.Fragment,{children:[(0,y.jsx)(a,{of:g,name:`Docs`}),`
+`,(0,y.jsx)(t.h1,{id:`badge`,children:`Badge`}),`
+`,(0,y.jsx)(t.p,{children:`Badge is used for showing a small amount of non-interactive color-categorized metadata, like a status or count.`}),`
+`,(0,y.jsx)(t.h2,{id:`visual-options`,children:`Visual options`}),`
+`,(0,y.jsx)(t.h3,{id:`default`,children:`Default`}),`
+`,(0,y.jsx)(i,{of:l}),`
+`,(0,y.jsx)(t.h3,{id:`variants`,children:`Variants`}),`
+`,(0,y.jsx)(i,{of:l}),`
+`,(0,y.jsx)(i,{of:s}),`
+`,(0,y.jsx)(i,{of:d}),`
+`,(0,y.jsx)(i,{of:m}),`
+`,(0,y.jsx)(i,{of:c}),`
+`,(0,y.jsx)(i,{of:f}),`
+`,(0,y.jsx)(i,{of:p}),`
+`,(0,y.jsx)(i,{of:u})]})}function v(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,y.jsx)(t,{...e,children:(0,y.jsx)(_,{...e})}):_(e)}var y;e((()=>{y=t(),r(),o(),h()}))();export{v as default};
