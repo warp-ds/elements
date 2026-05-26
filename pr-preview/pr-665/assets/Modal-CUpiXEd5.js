577eed1a909de8a74e2d1ec01de1dfcfd1b2770c
wkillerud <wkillerud@users.noreply.github.com>
2026-05-26 12:58:41 +0000

    Deploy preview for PR 665 🛫

---
+++ pr-preview/pr-665/assets/Modal-CUpiXEd5.js
@@ -0,0 +1,8 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{i as t,o as n}from"./react-C_CSCj7s.js";import{t as r}from"./mdx-react-shim-wZ56Y-Ct.js";import{n as i,o as a,s as o}from"./blocks-DXrCTk8R.js";import{Default as s,WithImage as c,n as l,t as u}from"./modal.stories-DYh2nlFA.js";function d(e){let n={h1:`h1`,h2:`h2`,h3:`h3`,p:`p`,...t(),...e.components};return(0,p.jsxs)(p.Fragment,{children:[(0,p.jsx)(a,{of:l,name:`Docs`}),`
+`,(0,p.jsx)(n.h1,{id:`modal`,children:`Modal`}),`
+`,(0,p.jsx)(n.p,{children:`Modals (or dialogs) display important information that users need to acknowledge.`}),`
+`,(0,p.jsx)(n.h2,{id:`variants`,children:`Variants`}),`
+`,(0,p.jsx)(n.h3,{id:`with-a-title-bar`,children:`With a title bar`}),`
+`,(0,p.jsx)(i,{of:s}),`
+`,(0,p.jsx)(n.h3,{id:`with-an-image-at-the-top`,children:`With an image at the top`}),`
+`,(0,p.jsx)(i,{of:c})]})}function f(e={}){let{wrapper:n}={...t(),...e.components};return n?(0,p.jsx)(n,{...e,children:(0,p.jsx)(d,{...e})}):d(e)}var p;e((()=>{p=n(),r(),o(),u()}))();export{f as default};
