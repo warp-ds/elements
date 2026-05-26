3529c80f6833f7077a8b8f2a8679573df8ccf9b7
William Killerud <william@killerud.com>
2026-05-26 14:48:56 +0200

    fix(snackbar): use the min-width we say we use in the docs

--- packages/snackbar-item/styles.ts
+++ packages/snackbar-item/styles.ts
@@ -13,7 +13,7 @@ export const styles = css`
         --_snackbar-item-action-color-hover: var(--w-c-snackbar-item-action-color-hover, var(--w-s-color-text-inverted-static));
         --_snackbar-item-action-color-active: var(--w-c-snackbar-item-action-color-active, var(--w-s-color-text-inverted-static));
         --_snackbar-item-max-width: var(--w-c-snackbar-item-max-width, 420px);
-        --_snackbar-item-min-width: var(--w-c-snackbar-item-min-width, 280px);
+        --_snackbar-item-min-width: var(--w-c-snackbar-item-min-width, 328px);
         --_snackbar-item-padding: var(--w-c-snackbar-item-padding, 8px);
     }
 
