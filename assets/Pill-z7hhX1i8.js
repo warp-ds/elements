fd49a488a57f3a68dcf6de2e83f0a393e755bff9
digitalsadhu <digitalsadhu@gmail.com>
2026-06-03 10:11:30 +0000

    Deploying to gh-pages from @ warp-ds/elements@217d56a4333a4279e7dcee8aa79654f0f1bcd030 🚀

---
+++ assets/Pill-z7hhX1i8.js
@@ -0,0 +1,9 @@
+import{i as e}from"./preload-helper-CwA5MRLW.js";import{i as t,o as n}from"./react-KururdvN.js";import{t as r}from"./mdx-react-shim-cQDIUnuk.js";import{n as i,o as a,s as o}from"./blocks-CT_oNY4C.js";import{Closable as s,Default as c,Suggestion as l,n as u,t as d}from"./pill.stories-BeFKp3Ih.js";function f(e){let n={h1:`h1`,h2:`h2`,p:`p`,...t(),...e.components};return(0,m.jsxs)(m.Fragment,{children:[(0,m.jsx)(a,{of:u,name:`Docs`}),`
+`,(0,m.jsx)(n.h1,{id:`pill`,children:`Pill`}),`
+`,(0,m.jsx)(n.p,{children:`Pill is a type of button that is often used as a filter, but can also be used as a rounded button for overlays, etc.`}),`
+`,(0,m.jsx)(n.h2,{id:`default`,children:`Default`}),`
+`,(0,m.jsx)(i,{of:c}),`
+`,(0,m.jsx)(n.h2,{id:`suggestion`,children:`Suggestion`}),`
+`,(0,m.jsx)(i,{of:l}),`
+`,(0,m.jsx)(n.h2,{id:`closable`,children:`Closable`}),`
+`,(0,m.jsx)(i,{of:s})]})}function p(e={}){let{wrapper:n}={...t(),...e.components};return n?(0,m.jsx)(n,{...e,children:(0,m.jsx)(f,{...e})}):f(e)}var m;e((()=>{m=n(),r(),o(),d()}))();export{p as default};
