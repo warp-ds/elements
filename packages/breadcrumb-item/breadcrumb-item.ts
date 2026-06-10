9a8741437be4d23a627ea5bed798bb37df7bdfca
Richard Walker <richard.walker@m10s.io>
2026-06-10 14:25:05 +0200

    chore: ensure anchor includes the class s-text-link

--- packages/breadcrumb-item/breadcrumb-item.ts
+++ packages/breadcrumb-item/breadcrumb-item.ts
@@ -1,13 +1,17 @@
-import { LitElement, html, nothing } from 'lit';
+import { LitElement, html, nothing, css } from 'lit';
 import { property } from 'lit/decorators.js';
 
 class WarpBreadcrumbItem extends LitElement {
   @property({ type: Boolean, attribute: 'current-page' }) currentPage = false;
   @property({ type: String }) href: string | null = null;
 
+  static styles = css`
+    .s-text-link{color:var(--w-s-color-text-link)}
+  `;
+
   render() {
     return html`
-      ${this.href ? html`<a href=${this.href} aria-current=${this.currentPage ? 'page' : nothing}><slot></slot></a>` : html`<span aria-current=${this.currentPage ? 'page' : nothing}><slot></slot></span>`}
+      ${this.href ? html`<a class="s-text-link" href=${this.href} aria-current=${this.currentPage ? 'page' : nothing}><slot></slot></a>` : html`<span aria-current=${this.currentPage ? 'page' : nothing}><slot></slot></span>`}
       ${!this.currentPage ? html`<span aria-hidden="true">/</span>` : nothing}
     `;
   }
