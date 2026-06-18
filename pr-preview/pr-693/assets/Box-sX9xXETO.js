2f3e4832bc0a2a02062a577a4569355a09f29cf7
digitalsadhu <digitalsadhu@users.noreply.github.com>
2026-06-18 10:23:19 +0000

    Deploy preview for PR 693 🛫

---
+++ pr-preview/pr-693/assets/Box-sX9xXETO.js
@@ -0,0 +1,11 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{i as t,o as n}from"./react-C_CSCj7s.js";import{t as r}from"./mdx-react-shim-wZ56Y-Ct.js";import{n as i,o as a,s as o}from"./blocks-CaozSLIh.js";import{Bleed as s,Bordered as c,Default as l,Info as u,Neutral as d,n as f,t as p}from"./box.stories-zNGTxQd8.js";function m(e){let n={a:`a`,code:`code`,h1:`h1`,h2:`h2`,p:`p`,...t(),...e.components};return(0,g.jsxs)(g.Fragment,{children:[(0,g.jsx)(a,{of:p,name:`Docs`}),`
+`,(0,g.jsx)(n.h1,{id:`box`,children:`Box`}),`
+`,(0,g.jsx)(n.p,{children:`Box is a layout component used for separating content areas on a page.`}),`
+`,(0,g.jsx)(n.h2,{id:`accessibility`,children:`Accessibility`}),`
+`,(0,g.jsxs)(n.p,{children:[`Use the ARIA role attribute to provide semantic meaning to a box element. If no role property is supplied, it will default to `,(0,g.jsx)(n.code,{children:`role="region"`}),`. To remove the default role attribute, supply an empty role (`,(0,g.jsx)(n.code,{children:`role=""`}),`). Consider using more suitable roles for your specific Box, as described on `,(0,g.jsx)(n.a,{href:`https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles#aria_role_types`,rel:`nofollow`,children:`MDN WAI-ARIA Roles`}),`.`]}),`
+`,(0,g.jsx)(n.h2,{id:`visual-options`,children:`Visual options`}),`
+`,(0,g.jsx)(i,{of:l}),`
+`,(0,g.jsx)(i,{of:u}),`
+`,(0,g.jsx)(i,{of:d}),`
+`,(0,g.jsx)(i,{of:c}),`
+`,(0,g.jsx)(i,{of:s})]})}function h(e={}){let{wrapper:n}={...t(),...e.components};return n?(0,g.jsx)(n,{...e,children:(0,g.jsx)(m,{...e})}):m(e)}var g;e((()=>{g=n(),r(),o(),f()}))();export{h as default};
