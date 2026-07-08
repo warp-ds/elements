577b8358e13104ba978f0ecc3f540dc717788aec
William Killerud <william@killerud.com>
2026-07-08 10:56:34 +0200

    test(tooltip): expand test coverage

--- packages/tooltip/tooltip.test.ts
+++ packages/tooltip/tooltip.test.ts
@@ -1,6 +1,7 @@
 import { html } from "lit";
 
 import { expect, test } from "vitest";
+import { server, userEvent } from "vitest/browser";
 import { render } from "vitest-browser-lit";
 
 import "../button/button.js";
@@ -67,4 +68,101 @@ test("tooltip's accessible description does not include arrow direction if the t
 		.toHaveAccessibleDescription(
 			"A black speech bubble providing complementary information",
 		);
+});
+
+test("tooltip opens when its target gets hovered", async () => {
+	// We wrap the contents in a span here so the test Locator can find an element
+	// that is not the w-tooltip itself for visibility testsing.
+	const component = html`
+		<w-button id="test-button" aria-labelledby="test-tooltip">Button</w-button>
+		<w-tooltip id="test-tooltip" for="test-button">
+			<span>Tooltip contents</span>
+		</w-tooltip>
+	`;
+
+	const page = render(component);
+	await expect.element(page.getByText("Tooltip contents")).not.toBeVisible();
+
+	await userEvent.hover(page.getByRole("button"));
+
+	await expect.element(page.getByText("Tooltip contents")).toBeVisible();
+});
+
+test("tooltip closes when its target gets unhovered, except when the tooltip itself gets hovered", async () => {
+	// We wrap the contents in a span here so the test Locator can find an element
+	// that is not the w-tooltip itself for visibility testsing.
+	const component = html`
+		<w-button id="test-button" aria-labelledby="test-tooltip">Button</w-button>
+		<w-tooltip id="test-tooltip" for="test-button">
+			<span data-testid="contents">Tooltip contents</span>
+		</w-tooltip>
+	`;
+
+	const page = render(component);
+	await expect.element(page.getByText("Tooltip contents")).not.toBeVisible();
+
+	await userEvent.hover(page.getByRole("button"));
+	await expect.element(page.getByText("Tooltip contents")).toBeVisible();
+
+	await userEvent.hover(page.getByTestId("contents"));
+	await expect.element(page.getByText("Tooltip contents")).toBeVisible();
+
+	await userEvent.unhover(page.getByTestId("contents"));
+	await expect.element(page.getByText("Tooltip contents")).not.toBeVisible();
+});
+
+test("tooltip opens when its target gets focus", async () => {
+	// We wrap the contents in a span here so the test Locator can find an element
+	// that is not the w-tooltip itself for visibility testsing.
+	const component = html`
+		<w-button id="test-button" aria-labelledby="test-tooltip">Button</w-button>
+		<w-tooltip id="test-tooltip" for="test-button">
+			<span>Tooltip contents</span>
+		</w-tooltip>
+	`;
+
+	const page = render(component);
+	await expect.element(page.getByText("Tooltip contents")).not.toBeVisible();
+
+	await userEvent.keyboard("[Tab]"); // focus!
+
+	await expect.element(page.getByText("Tooltip contents")).toBeVisible();
+});
+
+// tested OK manually in Firefox 152
+test.skipIf(server.browser === "firefox")("tooltip hides on blur", async () => {
+	// We wrap the contents in a span here so the test Locator can find an element
+	// that is not the w-tooltip itself for visibility testsing.
+	const component = html`
+		<w-button id="test-button" aria-labelledby="test-tooltip">Button</w-button>
+		<w-tooltip id="test-tooltip" for="test-button">
+			<span>Tooltip contents</span>
+		</w-tooltip>
+	`;
+
+	const page = render(component);
+	await expect.element(page.getByText("Tooltip contents")).not.toBeVisible();
+
+	await userEvent.keyboard("[Tab]"); // focus!
+	await userEvent.keyboard("[Tab]"); // blur!
+
+	await expect.element(page.getByText("Tooltip contents")).not.toBeVisible();
+});
+
+test("tooltip can be dismissed with the Escape key", async () => {
+	// We wrap the contents in a span here so the test Locator can find an element
+	// that is not the w-tooltip itself for visibility testsing.
+	const component = html`
+		<w-button id="test-button" aria-labelledby="test-tooltip">Button</w-button>
+		<w-tooltip id="test-tooltip" for="test-button" open>
+			<span>Tooltip contents</span>
+		</w-tooltip>
+	`;
+
+	const page = render(component);
+	await expect.element(page.getByText("Tooltip contents")).toBeVisible();
+
+	await userEvent.keyboard("[Escape]");
+
+	await expect.element(page.getByText("Tooltip contents")).not.toBeVisible();
 });
