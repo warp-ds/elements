bb6e8ad3aee9ff379a3e005e54fe183b92519417
digitalsadhu <digitalsadhu@users.noreply.github.com>
2026-08-17 08:43:27 +0000

    Remove preview for PR 752 🛬

--- pr-preview/pr-752/assets/Breadcrumbs-C_S_u4bM.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,o as n,s as r}from"./blocks-CVd2aGst.js";import{t as i}from"./jsx-runtime-DeHZSEgm.js";import{n as a,r as o}from"./lib-BRDOWE9X.js";import{a as s,i as c,n as l,r as u,t as d}from"./breadcrumbs.stories-Bo6WmZPs.js";function f(e){let r={h1:`h1`,h2:`h2`,h3:`h3`,p:`p`,...o(),...e.components};return(0,m.jsxs)(m.Fragment,{children:[(0,m.jsx)(n,{of:c,name:`Docs`}),`
-`,(0,m.jsx)(r.h1,{id:`breadcrumbs`,children:`Breadcrumbs`}),`
-`,(0,m.jsx)(r.p,{children:`Breadcrumbs show the navigation structure for the current location.`}),`
-`,(0,m.jsx)(t,{of:d}),`
-`,(0,m.jsx)(r.h2,{id:`visual-options`,children:`Visual options`}),`
-`,(0,m.jsx)(r.h3,{id:`all-segments-linkable`,children:`All segments linkable`}),`
-`,(0,m.jsx)(t,{of:l}),`
-`,(0,m.jsx)(r.h3,{id:`disabled-last-link`,children:`Disabled last link`}),`
-`,(0,m.jsx)(t,{of:u})]})}function p(e={}){let{wrapper:t}={...o(),...e.components};return t?(0,m.jsx)(t,{...e,children:(0,m.jsx)(f,{...e})}):f(e)}var m;function h(){return(h=e((()=>{m=i(),a(),r(),s()})))()}h();export{p as default};
